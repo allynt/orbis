617df7bc87c5cf45581be52348ba7fe2ca33128b
Mark Small <mark.small@astrosat.space>
2019-11-20 00:02:38 +0000

    fix(frontend): Add ability to toggle menu items open
    The now uses the latest functionality of our widget libray Detail
    component to set it's open state.

--- client/src/side-menu/side-menu.actions.js
+++ client/src/side-menu/side-menu.actions.js
@@ -1,3 +1,6 @@
 export const TOGGLE_MENU = 'TOGGLE_MENU';
+export const TOGGLE_MENU_ITEM = 'TOGGLE_MENU_ITEM';
 
 export const toggleMenu = () => dispatch => dispatch({ type: TOGGLE_MENU });
+
+export const toggleMenuItem = label => dispatch => dispatch({ type: TOGGLE_MENU_ITEM, label });
--- client/src/side-menu/side-menu.reducer.js
+++ client/src/side-menu/side-menu.reducer.js
@@ -1,13 +1,17 @@
-import { TOGGLE_MENU } from './side-menu.actions';
+import { TOGGLE_MENU, TOGGLE_MENU_ITEM } from './side-menu.actions';
 
 export const INITIAL_STATE = {
-  isMenuVisible: false
+  isMenuVisible: false,
+  visibleMenuItem: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TOGGLE_MENU:
       return { ...state, isMenuVisible: !state.isMenuVisible };
+
+    case TOGGLE_MENU_ITEM:
+      return { ...state, visibleMenuItem: action.label };
 
     default:
       return state;
