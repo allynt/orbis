dca4c30c105d68392bf5aa9896c0e5192d0229c7
Mark Small <mark.small@astrosat.space>
2019-12-09 22:26:55 +0000

    fix(frontend): Fix props pass-through
    The `rest` props  where not being passed down correctly, now they are.

--- client/src/utils/private-route.component.js
+++ client/src/utils/private-route.component.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, user, rest }) => (
+const PrivateRoute = ({ component: Component, user, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      user ? <Component {...props} /> : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+      user ? (
+        <Component {...props} {...rest} />
+      ) : (
+        <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+      )
     }
   />
 );
