1c1d53a04c117f78a850a9f4e11b0b89069ab163
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-01-07 12:20:14 +0000

    feat(backend): added new secrets
    Added mapbox_token and tracking_id to the environment secrets.

--- scripts/sync-secrets.sh
+++ scripts/sync-secrets.sh
@@ -11,5 +11,7 @@ fi
 AWS_SECRET=$1
 KUBE_SECRET=$2
 
+aws configure set default.region eu-west-1
+
 kubectl delete secret $KUBE_SECRET
 kubectl create secret generic $KUBE_SECRET --from-env-file=<(aws secretsmanager get-secret-value --secret-id $AWS_SECRET | jq -r '.SecretString' | jq -r 'to_entries | .[] | .key +"=" + .value')
