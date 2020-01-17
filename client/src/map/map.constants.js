e744606d20d45cd2464610b4761ff589f94e0ade
Rory MacGregor <rorymacgregor@astrosat.net>
2020-01-17 10:44:49 +0000

    Fix(frontend): Extract map constants out into separate file.
    Created map.constants.js in map directory and moved 'regions', 'domains' and 'MAP_STYLE_DATA' to this file from map.reducer.js
    
    IssueID #

---
+++ client/src/map/map.constants.js
@@ -0,0 +1,72 @@
+export const regions = [
+  {
+    name: 'Europe',
+    value: {
+      zoom: 6,
+      center: [2.588083, 48.953583]
+    }
+  },
+  {
+    name: 'Africa',
+    value: {
+      zoom: 6,
+      center: [23.44175, 0.0]
+    }
+  },
+  {
+    name: 'Central Asia',
+    value: {
+      zoom: 6,
+      center: [68.110556, 45.517806]
+    }
+  },
+  {
+    name: 'North America',
+    value: {
+      zoom: 6,
+      center: [-104.634381, 51.83855]
+    }
+  }
+];
+
+export const MAP_STYLE_DATA = [
+  {
+    id: 'streets',
+    uri: 'mapbox://styles/thermcert/cjyx8fn3u2ojo1cmzfl8spj05',
+    title: 'Streets'
+  },
+  {
+    id: 'light',
+    uri: 'mapbox://styles/thermcert/cjyx8apt22o611cl5ih9kdrrh',
+    title: 'Light'
+  },
+  {
+    id: 'dark',
+    uri: 'mapbox://styles/thermcert/cjyx8dj0m2om41cohblkd42my',
+    title: 'Dark'
+  },
+  {
+    id: 'satellite',
+    uri: 'mapbox://styles/thermcert/cjyx826k90y3g1cmwp64ropem',
+    title: 'Satellite'
+  }
+];
+
+export const domains = [
+  {
+    name: 'TropoSense',
+    value: 'troposense'
+  },
+  {
+    name: 'Rice Paddies',
+    value: 'ricepaddies'
+  },
+  {
+    name: 'Marine Pollution',
+    value: 'marinepolution'
+  },
+  {
+    name: 'Forestry & Landslides',
+    value: 'forestryandlandslides'
+  }
+];
--- client/src/map/map.reducer.js
+++ client/src/map/map.reducer.js
@@ -1,3 +1,5 @@
+import { regions, domains, MAP_STYLE_DATA } from './map.constants';
+
 import {
   TOGGLE_3D_MODE,
   TOGGLE_MULTI_MODE,
@@ -13,79 +15,6 @@ import {
   CUSTOM_DATA_REQUESTED_FAILURE,
   SET_VIEWPORT
 } from './map.actions';
-
-const MAP_STYLE_DATA = [
-  {
-    id: 'streets',
-    uri: 'mapbox://styles/thermcert/cjyx8fn3u2ojo1cmzfl8spj05',
-    title: 'Streets'
-  },
-  {
-    id: 'light',
-    uri: 'mapbox://styles/thermcert/cjyx8apt22o611cl5ih9kdrrh',
-    title: 'Light'
-  },
-  {
-    id: 'dark',
-    uri: 'mapbox://styles/thermcert/cjyx8dj0m2om41cohblkd42my',
-    title: 'Dark'
-  },
-  {
-    id: 'satellite',
-    uri: 'mapbox://styles/thermcert/cjyx826k90y3g1cmwp64ropem',
-    title: 'Satellite'
-  }
-];
-
-const domains = [
-  {
-    name: 'TropoSense',
-    value: 'troposense'
-  },
-  {
-    name: 'Rice Paddies',
-    value: 'ricepaddies'
-  },
-  {
-    name: 'Marine Pollution',
-    value: 'marinepolution'
-  },
-  {
-    name: 'Forestry & Landslides',
-    value: 'forestryandlandslides'
-  }
-];
-
-const regions = [
-  {
-    name: 'Europe',
-    value: {
-      zoom: 6,
-      center: [2.588083, 48.953583]
-    }
-  },
-  {
-    name: 'Africa',
-    value: {
-      zoom: 6,
-      center: [23.44175, 0.0]
-    }
-  },
-  {
-    name: 'Central Asia',
-    value: {
-      zoom: 6,
-      center: [68.110556, 45.517806]
-    }
-  },
-  {
-    name: 'North America',
-    value: {
-      zoom: 6,
-      center: [-104.634381, 51.83855]
-    }
-  }
-];
 
 const initialState = {
   viewport: { zoom: 6, center: [-4.84, 54.71] },
