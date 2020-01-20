ea8f3d26233fc255fe9edef413c950d963c98766
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-01-20 13:36:53 +0000

    feat(frontend): ensured bookmark passes correct data to API
    * removed form data boundary from FORM_HEADERS constant
    * stringified fromData content in addBookmark action as needed
    * used thumbnail URL in picture element
    * updated CSS to make thumbnails fit

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -4,8 +4,8 @@ export const JSON_HEADERS = {
 };
 
 export const FORM_HEADERS = {
-  Accept: 'application/json, application/xml, text/plain, text/html, *.*',
-  'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary2QT6fGjSCgRZuMif'
+  Accept: 'application/json, application/xml, text/plain, text/html, *.*'
+  // 'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary1234 // cannot hard-code form boundary
 };
 
 export const getData = (url, headers = {}) => {
