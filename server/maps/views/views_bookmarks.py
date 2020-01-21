33ba17b9599d642de7e7885ff9952b3f6476d4f8
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-01-21 11:46:13 +0000

    feat(backend): handle S3 Bookmark errors
    Fail gracefully w/ an informative message if anything goes wrong when
    trying to access the S3 storage backend for Bookmarks.  (This will catch
    the case where the media settings are improperly set.)

--- server/maps/views/views_bookmarks.py
+++ server/maps/views/views_bookmarks.py
@@ -1,11 +1,35 @@
-from rest_framework import viewsets
-from rest_framework.parsers import JSONParser, FormParser, MultiPartParser, FileUploadParser
-from rest_framework.permissions import IsAuthenticated
+import functools
+
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+
+from rest_framework import status, viewsets
+from rest_framework.exceptions import APIException
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.permissions import IsAuthenticated, BasePermission
+
+from botocore.exceptions import BotoCoreError
 
 from maps.models import Bookmark
 from maps.serializers import BookmarkSerializer
 
 
+def check_storage_access(view_fn):
+    """
+    Gracefully fails if anything storage-related fails in the ViewSet below
+    """
+    @functools.wraps(view_fn)
+    def check_storage_access_wrapper(request, *args, **kwargs):
+        try:
+            return view_fn(request, *args, **kwargs)
+        except BotoCoreError as e:
+            data = {"error": str(e)}
+            return JsonResponse(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return check_storage_access_wrapper
+
+
+@method_decorator(check_storage_access, name="dispatch")
 class BookmarkViewSet(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticated]
