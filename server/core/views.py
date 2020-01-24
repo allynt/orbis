edd21ea2a739cfae5cac833c02d71fb566fdc649
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-01-24 06:43:12 +0000

    chore(backend): added documentation
    Added explicit swagger documentation for the config_view as part of #47.

--- server/core/views.py
+++ server/core/views.py
@@ -1,8 +1,14 @@
+from collections import OrderedDict
+
 from django.conf import settings
 from django.views.generic import TemplateView
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from astrosat_users.conf import app_settings as astrosat_users_settings
 
@@ -21,7 +27,27 @@ index_view = TemplateView.as_view(template_name="index.html")
 
 
 class AppConfigView(APIView):
+
+    # AppConfigView has no serializer to generate a swagger schema from
+    # so I define one here just to make the generated documentation work
+    _config_schema = openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties=OrderedDict((
+            ("trackingId", openapi.Schema(type=openapi.TYPE_STRING)),
+            ("mapbox_token", openapi.Schema(type=openapi.TYPE_STRING)),
+            ("passwordMinLength", openapi.Schema(type=openapi.TYPE_INTEGER)),
+            ("passwordMaxLength", openapi.Schema(type=openapi.TYPE_INTEGER)),
+            ("isRegistrationOpen", openapi.Schema(type=openapi.TYPE_BOOLEAN)),
+            ("isVerificationRequired", openapi.Schema(type=openapi.TYPE_BOOLEAN)),
+            ("isApprovalRequired", openapi.Schema(type=openapi.TYPE_BOOLEAN)),
+        ))
+    )
+
+    @swagger_auto_schema(responses={status.HTTP_200_OK: _config_schema})
     def get(self, request, format=None):
+        """
+        Returns some information required by the client for initial configuration
+        """
 
         config = {
             "trackingId": settings.TRACKING_ID,
