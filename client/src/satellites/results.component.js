fbdaf107aff3773b8647630c07ddaa604b374f02
Mark Small <mark.small@astrosat.space>
2020-01-30 10:08:45 +0000

    fix(frontend): Filter list of scenes by clould coverage
    I've now added the ability to filter the list of scenes by the amount
    of cloud coverage that exists in the image.

--- client/src/satellites/results.component.js
+++ client/src/satellites/results.component.js
@@ -28,30 +28,35 @@ const Results = ({ scenes, setVisiblePanel, selectScene }) => {
         <div className={styles.results}>
           <h3>RESULTS</h3>
 
-          <div className={styles.resultCount}>Showing {scenes.length} Results</div>
+          <div className={styles.resultCount}>
+            Showing {scenes.filter(scene => scene.properties.cloudCoverAsPercentage <= cloudCoverPercentage[0]).length}{' '}
+            Results
+          </div>
 
           <ul className={styles.scenes}>
-            {scenes.map(scene => {
-              return (
-                <li
-                  key={scene.id}
-                  className={styles.scene}
-                  onClick={() => {
-                    dispatch(selectScene(scene));
-                    setVisiblePanel(VISUALISATION);
-                  }}
-                >
-                  <img className={styles.thumbnail} src={scene.thumbnail} alt="Thumbnail" />
-                  <ul className={styles.metadata}>
-                    <li>{format(parseISO(scene.properties.created), 'dd-MM-yyyy')}</li>
-                    <li>{format(parseISO(scene.properties.created), 'kk:mm:ss')} UTC</li>
-                    <li>{scene.properties.cloudCoverAsPercentage} %</li>
-                    <li>{scene.properties.crs}</li>
-                    <li>{scene.properties.label}</li>
-                  </ul>
-                </li>
-              );
-            })}
+            {scenes
+              .filter(scene => scene.properties.cloudCoverAsPercentage <= cloudCoverPercentage[0])
+              .map(scene => {
+                return (
+                  <li
+                    key={scene.id}
+                    className={styles.scene}
+                    onClick={() => {
+                      dispatch(selectScene(scene));
+                      setVisiblePanel(VISUALISATION);
+                    }}
+                  >
+                    <img className={styles.thumbnail} src={scene.thumbnail} alt="Thumbnail" />
+                    <ul className={styles.metadata}>
+                      <li>{format(parseISO(scene.properties.created), 'dd-MM-yyyy')}</li>
+                      <li>{format(parseISO(scene.properties.created), 'kk:mm:ss')} UTC</li>
+                      <li>{scene.properties.cloudCoverAsPercentage} %</li>
+                      <li>{scene.properties.crs}</li>
+                      <li>{scene.properties.label}</li>
+                    </ul>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
