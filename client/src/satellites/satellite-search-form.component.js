5da30c2a1c43151d8d1f3333cd950521ba2b6e08
Mark Small <mark.small@astrosat.space>
2020-03-09 18:15:23 +0000

    fix(frontend): Migrate search to a form component
    The form aspect took up a lot of the search component, so I factored
    it out into it's own component. I also altered it to be a form, instead
    of collecting a bunch of internal state, I feel this simplifies it.
    
    IssueID #158

---
+++ client/src/satellites/satellite-search-form.component.js
@@ -0,0 +1,217 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { useDispatch } from 'react-redux';
+
+import { subDays, formatISO } from 'date-fns';
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import Button from '@astrosat/astrosat-ui/dist/buttons/button';
+import Checkbox from '@astrosat/astrosat-ui/dist/forms/checkbox';
+import useForm from '@astrosat/astrosat-ui/dist/forms/use-form';
+
+import validate from './satellite-search-form.validator';
+
+import { setCurrentSearchQuery, searchSatellites } from './satellites.actions';
+
+import { RESULTS } from './satellites-panel.component';
+
+import { ReactComponent as InfoIcon } from './info.svg';
+
+import styles from './search.module.css';
+import sideMenuStyles from '../side-menu/side-menu.module.css';
+
+const DATE_FORMAT = 'yyy-MM-dd';
+const DAYS_IN_PAST = 7;
+const dateFormat = 'd MMM yyy';
+
+const resolutions = [
+  {
+    id: 'free',
+    label: 'Free images',
+    description: 'Some text describing the FREE images'
+  },
+  {
+    id: 'mid',
+    label: 'Mid-resolution',
+    description: 'Some text describing the MID-RES images'
+  },
+  {
+    id: 'high',
+    label: 'High-resolution',
+    description: 'Some text describing the HIGH-RES images'
+  }
+];
+
+const CustomInput = React.forwardRef(({ value, onClick }, ref) => (
+  <button className={styles.picker} onClick={onClick}>
+    {value}
+  </button>
+));
+
+const defaults = {
+  values: {
+    'sentinel-2': true,
+    free: true
+  }
+};
+
+const SatelliteSearchForm = ({
+  satellites,
+  geometry,
+  setVisiblePanel,
+  setSelectedSatelliteMoreInfo,
+  toggleSatelliteMoreInfoDialog,
+  setSelectedResolutionMoreInfo,
+  toggleResolutionMoreInfoDialog
+}) => {
+  const dispatch = useDispatch();
+
+  const [startDate, setStartDate] = useState(subDays(new Date(), DAYS_IN_PAST));
+  const [endDate, setEndDate] = useState(new Date());
+
+  const { handleChange, handleSubmit, values, errors } = useForm(onSubmit, validate, defaults);
+
+  function onSubmit() {
+    // Collect all selected satellites into one array of satellite ids.
+    const selectedSatellites = satellites.reduce((acc, satellite) => {
+      // Check if satellite id exists in values object.
+      const key = Object.keys(values).find(key => key === satellite.id);
+      if (key && values[key]) {
+        acc = [...acc, key];
+      }
+      return acc;
+    }, []);
+
+    // Collect all selected tiers into one array of tier ids.
+    const selectedTiers = resolutions.reduce((acc, resolution) => {
+      // Check if tier id exists in values object.
+      const key = Object.keys(values).find(key => key === resolution.id);
+      if (key && values[key]) {
+        acc = [...acc, key];
+      }
+      return acc;
+    }, []);
+
+    const query = {
+      satellites: selectedSatellites,
+      startDate: formatISO(startDate),
+      endDate: formatISO(endDate),
+      resolutions: selectedTiers,
+      geometry
+    };
+
+    dispatch(setCurrentSearchQuery(query));
+    dispatch(searchSatellites(query));
+    setVisiblePanel(RESULTS);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.filters}>
+        <div>
+          <h3>SEARCH</h3>
+
+          <ul className={styles.satellites}>
+            {satellites.map(satellite => (
+              <li key={satellite.label} className={styles.satellite}>
+                <Checkbox
+                  name={satellite.id}
+                  label={satellite.label}
+                  onChange={handleChange}
+                  checked={values[satellite.id]}
+                  disabled={satellite.id !== 'sentinel-2'}
+                />
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    console.log('Selected SATELLITE: ', satellite);
+                    setSelectedSatelliteMoreInfo({ id: 1, description: 'desc' });
+                    toggleSatelliteMoreInfoDialog();
+                  }}
+                >
+                  <InfoIcon className={styles.icon} />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h3>TIME RANGE</h3>
+
+          <div className={styles.options}>
+            <DatePicker
+              name="startDate"
+              dateFormat={DATE_FORMAT}
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              dateFormat={dateFormat}
+              customInput={<CustomInput />}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+            />
+            <span> to </span>
+            <DatePicker
+              name="endDate"
+              dateFormat={DATE_FORMAT}
+              selected={endDate}
+              onChange={date => setEndDate(date)}
+              dateFormat={dateFormat}
+              customInput={<CustomInput />}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+            />
+          </div>
+        </div>
+
+        <div>
+          <h3>Resolution</h3>
+
+          <ul className={styles.resolutions}>
+            {resolutions.map(resolution => {
+              return (
+                <li className={styles.resolution} key={resolution.id}>
+                  <Checkbox
+                    name={resolution.id}
+                    label={resolution.label}
+                    onChange={handleChange}
+                    checked={values[resolution.id]}
+                    disabled={resolution.id !== 'free'}
+                  />
+
+                  <button
+                    // onBlur={() => dispatch({ type: SET_IS_INFO_VISIBLE, payload: false })}
+                    type="button"
+                    onClick={() => {
+                      setSelectedResolutionMoreInfo({ id: 1, description: 'desc' });
+                      toggleResolutionMoreInfoDialog();
+                    }}
+                  >
+                    <InfoIcon className={styles.icon} />
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+
+      <div className={sideMenuStyles.buttons}>
+        <Button type="submit" theme="primary" classNames={[sideMenuStyles.button]}>
+          Search
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+SatelliteSearchForm.propTypes = {};
+
+export default SatelliteSearchForm;
--- client/src/satellites/search.component.js
+++ client/src/satellites/search.component.js
@@ -3,118 +3,40 @@ import PropTypes from 'prop-types';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import { subDays } from 'date-fns';
-
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-
-import { searchSatellites } from './satellites.actions';
-
 import Detail from '@astrosat/astrosat-ui/dist/containers/detail';
 import Button from '@astrosat/astrosat-ui/dist/buttons/button';
-import InfoButton from '@astrosat/astrosat-ui/dist/buttons/info-button';
-import Checkbox from '@astrosat/astrosat-ui/dist/forms/checkbox';
 import Dialog from '@astrosat/astrosat-ui/dist/containers/dialog';
 import useModal from '@astrosat/astrosat-ui/dist/containers/use-modal';
 
-import { RESULTS } from './satellites-panel.component';
 import SavedSearchList from './saved-search-list.component';
 
-import useMap from '../map/use-map.hook';
-import { fetchSavedSatellites, setCurrentSearchQuery } from './satellites.actions';
+import SatelliteSearchForm from './satellite-search-form.component';
 
-import { ReactComponent as InfoIcon } from './info.svg';
+import useMap from '../map/use-map.hook';
+import { fetchSavedSatellites } from './satellites.actions';
+
 import { ReactComponent as DrawAoiIcon } from './draw-aoi.svg';
 
 import styles from './search.module.css';
 import sideMenuStyles from '../side-menu/side-menu.module.css';
 
-const DATE_FORMAT = 'yyy-MM-dd';
-const dateFormat = 'd MMM yyy';
-
 const AOI_DRAW_MODE = 'RectangleMode';
 const BBOX_NO_OF_POINTS = 5;
-const DAYS_IN_PAST = 7;
-
-const resolutions = [
-  {
-    id: 'free',
-    label: 'Free images',
-    description: 'Some text describing the FREE images'
-  },
-  {
-    id: 'mid',
-    label: 'Mid-resolution',
-    description: 'Some text describing the MID-RES images'
-  },
-  {
-    id: 'high',
-    label: 'High-resolution',
-    description: 'Some text describing the HIGH-RES images'
-  }
-];
-
-const CustomInput = React.forwardRef(({ value, onClick }, ref) => (
-  <button className={styles.picker} onClick={onClick}>
-    {value}
-  </button>
-));
-
-// const initialState = {
-//   selectedSatellites: [],
-//   startDate: new Date(),
-//   endDate: new Date(),
-//   isInfoVisible: false,
-//   info: null,
-//   selectedSearch: {}
-// };
-
-// const SET_SELECTED_SATELLITES = 'SET_SELECTED_SATELLITES';
-// const SET_START_DATE = 'SET_START_DATE';
-// const SET_END_DATE = 'SET_END_DATE';
-// const SET_IS_INFO_VISIBLE = 'SET_IS_INFO_VISIBLE';
-// const SET_INFO = 'SET_INFO';
-// const SET_SELECTED_SEARCH = 'SET_SELECTED_SEARCH';
-
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case SET_SELECTED_SATELLITES:
-//       return { ...state, selectedSatellites: action.payload };
-//     case SET_START_DATE:
-//       return { ...state, startDate: action.payload };
-//     case SET_END_DATE:
-//       return { ...state, endDate: action.payload };
-//     case SET_IS_INFO_VISIBLE:
-//       return { ...state, isInfoVisible: action.payload };
-//     case SET_INFO:
-//       return { ...state, info: action.payload };
-//     case SET_SELECTED_SEARCH:
-//       return { ...state, selectedSearch: action.payload };
-//     default:
-//       throw new Error('Unknown Action Type: ', action.type);
-//   }
-// };
 
 const Search = ({ satellites, setVisiblePanel, map }) => {
-  const globalDispatch = useDispatch();
-  const [selectedSatellites, setSelectedSatellites] = useState([]);
-  const [isInfoVisible, setIsInfoVisible] = useState(false);
-  const [startDate, setStartDate] = useState(subDays(new Date(), DAYS_IN_PAST));
-  const [endDate, setEndDate] = useState(new Date());
+  const dispatch = useDispatch();
+
+  const [geometry, setGeometry] = useState(null);
 
   const savedSearches = useSelector(state => state.satellites.satelliteSearches);
 
-  const [selectedResolutions, setSelectedResolutions] = useState([]);
+  const ref = useRef(null);
   const [isSatelliteMoreInfoDialogVisible, toggleSatelliteMoreInfoDialog] = useModal(false);
   const [isResolutionMoreInfoDialogVisible, toggleResolutionMoreInfoDialog] = useModal(false);
-  const ref = useRef(null);
   const [selectedSatelliteMoreInfo, setSelectedSatelliteMoreInfo] = useState(null);
   const [selectedResolutionMoreInfo, setSelectedResolutionMoreInfo] = useState(null);
 
-  // const [state, dispatch] = useReducer(reducer, initialState);
-
   const [isAoiMode, setIsAoiMode] = useState(false);
-  const [geometry, setGeometry] = useState(null);
 
   useMap(
     map,
@@ -164,7 +86,6 @@ const Search = ({ satellites, setVisiblePanel, map }) => {
         if (feature && feature.properties.drawType === 'AOI') {
           const coordinates = feature.geometry.coordinates;
           if (coordinates[0].length === BBOX_NO_OF_POINTS) {
-            // console.log('SET GEOM: ', coordinates);
             setGeometry(coordinates);
           }
         }
@@ -176,171 +97,50 @@ const Search = ({ satellites, setVisiblePanel, map }) => {
 
   useEffect(() => {
     if (!savedSearches) {
-      globalDispatch(fetchSavedSatellites());
+      dispatch(fetchSavedSatellites());
     }
   }, [savedSearches]);
 
   return (
     <div className={styles.search} ref={ref}>
       <div>
-        <Detail title="Saved Searches">
-          <SavedSearchList searches={savedSearches} />
-        </Detail>
-      </div>
-
-      <div className={styles.filters}>
-        <div>
-          <h3>SEARCH</h3>
-
-          <ul className={styles.satellites}>
-            {satellites.map(satellite => (
-              <li key={satellite.label} className={styles.satellite}>
-                <Checkbox
-                  name={satellite.id}
-                  // value="true"
-                  // checked={satellite.label === 'Sentinel-2' ? true : false}
-                  label={satellite.label}
-                  onChange={() => {
-                    const result = selectedSatellites.find(sat => sat.label === satellite.label);
-
-                    if (result) {
-                      // Remove from selected list
-                      setSelectedSatellites(selectedSatellites.filter(sat => sat.label !== satellite.label));
-                    } else {
-                      // Add to selected list
-                      setSelectedSatellites([...selectedSatellites, satellite]);
-                    }
-                  }}
-                />
-
-                <button
-                  onClick={() => {
-                    console.log('Selected SATELLITE: ', satellite);
-                    setSelectedSatelliteMoreInfo({ id: 1, description: 'desc' });
-                    toggleSatelliteMoreInfoDialog();
-                  }}
-                >
-                  <InfoIcon className={styles.icon} />
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <h3>TIME RANGE</h3>
-
-          <div className={styles.options}>
-            <DatePicker
-              dateFormat={DATE_FORMAT}
-              selected={startDate}
-              onChange={date => setStartDate(date)}
-              dateFormat={dateFormat}
-              customInput={<CustomInput />}
-              selectsStart
-              startDate={startDate}
-              endDate={endDate}
-            />
-            <span> to </span>
-            <DatePicker
-              dateFormat={DATE_FORMAT}
-              selected={endDate}
-              onChange={date => setEndDate(date)}
-              dateFormat={dateFormat}
-              customInput={<CustomInput />}
-              selectsEnd
-              startDate={startDate}
-              endDate={endDate}
-              minDate={startDate}
-            />
+        {savedSearches ? (
+          <div>
+            <Detail title="Saved Searches">
+              <SavedSearchList searches={savedSearches} />
+            </Detail>
           </div>
-        </div>
+        ) : (
+          <p>There are no saved AOI yet</p>
+        )}
 
-        <div>
-          <h3>Resolution</h3>
-
-          <ul className={styles.resolutions}>
-            {resolutions.map(resolution => {
-              return (
-                <li className={styles.resolution} key={resolution.id}>
-                  <Checkbox
-                    name={resolution.id}
-                    // value={values.resolution || resolution.label}
-                    label={resolution.label}
-                    // onChange={handleChange}
-                    // checked={state.selectedSearch.resolutions.includes(res.id)}
-                    onChange={event => {
-                      // console.log('CHECKBOX EVENT: ', event);
-                      const result = selectedResolutions.find(res => res.id === resolution.id);
-
-                      if (result) {
-                        // Remove from selected list
-                        setSelectedResolutions(selectedResolutions.filter(res => res.id !== resolution.id));
-                      } else {
-                        // Add to selected list
-                        setSelectedResolutions([...selectedResolutions, resolution]);
-                      }
-                    }}
-                  />
-
-                  <button
-                    // onBlur={() => dispatch({ type: SET_IS_INFO_VISIBLE, payload: false })}
-                    onClick={() => {
-                      setSelectedResolutionMoreInfo({ id: 1, description: 'desc' });
-                      toggleResolutionMoreInfoDialog();
-                      // dispatch({ type: SET_INFO, payload: resolution });
-                      // dispatch({ type: SET_IS_INFO_VISIBLE, payload: !state.isInfoVisible });
-                    }}
-                  >
-                    <InfoIcon className={styles.icon} />
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <div>
-          <div className={styles.noSavedSearches}>
-            <p>There are no saved AOI yet</p>
-            <div className={styles.drawAOI} onClick={() => setIsAoiMode(true)}>
-              <DrawAoiIcon className={styles.drawAOIIcon} />
-              <Button theme="link" classNames={[styles.AOIButton]}>
-                Draw AOI
-              </Button>
-            </div>
+        <div className={styles.noSavedSearches}>
+          <div className={styles.drawAOI} onClick={() => setIsAoiMode(true)}>
+            <DrawAoiIcon className={styles.drawAOIIcon} />
+            <Button theme="link" classNames={[styles.AOIButton]}>
+              Draw AOI
+            </Button>
           </div>
         </div>
       </div>
 
-      <div className={sideMenuStyles.buttons}>
-        <Button
-          theme="primary"
-          classNames={[sideMenuStyles.button]}
-          onClick={() => {
-            globalDispatch(
-              setCurrentSearchQuery({
-                satellites: selectedSatellites,
-                startDate,
-                endDate,
-                resolutions: selectedResolutions,
-                geometry
-              })
-            );
-            globalDispatch(searchSatellites(selectedSatellites, startDate, endDate));
-            setVisiblePanel(RESULTS);
-          }}
-        >
-          Search
-        </Button>
-        <Button
-          classNames={[sideMenuStyles.button]}
-          theme="tertiary"
-          onClick={() => console.log('Task Satellite Button Clicked')}
-        >
-          Task Satellite
-        </Button>
-      </div>
+      <SatelliteSearchForm
+        satellites={satellites}
+        geometry={geometry}
+        setVisiblePanel={setVisiblePanel}
+        setSelectedSatelliteMoreInfo={setSelectedSatelliteMoreInfo}
+        toggleSatelliteMoreInfoDialog={toggleSatelliteMoreInfoDialog}
+        setSelectedResolutionMoreInfo={setSelectedResolutionMoreInfo}
+        toggleResolutionMoreInfoDialog={toggleResolutionMoreInfoDialog}
+      />
+      <Button
+        classNames={[sideMenuStyles.button]}
+        theme="tertiary"
+        onClick={() => console.log('Task Satellite Button Clicked')}
+      >
+        Task Satellite
+      </Button>
+
       <Dialog
         isVisible={isSatelliteMoreInfoDialogVisible}
         title="Satellite Info"
