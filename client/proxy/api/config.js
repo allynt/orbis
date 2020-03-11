ef8bcb8cc979373220921f96da981bb5938373f6
Reignable <joebarrett401@gmail.com>
2020-03-11 14:41:33 +0000

    fix(frontend): Add map styles to proxy
    Added the array of map styles to appConfig
    This fixes an issue where the app crashes on load due them missing

--- client/proxy/api/config.js
+++ client/proxy/api/config.js
@@ -2,7 +2,13 @@
 
 const appConfig = {
   trackingId: 'UA-143753043-1',
-  mapbox_token: 'pk.eyJ1IjoidGhlcm1jZXJ0IiwiYSI6ImNqbmN5N2F6NzBnODYza3A2anVqYWExOW8ifQ.10y0sH8cDQp9AfZNg1-M3Q'
+  mapbox_token: 'pk.eyJ1IjoidGhlcm1jZXJ0IiwiYSI6ImNqbmN5N2F6NzBnODYza3A2anVqYWExOW8ifQ.10y0sH8cDQp9AfZNg1-M3Q',
+  mapStyles: [
+    { id: 'streets', uri: 'mapbox://styles/mapbox/streets-v11', title: 'Taking It To The Streets' },
+    { id: 'light', uri: 'mapbox://styles/mapbox/light-v10', title: 'Light My Fire' },
+    { id: 'dark', uri: 'mapbox://styles/mapbox/dark-v10', title: 'Dark Was The Night' },
+    { id: 'satellite', uri: 'mapbox://styles/mapbox/satellite-v9', title: 'Satellite Of Love' }
+  ]
 };
 
 const userKey = { key: '57bd67287664bb1497cb29fe89d2d5087195a3ae' };
