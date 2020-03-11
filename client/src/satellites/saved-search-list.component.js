d8ae8c8a5e0966e0b64eea68c1cfdbf6b12557c3
Reignable <joebarrett401@gmail.com>
2020-03-11 15:37:13 +0000

    fix(frontend): Finish aligning saved searches
    This brings the saved searches into line with designs
    
    IssueID #179

--- client/src/satellites/saved-search-list.component.js
+++ client/src/satellites/saved-search-list.component.js
@@ -1,15 +1,14 @@
 import Button from '@astrosat/astrosat-ui/dist/buttons/button';
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteSavedSatelliteSearch, selectSearchQuery } from './satellites.actions';
 import styles from './saved-search-list.module.css';
 
 const SavedSearchList = ({ savedSearches }) => {
   const dispatch = useDispatch();
-  console.table(savedSearches);
   return (
-    <ul>
+    <ul className={styles.list}>
       {savedSearches &&
         savedSearches.map(search => {
           return (
@@ -21,7 +20,7 @@ const SavedSearchList = ({ savedSearches }) => {
                   {search.start_date} to {search.end_date}
                 </p>
                 <p className={styles.descriptionItem}>Resolution:</p>
-                <p> {search.tiers[0]}</p>
+                <p>{search.tiers.join(', ')}</p>
               </div>
               <div className={styles.buttons}>
                 <Button classNames={[styles.button]} onClick={() => dispatch(selectSearchQuery(search))}>
