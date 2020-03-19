64d255542826d7c5f2700fa83af42ccc7ead7702
Rory MacGregor <rorymacgregor@astrosat.net>
2020-03-19 20:11:52 +0000

    fix(frontend): Format date/time correctly using date-fns, use styling variables
    I split the date into DD/MM/YYYY and HH/MM as shown in the designs.
    
    Unfortunately, This means I cannot use the mapping of keys, however cool that was, as tthe date must be split in two and processed using date-fns before being rendered.

--- client/src/satellites/satellites-info-tables.component.js
+++ client/src/satellites/satellites-info-tables.component.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { format } from 'date-fns';
+
 import styles from './satellites-info-tables.module.css';
 
 export const TierInfoTable = ({ tier }) => (
@@ -51,48 +54,66 @@ export const SatelliteInfoTable = ({ satellite }) => (
   </table>
 );
 
-export const SceneInfoTable = ({ scene }) => (
-  <table className={styles.table}>
-    <tbody>
-      <tr className={styles.row}>
-        <td className={styles.title}>Summary: </td>
-        <td>
-          <table>
-            <tbody>
-              {Object.keys(scene.metadata.summary).map(key => {
-                const [first, ...rest] = key;
-                const title = `${first.toUpperCase()}${rest.join('')}`;
-                return (
-                  <tr>
-                    <td className={styles.title}>{title}</td>
-                    <td>{scene.metadata.summary[key] || 'not currently available'}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </td>
-      </tr>
-      <tr className={styles.row}>
-        <td className={styles.title}>Name of the instrument: </td>
-        <td>{scene.metadata.name_of_instrument || 'not currently available'}</td>
-      </tr>
-      <tr className={styles.row}>
-        <td className={styles.title}>Operational mode of the sensor: </td>
-        <td>{scene.metadata.operational_mode_of_sensor || 'not currently available'}</td>
-      </tr>
-      <tr className={styles.row}>
-        <td className={styles.title}>Orbit direction: </td>
-        <td>{scene.metadata.orbit_direction || 'not currently available'}</td>
-      </tr>
-      <tr className={styles.row}>
-        <td className={styles.title}>Product type: </td>
-        <td>{scene.metadata.product_type || 'not currently available'}</td>
-      </tr>
-      <tr className={styles.row}>
-        <td className={styles.title}>Orbit direction: </td>
-        <td>{scene.metadata.cloud_coverage || 'not currently available'}</td>
-      </tr>
-    </tbody>
-  </table>
-);
+export const SceneInfoTable = ({ scene }) => {
+  const date = format(new Date(scene.metadata.summary.date), 'dd MMM yyyy');
+  const time = format(new Date(scene.metadata.summary.date), 'HH:mm');
+  return (
+    <table className={styles.table}>
+      <tbody>
+        <tr className={styles.row}>
+          <td className={styles.title}>Summary: </td>
+          <td>
+            <table>
+              <tbody>
+                <tr>
+                  <td className={styles.title}>Date: </td>
+                  <td>{date || 'not currently available'}</td>
+                </tr>
+                <tr>
+                  <td className={styles.title}>Time: </td>
+                  <td>{time || 'not currently available'}</td>
+                </tr>
+                <tr>
+                  <td className={styles.title}>Instrument: </td>
+                  <td>{scene.metadata.summary.instrument || 'not currently available'}</td>
+                </tr>
+                <tr>
+                  <td className={styles.title}>Mode: </td>
+                  <td>{scene.metadata.summary.mode || 'not currently available'}</td>
+                </tr>
+                <tr>
+                  <td className={styles.title}>Satellite: </td>
+                  <td>{scene.metadata.summary.satellite || 'not currently available'}</td>
+                </tr>
+                <tr>
+                  <td className={styles.title}>Size: </td>
+                  <td>{scene.metadata.summary.size || 'not currently available'}</td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.title}>Name of the instrument: </td>
+          <td>{scene.metadata.name_of_instrument || 'not currently available'}</td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.title}>Operational mode of the sensor: </td>
+          <td>{scene.metadata.operational_mode_of_sensor || 'not currently available'}</td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.title}>Orbit direction: </td>
+          <td>{scene.metadata.orbit_direction || 'not currently available'}</td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.title}>Product type: </td>
+          <td>{scene.metadata.product_type || 'not currently available'}</td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.title}>Orbit direction: </td>
+          <td>{scene.metadata.cloud_coverage || 'not currently available'}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
