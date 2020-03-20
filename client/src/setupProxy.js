2c14d8d95641c29727d096c91e7a934f2da8dcd1
Reignable <joebarrett401@gmail.com>
2020-03-20 09:10:19 +0000

    fix(frontend): Dynamic proxy
    Added a setupProxy.js to dynamically proxy based on environment

---
+++ client/src/setupProxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const PORT = '8000';
+
+module.exports = function(app) {
+  const isDocker = process.env.REACT_APP_DOCKER === 'true';
+  const isDev = process.env.NODE_ENV === 'development';
+  console.log(isDocker, isDev);
+
+  let target = '';
+  if (isDev) {
+    target = isDocker ? `http://server:${PORT}` : `http://localhost:${PORT}`;
+  }
+  app.use(
+    createProxyMiddleware('/api', {
+      target,
+      changeOrigin: true
+    })
+  );
+};
