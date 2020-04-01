052bd489790267f8e0080add14108cfed9223128
Mark Small <mark.small@astrosat.space>
2020-04-01 20:13:39 +0100

    fix(frontend): Refactor due to changes in data model
    The data structure has changed, meaning changes needed in the filters.
    I tidied up (at least in my opinion) the `user-info-details` component,
    probably didn't have to, but there we go.

--- client/src/map/user-info-details.component.js
+++ client/src/map/user-info-details.component.js
@@ -6,34 +6,27 @@ import styles from './user-info-details.module.css';
 const PK = 'pk';
 const CREATED = 'created';
 
-const UserInfoDetail = ({ features }) => {
-  return (
-    <>
-      <h1 className={styles.header}>User Details</h1>
-      <div className={styles.modal}>
-        {features.map(feature => {
-          const properties = feature.properties;
-          return (
-            <div key={feature.id}>
-              <ul className={styles.list}>
-                {Object.keys(properties)
-                  .filter(key => key !== PK && key !== CREATED)
-                  .map(key => {
-                    return (
-                      <li key={key}>
-                        <span className={styles.label}>{key}: </span>
-                        <span>{properties[key]}</span>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-};
+const UserInfoDetail = ({ features }) => (
+  <>
+    <h1 className={styles.header}>User Details</h1>
+    <div className={styles.modal}>
+      {features.map(feature => (
+        <div key={feature.id}>
+          <ul className={styles.list}>
+            {Object.keys(feature.properties)
+              .filter(key => key !== PK && key !== CREATED)
+              .map(key => (
+                <li key={key}>
+                  <span className={styles.label}>{key}: </span>
+                  <span>{feature.properties[key]}</span>
+                </li>
+              ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  </>
+);
 
 UserInfoDetail.propTypes = {
   features: PropTypes.array.isRequired
