00ab40ba97dea7eb7648d73c6f5229bf77d7a4f5
Rory MacGregor <rorymacgregor@astrosat.net>
2020-04-01 11:28:41 +0100

    fix(frontend): Fix covid modals
    I now realise that the modal for multiple users is just the one for a single user with many being iterated over.
    So to fix this, I had to map lal of them into the function that then maps the list elements.
    
    I also took the scrollbar from the Satellites panel and re-used it here.
    
    I had to set a min-width to the modals sop they are not cramped and match the designs.

--- client/src/map/user-info-details.component.js
+++ client/src/map/user-info-details.component.js
@@ -7,22 +7,30 @@ const PK = 'pk';
 const CREATED = 'created';
 
 const UserInfoDetail = ({ features }) => {
-  const properties = features[0].properties;
   return (
     <>
       <h1 className={styles.header}>User Details</h1>
-      <ul className={styles.list}>
-        {Object.keys(properties)
-          .filter(key => key !== PK && key !== CREATED)
-          .map(key => {
-            return (
-              <li key={key}>
-                <span className={styles.label}>{key}: </span>
-                <span>{properties[key]}</span>
-              </li>
-            );
-          })}
-      </ul>
+      <div className={styles.modal}>
+        {features.map(feature => {
+          const properties = feature.properties;
+          return (
+            <>
+              <ul className={styles.list}>
+                {Object.keys(properties)
+                  .filter(key => key !== PK && key !== CREATED)
+                  .map(key => {
+                    return (
+                      <li key={key}>
+                        <span className={styles.label}>{key}: </span>
+                        <span>{properties[key]}</span>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </>
+          );
+        })}
+      </div>
     </>
   );
 };
