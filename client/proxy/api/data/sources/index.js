25ae7926a4550bc9b477326454ecae74bf3e9e21
Rory MacGregor <rorymacgregor@astrosat.net>
2020-05-13 13:50:11 +0100

    fix(frontend): Make requested changes
    Removed some unneeded code, renamed "population-information" to "hourglass-population-information" to be more specific.

--- client/proxy/api/data/sources/index.js
+++ client/proxy/api/data/sources/index.js
@@ -1,6 +1,6 @@
 const healthInfrastructure = require('./health-infrastructure');
 const hospitalsUk = require('./hospitals-uk');
-const populationInformation = require('./population-information');
+const populationInformation = require('./hourglass-population-information');
 const sentinel2Rgb = require('./sentinel-2-rgb');
 const stokeOnTrent = require('./stoke-on-trent');
 const superSen2JapanBand = require('./super-sen2-japan-band');
