d8d7d029c0771f3aef506f8970bd9836d474ef1f
Rory MacGregor <rorymacgregor@astrosat.net>
2020-05-18 10:25:50 +0100

    fix(frontend): Extract data format to constant, inline date string
    A bit of cleanup, the date format has been extracted out to its own constant, and the date string itself is simply inlined instead of being stored inside a variable and passed in.

--- client/src/landing/landing.component.js
+++ client/src/landing/landing.component.js
@@ -20,6 +20,8 @@ import { ReactComponent as OrbisLogo } from '../orbis.svg';
 
 import styles from './landing.module.css';
 
+const DATE_FORMAT = ['MMMM do Y'];
+
 const ViewAllItems = ({ items, chooseBookmark, toggle, setViewAllItems }) => (
   <div className={styles.content}>
     <div className={styles.header}>
@@ -46,8 +48,7 @@ const Items = ({ items, chooseItem, toggle }) => {
     <div className={styles.itemLayout}>
       <div className={styles.items}>
         {items.map(item => {
-          const date = format(new Date(item.created), ['MMMM do Y']);
-          const dateString = `Created ${date}`;
+          const date = format(new Date(item.created), DATE_FORMAT);
 
           return (
             <div className={styles.item} key={item.title}>
@@ -59,7 +60,7 @@ const Items = ({ items, chooseItem, toggle }) => {
               <div className={styles.info}>
                 <div>
                   <h3 className={styles.title}>{item.title}</h3>
-                  <p className={styles.creationDate}>{dateString}</p>
+                  <p className={styles.creationDate}>{`Created ${date}`}</p>
                 </div>
               </div>
             </div>
