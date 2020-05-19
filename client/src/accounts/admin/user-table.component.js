9d02e4f08e96501de5677be0fb6d7f0366436ac7
Mark Small <mark.small@astrosat.space>
2020-05-19 22:43:00 +0100

    fix(frontend): Upgrade react-table to latest
    This upgrade was a breaking change, I had to pretty much redo the
    whole table. This upgrade also fixes the updating of a user, which
    was formerly broken.

--- client/src/accounts/admin/user-table.component.js
+++ client/src/accounts/admin/user-table.component.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
+import { useTable } from 'react-table';
 
 import ReactTooltip from 'react-tooltip';
 
@@ -11,39 +10,42 @@ import { ReactComponent as CopyIcon } from './cog.svg';
 
 import style from './user-table.module.css';
 
-const EditableCell = ({ row, data, updateUser }) => (
-  <div
-    style={{ backgroundColor: '#fafafa' }}
-    contentEditable
-    suppressContentEditableWarning
-    onBlur={event => {
-      const newData = [...data];
-      newData[row.index][row.column.id] = event.target.innerHTML;
-      updateUser(newData.find(user => user.id === row.original.id));
-    }}
-    dangerouslySetInnerHTML={{
-      __html: data[row.index][row.column.id],
-    }}
-  />
-);
+const EditableCell = ({ value: initialValue, row, column: { id }, updateUser }) => {
+  const initialState = initialValue || '';
+  const [value, setValue] = useState(initialState);
+  const onChange = event => {
+    setValue(event.target.value);
+  };
 
-const UserTable = ({ data, deleteUser, updateUser, copyUser }) => (
-  <div className={style.container}>
-    {data && (
-      <ReactTable
-        noDataText="No Data Available"
-        data={data}
-        // filterable
-        // defaultFilterMethod={(filter, row) =>
-        //   String(row[filter.id]) === filter.value
-        // }
-        columns={[
-          // { Header: 'ID', accessor: 'id' },
+  // Only update the external data when the input is blurred
+  const onBlur = () => {
+    if (id) {
+      updateUser({ ...row.original, [id]: value });
+    }
+  };
+
+  // If the initialValue is changed externall, sync it up with our state
+  useEffect(() => {
+    setValue(initialState);
+  }, [initialState]);
+
+  return <input value={value} onChange={onChange} onBlur={onBlur} />;
+};
+
+const defaultColumn = {
+  Cell: EditableCell,
+};
+
+const UserTable = ({ data, deleteUser, updateUser, copyUser }) => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Actions',
+        columns: [
           {
-            Header: 'Actions',
-            accessor: 'action',
-            maxWidth: 150,
-            Cell: row => {
+            Header: 'Copy User',
+            Cell: ({ row }) => {
+              // console.log('COPY CELL ROW: ', row);
               return (
                 <span>
                   <button onClick={() => copyUser(row.original)} data-tip data-for="copy-user">
@@ -52,7 +54,16 @@ const UserTable = ({ data, deleteUser, updateUser, copyUser }) => (
                   <ReactTooltip id="copy-user">
                     <span>Copy User</span>
                   </ReactTooltip>
-
+                </span>
+              );
+            },
+          },
+          {
+            Header: 'Delete User',
+            Cell: ({ row }) => {
+              // console.log('DELETE CELL ROW: ', row);
+              return (
+                <span>
                   <button onClick={() => deleteUser(row.original.id)} data-tip data-for="delete-user">
                     <DeleteIcon className={style.icon} alt="Delete" />
                   </button>
@@ -63,46 +74,64 @@ const UserTable = ({ data, deleteUser, updateUser, copyUser }) => (
               );
             },
           },
+        ],
+      },
+      {
+        Header: 'User Details',
+        columns: [
           {
             Header: 'Key',
-            id: 'id',
-            accessor: d => d.id,
-            contentEditable: true,
-            filterable: true,
-            filterMethod: (filter, row) => row[filter.id].startsWith(filter.value),
-            Cell: row => {
-              return <EditableCell row={row} data={data} updateUser={updateUser} />;
-            },
+            accessor: 'id',
+            Cell: ({ value }) => <span>{value}</span>,
           },
           {
             Header: 'Email',
-            id: 'email',
-            accessor: d => d.email,
-            contentEditable: true,
-            filterable: true,
-            filterMethod: (filter, row) => row[filter.id].startsWith(filter.value),
-            Cell: row => {
-              return <EditableCell row={row} data={data} updateUser={updateUser} />;
-            },
+            accessor: 'email',
           },
           {
             Header: 'Name',
-            id: 'name',
-            accessor: d => d.name,
-            contentEditable: true,
-            filterable: true,
-            filterMethod: (filter, row) => row[filter.id].startsWith(filter.value),
-            Cell: row => {
-              return <EditableCell row={row} data={data} updateUser={updateUser} />;
-            },
+            accessor: 'name',
           },
-        ]}
-        defaultPageSize={5}
-        className="-striped -highlight"
-      />
-    )}
-  </div>
-);
+        ],
+      },
+    ],
+    [copyUser, deleteUser],
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+    defaultColumn,
+    updateUser,
+  });
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 UserTable.propTypes = {
   data: PropTypes.array,
