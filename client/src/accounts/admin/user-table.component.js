972440b95b1b82ec31419639f15f818971c0dfa1
Mark Small <mark.small@astrosat.space>
2020-05-19 23:27:15 +0100

    fix(frontend): Add pagination of users
    We now paginate the list of users, making it easier for large amounts
    of data. This doesn't actually make any new data requests, that is
    possible, just didn't go that far.

--- client/src/accounts/admin/user-table.component.js
+++ client/src/accounts/admin/user-table.component.js
@@ -1,7 +1,7 @@
 import React, { useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import { useTable } from 'react-table';
+import { useTable, usePagination } from 'react-table';
 
 import ReactTooltip from 'react-tooltip';
 
@@ -11,11 +11,8 @@ import { ReactComponent as CopyIcon } from './cog.svg';
 import style from './user-table.module.css';
 
 const EditableCell = ({ value: initialValue, row, column: { id }, updateUser }) => {
-  const initialState = initialValue || '';
-  const [value, setValue] = useState(initialState);
-  const onChange = event => {
-    setValue(event.target.value);
-  };
+  const [value, setValue] = useState(initialValue || '');
+  const onChange = event => setValue(event.target.value);
 
   // Only update the external data when the input is blurred
   const onBlur = () => {
@@ -24,10 +21,12 @@ const EditableCell = ({ value: initialValue, row, column: { id }, updateUser }) 
     }
   };
 
-  // If the initialValue is changed externall, sync it up with our state
+  // If the initialValue is changed externally, sync it up with our state
   useEffect(() => {
-    setValue(initialState);
-  }, [initialState]);
+    if (initialValue) {
+      setValue(initialValue);
+    }
+  }, [initialValue]);
 
   return <input value={value} onChange={onChange} onBlur={onBlur} />;
 };
@@ -44,35 +43,29 @@ const UserTable = ({ data, deleteUser, updateUser, copyUser }) => {
         columns: [
           {
             Header: 'Copy User',
-            Cell: ({ row }) => {
-              // console.log('COPY CELL ROW: ', row);
-              return (
-                <span>
-                  <button onClick={() => copyUser(row.original)} data-tip data-for="copy-user">
-                    <CopyIcon className={style.icon} alt="Copy" />
-                  </button>
-                  <ReactTooltip id="copy-user">
-                    <span>Copy User</span>
-                  </ReactTooltip>
-                </span>
-              );
-            },
+            Cell: ({ row }) => (
+              <span>
+                <button onClick={() => copyUser(row.original)} data-tip data-for="copy-user">
+                  <CopyIcon className={style.icon} alt="Copy" />
+                </button>
+                <ReactTooltip id="copy-user">
+                  <span>Copy User</span>
+                </ReactTooltip>
+              </span>
+            ),
           },
           {
             Header: 'Delete User',
-            Cell: ({ row }) => {
-              // console.log('DELETE CELL ROW: ', row);
-              return (
-                <span>
-                  <button onClick={() => deleteUser(row.original.id)} data-tip data-for="delete-user">
-                    <DeleteIcon className={style.icon} alt="Delete" />
-                  </button>
-                  <ReactTooltip id="delete-user">
-                    <span>Delete User</span>
-                  </ReactTooltip>
-                </span>
-              );
-            },
+            Cell: ({ row }) => (
+              <span>
+                <button onClick={() => deleteUser(row.original.id)} data-tip data-for="delete-user">
+                  <DeleteIcon className={style.icon} alt="Delete" />
+                </button>
+                <ReactTooltip id="delete-user">
+                  <span>Delete User</span>
+                </ReactTooltip>
+              </span>
+            ),
           },
         ],
       },
@@ -97,39 +90,107 @@ const UserTable = ({ data, deleteUser, updateUser, copyUser }) => {
     ],
     [copyUser, deleteUser],
   );
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-    columns,
-    data,
-    defaultColumn,
-    updateUser,
-  });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    // rows,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+      updateUser,
+    },
+    usePagination,
+  );
 
   // Render the UI for your table
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row);
-
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-              })}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page &&
+            page.map(row => {
+              prepareRow(row);
+
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                  })}
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>
+        <span>
+          Page
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <span>
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>
+        <select
+          value={pageSize}
+          onChange={event => {
+            setPageSize(Number(event.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 };
 
