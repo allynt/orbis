4e52a3e031585f805ba68eec713e0c60dac9a982
Rory MacGregor <rorymacgregor@astrosat.net>
2020-06-11 16:09:44 +0100

    fic(frontend): Update tests
    Changed what was necessary to pass tests, but these will require further updating as development continues.

--- client/src/accounts/admin/user-list.component.test.js
+++ client/src/accounts/admin/user-list.component.test.js
@@ -15,7 +15,6 @@ jest.mock('./user-table.component', () => () => <div />);
 describe('User List Component', () => {
   let store = null;
   let users = null;
-  let fetchUsers = null;
   let createUser = null;
   let deleteUser = null;
   let updateUser = null;
@@ -23,7 +22,6 @@ describe('User List Component', () => {
 
   beforeEach(() => {
     users = [];
-    fetchUsers = jest.fn();
     createUser = jest.fn();
     deleteUser = jest.fn();
     updateUser = jest.fn();
@@ -38,7 +36,6 @@ describe('User List Component', () => {
     const { getByText } = render(
       <UserList
         users={null}
-        fetchUsers={fetchUsers}
         createUser={createUser}
         deleteUser={deleteUser}
         updateUser={updateUser}
@@ -48,7 +45,6 @@ describe('User List Component', () => {
 
     expect(getByText('Maintain Users')).toBeInTheDocument();
     expect(getByText('Use actions within table to update user(s)')).toBeInTheDocument();
-    expect(fetchUsers).toHaveBeenCalled();
   });
 
   it('should render the `New User` form when `New User` button clicked', () => {
@@ -57,7 +53,6 @@ describe('User List Component', () => {
       <Provider store={store}>
         <UserList
           users={users}
-          fetchUsers={fetchUsers}
           createUser={createUser}
           deleteUser={deleteUser}
           updateUser={updateUser}
