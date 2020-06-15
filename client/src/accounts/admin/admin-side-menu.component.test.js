457bff1caa1d1061639f186926fae0bfd532c1e7
Rory MacGregor <rorymacgregor@astrosat.net>
2020-06-15 15:42:37 +0100

    fix(frontend): Update tests
    Updated tests to include the selectedCustomer object passes as a prop.

--- client/src/accounts/admin/admin-side-menu.component.test.js
+++ client/src/accounts/admin/admin-side-menu.component.test.js
@@ -13,17 +13,85 @@ describe('Admin Side Menu Component', () => {
   let history = null;
   let user = null;
   let setVisiblePanel = null;
+  let selectedCustomer = null;
 
   beforeEach(() => {
     history = createMemoryHistory({ initialEntries: ['/'] });
     user = { name: 'John Smith', avatar: 'test-url' };
     setVisiblePanel = jest.fn();
+    selectedCustomer = {
+      type: 'MULTIPLE',
+      name: 'cyberdyne',
+      title: 'Cyberdyne Systems',
+      description: 'Bringing you the future, today.',
+      logo: 'https://ichef.bbci.co.uk/images/ic/1200x675/p03t1sm8.jpg',
+      users: [
+        {
+          id: 1,
+          username: 'user@test.com',
+          email: 'user@test.com',
+          name: null,
+          description: '',
+          is_verified: true,
+          is_approved: true,
+          profiles: {},
+          roles: ['UserRole', 'AstrosatRole'],
+        },
+        {
+          id: 2,
+          customers: [
+            {
+              name: 'cyberdyne',
+              manager: true,
+            },
+          ],
+          username: 'admin@test.com',
+          email: 'admin@test.com',
+          name: 'Harry Callahan',
+          avatar:
+            'https://www.bfi.org.uk/sites/bfi.org.uk/files/styles/full/public/image/dirty-harry-1971-002-clint-eastwood-medium-shot.jpg?itok=Gt8uYZDg',
+          description: '',
+          is_verified: true,
+          is_approved: true,
+          profiles: {},
+          roles: ['AdminRole', 'UserRole'],
+        },
+        {
+          id: 2,
+          username: 'verified@test.com',
+          email: 'verified@test.com',
+          password: 'pandaconcretespoon',
+          name: null,
+          description: '',
+          is_verified: true,
+          is_approved: false,
+          profiles: {},
+          roles: ['UserRole'],
+        },
+        {
+          id: 3,
+          username: 'approved@test.com',
+          email: 'approved@test.com',
+          password: 'pandaconcretespoon',
+          name: null,
+          description: '',
+          is_verified: false,
+          is_approved: true,
+          profiles: {},
+          roles: ['UserRole'],
+        },
+      ],
+      roles: ['SaveTheWorldRole'],
+      permissions: ['can_deploy_skynet'],
+      data_limit: 100,
+      data_total: 50,
+    };
   });
 
   it('should render the admin console side menu', () => {
     const { getByText, getByAltText } = render(
       <Router history={history}>
-        <AdminSideMenu user={user} setVisiblePanel={setVisiblePanel} />
+        <AdminSideMenu user={user} selectedCustomer={selectedCustomer} setVisiblePanel={setVisiblePanel} />
       </Router>,
     );
 
@@ -38,7 +106,7 @@ describe('Admin Side Menu Component', () => {
     user.avatar = null;
     const { getByTestId } = render(
       <Router history={history}>
-        <AdminSideMenu user={user} setVisiblePanel={setVisiblePanel} />
+        <AdminSideMenu user={user} selectedCustomer={selectedCustomer} setVisiblePanel={setVisiblePanel} />
       </Router>,
     );
 
@@ -48,7 +116,7 @@ describe('Admin Side Menu Component', () => {
   it('should return to user table view when Orbis logo is clicked', () => {
     const { getByTestId } = render(
       <Router history={history}>
-        <AdminSideMenu user={user} setVisiblePanel={setVisiblePanel} />
+        <AdminSideMenu user={user} selectedCustomer={selectedCustomer} setVisiblePanel={setVisiblePanel} />
       </Router>,
     );
 
@@ -59,7 +127,7 @@ describe('Admin Side Menu Component', () => {
   it('should open Orbis in a new tab when "Launch Orbis" link clicked', () => {
     const { getByText } = render(
       <Router history={history}>
-        <AdminSideMenu user={user} setVisiblePanel={setVisiblePanel} />
+        <AdminSideMenu user={user} selectedCustomer={selectedCustomer} setVisiblePanel={setVisiblePanel} />
       </Router>,
     );
 
@@ -70,7 +138,7 @@ describe('Admin Side Menu Component', () => {
   it('should switch main panel views when side-menu buttons are clicked', () => {
     const { getByText } = render(
       <Router history={history}>
-        <AdminSideMenu user={user} setVisiblePanel={setVisiblePanel} />
+        <AdminSideMenu user={user} selectedCustomer={selectedCustomer} setVisiblePanel={setVisiblePanel} />
       </Router>,
     );
 
