7c4d268b483437618cb7c518eeae07c83edb3048
Reignable <joebarrett401@gmail.com>
2020-06-16 15:15:41 +0100

    fix(SidebarItem): Fix tooltip
    IssueID #511

--- client/src/components/sidebar/sidebaritem/sidebaritem.component.js
+++ client/src/components/sidebar/sidebaritem/sidebaritem.component.js
@@ -3,11 +3,14 @@ import styles from './sidebaritem.module.css';
 import ReactTooltip from 'react-tooltip';
 
 export const SidebarItem = ({ children, icon, selected, tooltip, onClick }) => (
-  <li className={`${styles.sidebarItem} ${selected && styles.selected}`} onClick={onClick}>
+  <li
+    className={`${styles.sidebarItem} ${selected && styles.selected}`}
+    onClick={onClick}
+    data-tip
+    data-for={`toolbar-item-${tooltip}-tooltip`}
+  >
     {icon && <div className={styles.icon}>{icon}</div>}
     {children && <div className={styles.label}>{children}</div>}
-    <ReactTooltip id={`toolbar-item-${tooltip}-tooltip`}>
-      <span>{tooltip}</span>
-    </ReactTooltip>
+    {tooltip && <ReactTooltip id={`toolbar-item-${tooltip}-tooltip`}>{tooltip}</ReactTooltip>}
   </li>
 );
