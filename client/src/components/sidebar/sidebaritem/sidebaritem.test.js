74ec03222898efe29d5c8db5d2b1ce153085efa1
Reignable <joebarrett401@gmail.com>
2020-06-16 15:43:07 +0100

    fix(SidebarItem): Add tests for sidebar item
    IssueID #511

---
+++ client/src/components/sidebar/sidebaritem/sidebaritem.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { SidebarItem } from './sidebaritem.component';
+import userEvent from '@testing-library/user-event';
+
+describe.only('SidebarItem', () => {
+  it('Displays an icon if present', () => {
+    const { getByText } = render(<SidebarItem icon={<div>Hello</div>} />);
+    expect(getByText('Hello')).toBeInTheDocument();
+  });
+
+  it('Displays a label if present', () => {
+    const { getByText } = render(<SidebarItem>Hello</SidebarItem>);
+    expect(getByText('Hello')).toBeInTheDocument();
+  });
+
+  it('Calls the onClick function when clicked', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<SidebarItem onClick={onClick}>Hello</SidebarItem>);
+    userEvent.click(getByText('Hello'));
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it('Has the selected style when selected', () => {
+    const { container } = render(<SidebarItem selected />);
+    expect(container.firstChild.classList).toContain('selected');
+  });
+
+  it('has a tooltip if given', () => {
+    const { getByText } = render(<SidebarItem tooltip="test" />);
+    expect(getByText('test')).toBeInTheDocument();
+  });
+});
