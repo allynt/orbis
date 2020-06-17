44a86cd74dfc334a02384cfa1bc994b0edda1610
Reignable <joebarrett401@gmail.com>
2020-06-17 10:11:26 +0100

    fix(UserProfile): Add stories
    IssueID #511

---
+++ client/src/admin/left-sidebar/user-profile/user-profile.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { UserProfile } from './user-profile.component';
+
+export default { title: 'Admin/LeftSidebar/UserProfile', component: UserProfile };
+
+export const NoUser = () => <UserProfile />;
+
+export const NoAvatar = () => <UserProfile name="Henry Dillmond" />;
+
+export const NameAndAvatar = () => (
+  <UserProfile
+    name="Henry Dillmond"
+    avatar="https://pbs.twimg.com/profile_images/1218555839826579458/GL8pjTKD_400x400.jpg"
+  />
+);
