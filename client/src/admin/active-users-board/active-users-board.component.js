eb1d437e24ec83e388b13bc4f2edcc74cb2fee16
Reignable <joebarrett401@gmail.com>
2020-06-19 12:19:05 +0100

    feat(ActiveUsersBoard): Style active users board
    IssueID #512

--- client/src/admin/active-users-board/active-users-board.component.js
+++ client/src/admin/active-users-board/active-users-board.component.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import tableStyles from '../table.module.css';
 
 export const ActiveUsersBoard = ({ activeUsers }) => (
-  <table>
-    <thead>
-      <th>User</th>
-      <th>Activated Licences</th>
-      <th>Email</th>
+  <table className={tableStyles.table}>
+    <thead className={tableStyles.thead}>
+      <tr className={tableStyles.tr}>
+        <th align="left" className={tableStyles.th}>
+          User
+        </th>
+        <th align="left" className={tableStyles.th}>
+          Activated Licences
+        </th>
+        <th align="left" className={tableStyles.th}>
+          Email
+        </th>
+      </tr>
     </thead>
     <tbody>
-      {activeUsers && activeUsers.length
-        ? activeUsers.map(user => (
-            <tr>
-              <td>{user.name}</td>
-              <td>{user.licences?.sort().join(', ')}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))
-        : 'No Active Users'}
+      {activeUsers && activeUsers.length ? (
+        activeUsers.map(user => (
+          <tr className={tableStyles.tr}>
+            <td className={tableStyles.td}>{user.name}</td>
+            <td className={tableStyles.td}>{user.licences?.sort().join(', ')}</td>
+            <td className={tableStyles.td}>{user.email}</td>
+          </tr>
+        ))
+      ) : (
+        <tr className={tableStyles.tr}>
+          <td align="center" colSpan={3} className={tableStyles.td}>
+            No Active Users
+          </td>
+        </tr>
+      )}
     </tbody>
   </table>
 );
