8682d048a8d409cdd8cc54a57aa58959e00916fe
Reignable <joebarrett401@gmail.com>
2020-07-07 11:50:44 +0100

    feat(Map): Icon layer working
    IssueID #563

--- client/src/map/map.component.js
+++ client/src/map/map.component.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { LayersIcon, Button, LoadMask } from '@astrosat/astrosat-ui/';
 
 import DeckGL from '@deck.gl/react';
+import { IconLayer } from '@deck.gl/layers';
 import { useDispatch, useSelector } from 'react-redux';
 import { StaticMap } from 'react-map-gl';
 
@@ -20,6 +21,10 @@ import {
 import { mapboxTokenSelector, mapStylesSelector } from 'app.slice';
 
 import styles from './map.module.css';
+import { selectActiveLayers } from 'data-layers/data-layers.slice';
+
+import iconAtlas from './layers/hourglass/infrastructure/iconAtlas.png';
+import iconMapping from './layers/hourglass/infrastructure/iconMapping.json';
 
 const Map = () => {
   const dispatch = useDispatch();
@@ -32,10 +37,39 @@ const Map = () => {
   const [isMapStyleSwitcherVisible, setIsMapStyleSwitcherVisible] = useState(
     false,
   );
+  const selectedLayers = useSelector(selectActiveLayers);
+  const [layers, setLayers] = useState([]);
 
   useEffect(() => {
     dispatch(isLoaded());
   }, [selectedBookmark, dispatch]);
+
+  useEffect(() => {
+    const newLayers = layers.filter(
+      layer =>
+        !!selectedLayers.find(selectedLayer => selectedLayer.name === layer.id),
+    );
+    const infraLayer = selectedLayers.find(
+      layer => layer.name === 'health-infrastructure',
+    );
+    if (infraLayer) {
+      newLayers.push(
+        new IconLayer({
+          id: infraLayer.name,
+          data: infraLayer.data.features,
+          iconMapping,
+          iconAtlas,
+          getIcon: d => d.properties.type,
+          getPosition: d => d.geometry.coordinates,
+          getSize: 60,
+        }),
+      );
+    }
+    setLayers(newLayers);
+  }, [selectedLayers, layers]);
+
+  console.log(layers[0]?.id);
+
   return (
     <>
       {bookmarksLoading && (
@@ -43,7 +77,7 @@ const Map = () => {
           <LoadMask />
         </div>
       )}
-      <DeckGL controller initialViewState={viewport}>
+      <DeckGL controller initialViewState={viewport} layers={layers}>
         <StaticMap
           reuseMap
           mapboxApiAccessToken={accessToken}
