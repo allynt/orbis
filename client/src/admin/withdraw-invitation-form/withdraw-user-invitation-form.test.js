545481d6d72173bf96ecb632bcce76eb1470ee22
Rory MacGregor <rorymacgregor@astrosat.net>
2020-07-17 15:46:07 +0100

    fix(frontend): Fix other tests that I missed
    I updated these tests in the exact same way that I did the tests in the previous commit, I just missed these ones.

--- client/src/admin/withdraw-invitation-form/withdraw-user-invitation-form.test.js
+++ client/src/admin/withdraw-invitation-form/withdraw-user-invitation-form.test.js
@@ -5,6 +5,15 @@ import userEvent from '@testing-library/user-event';
 
 import { WithdrawUserInvitationForm } from './withdraw-user-invitation-form.component';
 
+const renderComponent = (user, withdrawInvitation, close) =>
+  render(
+    <WithdrawUserInvitationForm
+      user={user}
+      withdrawInvitation={withdrawInvitation}
+      close={close}
+    />,
+  );
+
 describe('WithdrawUserInvitationForm', () => {
   let user = null;
   let withdrawInvitation = null;
@@ -17,37 +26,21 @@ describe('WithdrawUserInvitationForm', () => {
   });
 
   it('displays name of user to be withdrawn in message text', () => {
-    const { getByText } = render(
-      <WithdrawUserInvitationForm
-        user={user}
-        withdrawInvitation={withdrawInvitation}
-        close={close}
-      />,
-    );
+    const { getByText } = renderComponent(user, withdrawInvitation, close);
 
     expect(getByText(user.user.name)).toBeInTheDocument();
   });
 
   it('closes when `cancel` button is clicked', () => {
-    const { getByText } = render(
-      <WithdrawUserInvitationForm
-        user={user}
-        withdrawInvitation={withdrawInvitation}
-        close={close}
-      />,
-    );
+    const { getByText } = renderComponent(user, withdrawInvitation, close);
+
     userEvent.click(getByText('Cancel'));
     expect(close).toHaveBeenCalled();
   });
 
   it('calls dispatch function with user and closes the dialog when `Yes` button is clicked', () => {
-    const { getByText } = render(
-      <WithdrawUserInvitationForm
-        user={user}
-        withdrawInvitation={withdrawInvitation}
-        close={close}
-      />,
-    );
+    const { getByText } = renderComponent(user, withdrawInvitation, close);
+
     userEvent.click(getByText('Yes'));
     expect(withdrawInvitation).toHaveBeenCalledWith(user);
     expect(close).toHaveBeenCalled();
