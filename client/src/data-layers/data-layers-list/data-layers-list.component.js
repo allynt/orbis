8176faa474353e7e4bf98bba6b6c90be650da1ee
Reignable <joebarrett401@gmail.com>
2020-07-27 12:00:09 +0100

    fix(DataLayers): Break out DataLayersList component
    IssueID #597

---
+++ client/src/data-layers/data-layers-list/data-layers-list.component.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import { Detail, Slider, Button } from '@astrosat/astrosat-ui';
+
+import { PopulationInformation } from '../population-information.component';
+import { HealthInfrastructure } from '../health-infrastructure.component';
+
+import { ReactComponent as RemoveIcon } from '../remove.svg';
+import { ReactComponent as HideIcon } from '../layer-invisible.svg';
+
+import styles from '../data-layers.module.css';
+import { useDispatch } from 'react-redux';
+import { removeLayer } from 'data-layers/data-layers.slice';
+
+const DefaultComponent = ({ selectedLayer, dispatch }) => (
+  <div>
+    {selectedLayer.metadata.range && (
+      <Slider
+        min={0}
+        max={10}
+        values={[1, 7]}
+        onChange={() => console.log('Slider changed')}
+      />
+    )}
+
+    <div className={styles.buttons}>
+      <Button
+        theme="primary"
+        classNames={[styles.button]}
+        onClick={() => console.log('Toggle Show/Hide Layer')}
+      >
+        <HideIcon className={styles.icon} />
+      </Button>
+      <Button
+        theme="primary"
+        classNames={[styles.button]}
+        onClick={() => dispatch(removeLayer(selectedLayer))}
+      >
+        <RemoveIcon className={styles.icon} />
+      </Button>
+    </div>
+  </div>
+);
+
+const detailComponentMap = {
+  hourglass: PopulationInformation,
+  infrastructure: HealthInfrastructure,
+  default: DefaultComponent,
+};
+
+export const DataLayersList = ({ selectedLayers }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.layers}>
+      {selectedLayers.map(selectedLayer => {
+        // We need to have structure to our layer naming for this to work,
+        // but if we prepend each layer with it's type, e.g `scotish-infrastructure` becomes `infrastructure` and `people`,
+        // remains `people`.
+        const [layerType] = selectedLayer.name.split('-').slice(-1);
+        const Component =
+          detailComponentMap[layerType ? layerType : selectedLayer.name] ??
+          detailComponentMap['default'];
+
+        return (
+          <Detail
+            key={selectedLayer.source_id}
+            title={selectedLayer.metadata.label}
+          >
+            <div className={styles.detailContent}>
+              <Component selectedLayer={selectedLayer} dispatch={dispatch} />
+            </div>
+          </Detail>
+        );
+      })}
+    </div>
+  );
+};
--- client/src/data-layers/data-layers.component.js
+++ client/src/data-layers/data-layers.component.js
@@ -3,12 +3,8 @@ import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Button from '@astrosat/astrosat-ui/dist/buttons/button';
-import Detail from '@astrosat/astrosat-ui/dist/containers/detail';
-import Slider from '@astrosat/astrosat-ui/dist/forms/slider';
 import useModal from '@astrosat/astrosat-ui/dist/containers/use-modal';
 
-import { ReactComponent as RemoveIcon } from './remove.svg';
-import { ReactComponent as HideIcon } from './layer-invisible.svg';
 import { ReactComponent as AddNewCategoryIcon } from './add-more-categories.svg';
 
 import {
@@ -18,47 +14,10 @@ import {
   activeDataSourcesSelector,
 } from './data-layers.slice';
 
-import { PopulationInformation } from './population-information.component';
-import { HealthInfrastructure } from './health-infrastructure.component';
 import DataLayersDialog from './data-layers-dialog/data-layers-dialog.component';
 
 import styles from './data-layers.module.css';
-
-const DefaultComponent = ({ selectedLayer, dispatch }) => (
-  <div>
-    {selectedLayer.metadata.range && (
-      <Slider
-        min={0}
-        max={10}
-        values={[1, 7]}
-        onChange={() => console.log('Slider changed')}
-      />
-    )}
-
-    <div className={styles.buttons}>
-      <Button
-        theme="primary"
-        classNames={[styles.button]}
-        onClick={() => console.log('Toggle Show/Hide Layer')}
-      >
-        <HideIcon className={styles.icon} />
-      </Button>
-      <Button
-        theme="primary"
-        classNames={[styles.button]}
-        onClick={() => dispatch(removeLayer(selectedLayer))}
-      >
-        <RemoveIcon className={styles.icon} />
-      </Button>
-    </div>
-  </div>
-);
-
-const detailComponentMap = {
-  hourglass: PopulationInformation,
-  infrastructure: HealthInfrastructure,
-  default: DefaultComponent,
-};
+import { DataLayersList } from './data-layers-list/data-layers-list.component';
 
 const DataLayers = () => {
   const [isVisible, toggle] = useModal(false);
@@ -81,32 +40,7 @@ const DataLayers = () => {
   return (
     <>
       <div className={styles.selectData} ref={ref}>
-        <div className={styles.layers}>
-          {selectedLayers.map(selectedLayer => {
-            // We need to have structure to our layer naming for this to work,
-            // but if we prepend each layer with it's type, e.g `scotish-infrastructure` becomes `infrastructure` and `people`,
-            // remains `people`.
-            const [layerType] = selectedLayer.name.split('-').slice(-1);
-            const Component =
-              detailComponentMap[layerType ? layerType : selectedLayer.name] ??
-              detailComponentMap['default'];
-
-            return (
-              <Detail
-                key={selectedLayer.source_id}
-                title={selectedLayer.metadata.label}
-              >
-                <div className={styles.detailContent}>
-                  <Component
-                    selectedLayer={selectedLayer}
-                    dispatch={dispatch}
-                  />
-                </div>
-              </Detail>
-            );
-          })}
-        </div>
-
+        <DataLayersList selectedLayers={selectedLayers} />
         <div className={styles.buttons}>
           <AddNewCategoryIcon
             className={styles.addNewCategoryIcon}
