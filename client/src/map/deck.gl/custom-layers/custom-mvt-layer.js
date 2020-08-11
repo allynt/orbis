5d883d407b357f7f6b327108aec2a9252327a635
Reignable <joebarrett401@gmail.com>
2020-08-11 13:37:23 +0100

    fix(CustomMVTLayer): Add comments

--- client/src/map/deck.gl/custom-layers/custom-mvt-layer.js
+++ client/src/map/deck.gl/custom-layers/custom-mvt-layer.js
@@ -3,6 +3,26 @@ import { MVTLoader } from '@loaders.gl/mvt';
 import { MVTLayer } from 'deck.gl';
 import { gunzipSync } from 'zlib';
 
+/**
+ * @typedef Tile
+ * @property {number} x x index of the tile
+ * @property {number} y y index of the tile
+ * @property {number} z z index of the tile
+ * @property {string} url resolved url of the tile if the data prop is provided, otherwise null
+ * @property {{ west: number, north: number, east: number, south: number } |
+ *            { left: number, top: number, bottom: number, right: number }
+ * } bbox bounding box of the tile. When used with a geospatial view, bbox is in the shape of {west: <longitude>, north: <latitude>, east: <longitude>, south: <latitude>}.
+ *        When used with a non-geospatial view, bbox is in the shape of {left, top, right, bottom}
+ */
+
+/**
+ * Taken from https://github.com/visgl/deck.gl/blob/2b15bc459c6534ea38ce1153f254ce0901f51d6f/modules/geo-layers/src/tile-layer/utils.js#L32 under MIT Licence
+ *
+ * Converts a tile server URL template to a complete URL using properties object.
+ *
+ * @param {string | string[]} template A URL template for a tile server including "/{x}/{y}/{z}/" substring
+ * @param {Tile} properties - the tile properties to create a URL from
+ */
 function getURLFromTemplate(template, properties) {
   if (!template || !template.length) {
     return null;
