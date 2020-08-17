197cd82e62a573c0e7646d9ba08ee610a96f0564
Reignable <joebarrett401@gmail.com>
2020-08-17 14:58:26 +0100

    chore(frontend): Add constant for value type
    IssueID #636

--- client/src/components/feature-detail/feature-detail.component.js
+++ client/src/components/feature-detail/feature-detail.component.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import { DEFAULT_TITLE } from './feature-detail.constants';
+import { DEFAULT_TITLE, VALUE_TYPE } from './feature-detail.constants';
 
 import styles from './feature-detail.module.css';
 
 const NO_DATA = 'Not available';
 
 /**
+ * @typedef {import('./feature-detail.constants').ValueType} ValueType
+ */
+/**
  * @param {*} value
- * @returns {'array' | 'object' | 'item'}
+ * @returns {ValueType[keyof ValueType]}
  */
 const getTypeForValue = value => {
-  if (Array.isArray(value)) return 'array';
-  if (typeof value === 'object' && value !== null) return 'object';
-  return 'item';
+  if (Array.isArray(value)) return VALUE_TYPE.array;
+  if (typeof value === 'object' && value !== null) return VALUE_TYPE.object;
+  return VALUE_TYPE.item;
 };
 
 /**
@@ -70,11 +73,11 @@ const ArrayItem = ({ jsonKey, value }) => (
       {value.length > 0 ? (
         value.map((item, i) => {
           switch (getTypeForValue(item)) {
-            case 'array':
+            case VALUE_TYPE.array:
               return <ArrayItem key={`${jsonKey}-${i}`} value={item} />;
-            case 'object':
+            case VALUE_TYPE.object:
               return <ObjectItem key={`${jsonKey}-${i}`} value={item} />;
-            case 'item':
+            case VALUE_TYPE.item:
             default:
               return (
                 <li key={i} className={`${styles.value} ${styles.listItem}`}>
@@ -95,7 +98,7 @@ const mapObject = feature => {
     feature &&
     Object.entries(feature).map(([jsonKey, value], i) => {
       switch (getTypeForValue(value)) {
-        case 'array':
+        case VALUE_TYPE.array:
           return (
             <ArrayItem
               key={`${jsonKey}-${i}`}
@@ -103,7 +106,7 @@ const mapObject = feature => {
               value={value}
             />
           );
-        case 'object':
+        case VALUE_TYPE.object:
           return (
             <ObjectItem
               key={`${jsonKey}-${i}`}
@@ -111,7 +114,7 @@ const mapObject = feature => {
               value={value}
             />
           );
-        case 'item':
+        case VALUE_TYPE.item:
         default:
           return (
             <Item key={`${jsonKey}-${i}`} jsonKey={jsonKey} value={value} />
--- client/src/components/feature-detail/feature-detail.constants.js
+++ client/src/components/feature-detail/feature-detail.constants.js
@@ -1,1 +1,16 @@
 export const DEFAULT_TITLE = 'Feature Details';
+
+/**
+ * @typedef {{
+ *   object: 'object'
+ *   array: 'array'
+ *   item: 'item'
+ * }} ValueType
+ */
+
+/** @type {ValueType} */
+export const VALUE_TYPE = {
+  object: 'object',
+  array: 'array',
+  item: 'item',
+};
