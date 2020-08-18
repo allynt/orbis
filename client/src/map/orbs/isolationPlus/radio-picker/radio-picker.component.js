38f55a27693df4a01899897279a3068feaa45247
Reignable <joebarrett401@gmail.com>
2020-08-18 11:58:09 +0100

    fix(frontend): Add info button logic and elements
    IssueID #637

--- client/src/map/orbs/isolationPlus/radio-picker/radio-picker.component.js
+++ client/src/map/orbs/isolationPlus/radio-picker/radio-picker.component.js
@@ -1,5 +1,6 @@
-import React from 'react';
 import { Radio } from '@astrosat/astrosat-ui';
+import InfoIcon from '@astrosat/astrosat-ui/dist/icons/info-icon';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { propertySelector, setProperty } from '../isolation-plus.slice';
 
@@ -7,27 +8,37 @@ export const RadioPicker = ({ selectedLayer, dispatch }) => {
   const selectedProperty = useSelector(state =>
     propertySelector(state, selectedLayer.source_id),
   );
+  /** @type {[string | undefined, React.Dispatch<string | undefined>]} */
+  const [visibleInfoProperty, setVisibleInfoProperty] = useState();
 
   return (
     <>
       {Object.keys(selectedLayer?.metadata?.properties)
         .filter(p => !p.includes('LSOA'))
         .map(property => (
-          <Radio
-            key={property}
-            label={property}
-            name={selectedLayer?.source_id}
-            value={property}
-            checked={property === selectedProperty}
-            onChange={e =>
-              dispatch(
-                setProperty({
-                  source_id: selectedLayer.source_id,
-                  property,
-                }),
-              )
-            }
-          />
+          <>
+            <Radio
+              key={property}
+              label={property}
+              name={selectedLayer?.source_id}
+              value={property}
+              checked={property === selectedProperty}
+              onChange={() =>
+                dispatch(
+                  setProperty({
+                    source_id: selectedLayer.source_id,
+                    property,
+                  }),
+                )
+              }
+            />
+            <button onClick={() => setVisibleInfoProperty(property)}>
+              <InfoIcon title={property} />
+            </button>
+            {visibleInfoProperty === property && (
+              <p>{selectedLayer.metadata.properties[property].description}</p>
+            )}
+          </>
         ))}
     </>
   );
