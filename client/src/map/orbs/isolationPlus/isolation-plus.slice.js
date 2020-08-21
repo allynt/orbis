79fffabe33f9bb33860a735441ffebe5dd1ca8fa
Reignable <joebarrett401@gmail.com>
2020-08-21 12:45:47 +0100

    fix(frontend): Update isolationPlus slice to use one property
    Only one property is supposed to be selected at a time
    This updates the redux slice to store that
    
    IssueID #667

--- client/src/map/orbs/isolationPlus/isolation-plus.slice.js
+++ client/src/map/orbs/isolationPlus/isolation-plus.slice.js
@@ -15,12 +15,17 @@ const isolationPlusSlice = createSlice({
       [LAYER_IDS.astrosat.isolationPlus.localFuelPoverty.v1]: 'Oranges',
       [LAYER_IDS.astrosat.isolationPlus.mobileConnectivity.v1]: 'PuRd',
     },
-    properties: {},
+    property: {
+      source_id: undefined,
+      name: undefined,
+    },
   },
   reducers: {
     setProperty: (state, { payload }) => {
-      if (payload.source_id && payload.property)
-        state.properties[payload.source_id] = payload.property;
+      if (payload.source_id && payload.name) {
+        state.property.source_id = payload.source_id;
+        state.property.name = payload.name;
+      }
     },
   },
 });
@@ -43,15 +48,9 @@ export const colorSchemeSelector = createSelector(
   (orb, source_id) => orb?.colorSchemes?.[source_id],
 );
 
-export const propertiesSelector = createSelector(
-  baseSelector,
-  orb => orb?.properties,
-);
-
 export const propertySelector = createSelector(
   baseSelector,
-  (_, source_id) => source_id,
-  (orb, source_id) => orb?.properties?.[source_id],
+  orb => orb?.property,
 );
 
 export default isolationPlusSlice.reducer;
