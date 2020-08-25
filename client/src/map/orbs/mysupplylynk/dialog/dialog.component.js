819f78e748a8182deeb39768fa7c3c44a2543c3c
Reignable <joebarrett401@gmail.com>
2020-08-25 15:12:48 +0100

    chore(frontend): Add more copy
    IssueID #680

--- client/src/map/orbs/mysupplylynk/dialog/dialog.component.js
+++ client/src/map/orbs/mysupplylynk/dialog/dialog.component.js
@@ -6,10 +6,14 @@ import { Button, CloseButton } from '@astrosat/astrosat-ui';
 const COPY = {
   businessInformationHeading: 'Business Information',
   itemsHeading: 'What can we supply?',
+  contactDetailsLabel: 'Contact Details',
+  addressLabel: 'Address',
+  existingLineOfBusinessLabel: 'Existing line of business',
+  newProductLinesLabel: 'New Product Lines in Response to COVID-19',
+  paymentTermsLabel: 'Payment Terms',
 };
 
 /**
- *
  * @param {{
  *   supplier?: any
  *   onCloseClick: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -23,7 +27,7 @@ export const Dialog = ({ supplier, onCloseClick }) => (
     </header>
     <div className={styles.content}>
       <div className={styles.label}>
-        <h3>Contact Details</h3>
+        <h3>{COPY.contactDetailsLabel}</h3>
       </div>
       <ul className={styles.item}>
         <li className={styles.listItem}>{supplier['Contact Name']}</li>
@@ -41,7 +45,7 @@ export const Dialog = ({ supplier, onCloseClick }) => (
         </li>
       </ul>
       <div className={styles.label}>
-        <h3>Address</h3>
+        <h3>{COPY.addressLabel}</h3>
       </div>
       <ul className={styles.item}>
         <li className={styles.listItem}>{supplier['Address Line 1']}</li>
@@ -52,17 +56,17 @@ export const Dialog = ({ supplier, onCloseClick }) => (
       </ul>
       <h2 className={styles.heading}>{COPY.businessInformationHeading}</h2>
       <div className={styles.label}>
-        <h3>Existing line of business</h3>
+        <h3>{COPY.existingLineOfBusinessLabel}</h3>
       </div>
       <p className={styles.item}>{supplier['Line of Business']}</p>
       <div className={`${styles.label} ${styles.highlight}`}>
-        <h3>New Product Lines in Response to COVID-19</h3>
+        <h3>{COPY.newProductLinesLabel}</h3>
       </div>
       <p className={`${styles.item} ${styles.highlight}`}>
         {supplier['New Product Lines']}
       </p>
       <div className={styles.label}>
-        <h3>Payment Terms</h3>
+        <h3>{COPY.paymentTermsLabel}</h3>
       </div>
       <p className={styles.item}>{supplier['Payment Terms']}</p>
       <h2 className={styles.heading}>{COPY.itemsHeading}</h2>
