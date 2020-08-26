a8b96a4e37626c2a998a882f28192f65adf2412b
Reignable <joebarrett401@gmail.com>
2020-08-26 08:27:56 +0100

    feat(frontend): Style and add functionality to checkboxes
    IssueID #681

--- client/src/map/orbs/mysupplylynk/checkbox-filters/checkbox-filters.component.js
+++ client/src/map/orbs/mysupplylynk/checkbox-filters/checkbox-filters.component.js
@@ -2,6 +2,8 @@ import React from 'react';
 import * as Icons from './icons';
 import { Checkbox } from '@astrosat/astrosat-ui';
 
+import styles from './checkbox-filters.module.css';
+
 /** @type {{
  *   Icon: React.FunctionComponent,
  *   name: string
@@ -25,20 +27,39 @@ const CATEGORIES = [
   { name: 'Miscellaneous', Icon: Icons.Other },
 ];
 
-export const CheckboxFilters = () => (
-  <>
-    {CATEGORIES.map(({ name, Icon }) => (
-      <Checkbox
-        key={name}
-        id={name}
-        defaultChecked
-        label={
-          <span>
-            <Icon title={name} />
-            {name}
-          </span>
-        }
-      />
-    ))}
-  </>
-);
+export const CheckboxFilters = () => {
+  /**
+   * @param {React.ChangeEvent<HTMLInputElement>} event
+   */
+  const handleChange = event => {
+    // This tells you the value of the checkbox which has changed
+    // and whether it is now checked or not.
+    // Will be enough to maintain a list of items to filter by in state.
+    // The initial state should be an array of all the categories
+    console.log(event.target.value, event.target.checked);
+  };
+
+  return (
+    <>
+      {CATEGORIES.map(({ name, Icon }) => (
+        <Checkbox
+          key={name}
+          id={name}
+          className={styles.checkbox}
+          defaultChecked
+          name="msl-filter-checkbox"
+          value={name}
+          onChange={handleChange}
+          label={
+            <span className={styles.label}>
+              <div className={styles.iconWrapper}>
+                <Icon className={styles.icon} title={name} />
+              </div>
+              {name}
+            </span>
+          }
+        />
+      ))}
+    </>
+  );
+};
