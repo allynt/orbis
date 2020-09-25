61fad4c923471b2e1abccb045872bd1320a8c60f
Reignable <joebarrett401@gmail.com>
2020-09-25 11:58:23 +0100

    fix(frontend): Combine switcher handler functions
    IssueID #776

--- client/src/map-style/map-style-switcher/map-style-switcher.component.js
+++ client/src/map-style/map-style-switcher/map-style-switcher.component.js
@@ -10,35 +10,39 @@ import { toTitleCase } from 'utils/text';
  *   selectMapStyle?: (newStyle: import('map-style/styles').MapStyleKey) => void
  * }} props
  */
-const MapStyleSwitcher = ({ mapStyles, selectedMapStyle, selectMapStyle }) => (
-  <ul className={style['mapstyle-switcher-container']}>
-    {Object.entries(mapStyles).map(([styleKey, { img, webp }]) => (
-      <li
-        key={styleKey}
-        className={`${style.item} ${
-          styleKey === selectedMapStyle ? style.checked : ''
-        }`}
-      >
-        <label className={style.label}>
-          <input
-            className={style.input}
-            name="mapStyle"
-            type="radio"
-            tabIndex={0}
-            onChange={() => selectMapStyle(styleKey)}
-            onKeyUp={() => selectMapStyle(styleKey)}
-            value={styleKey}
-            checked={styleKey === selectedMapStyle}
-          />
-          <picture>
-            <source srcSet={webp} type="image/webp" />
-            <img className={style.picture} src={img} alt="Preview" />
-          </picture>
-          <span className={style.text}>{toTitleCase(styleKey)}</span>
-        </label>
-      </li>
-    ))}
-  </ul>
-);
+const MapStyleSwitcher = ({ mapStyles, selectedMapStyle, selectMapStyle }) => {
+  const handleInputChange = styleKey => () => selectMapStyle(styleKey);
+
+  return (
+    <ul className={style['mapstyle-switcher-container']}>
+      {Object.entries(mapStyles).map(([styleKey, { img, webp }]) => (
+        <li
+          key={styleKey}
+          className={`${style.item} ${
+            styleKey === selectedMapStyle ? style.checked : ''
+          }`}
+        >
+          <label className={style.label}>
+            <input
+              className={style.input}
+              name="mapStyle"
+              type="radio"
+              tabIndex={0}
+              onChange={handleInputChange(styleKey)}
+              onKeyUp={handleInputChange(styleKey)}
+              value={styleKey}
+              checked={styleKey === selectedMapStyle}
+            />
+            <picture>
+              <source srcSet={webp} type="image/webp" />
+              <img className={style.picture} src={img} alt="Preview" />
+            </picture>
+            <span className={style.text}>{toTitleCase(styleKey)}</span>
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default MapStyleSwitcher;
