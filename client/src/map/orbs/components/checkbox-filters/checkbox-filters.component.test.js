8e5d6e26ae7e2c85c40f16a0ad1f2a9c041bf4dc
Reignable <joebarrett401@gmail.com>
2020-10-06 10:41:55 +0100

    chore(frontend): Remove skipped tests

--- client/src/map/orbs/components/checkbox-filters/checkbox-filters.component.test.js
+++ client/src/map/orbs/components/checkbox-filters/checkbox-filters.component.test.js
@@ -32,34 +32,4 @@ describe('Checkbox Filters', () => {
       expect(getByLabelText(cat)).toHaveAttribute('checked');
     });
   });
-
-  it.skip('calls `setSelectedFeatures` function with updated categories when checkboxes are checked/unchecked', () => {
-    const filteredCategories = CATEGORIES.filter(cat => cat !== 'PPE');
-
-    const { getByText } = render(<CheckboxFilters />, {
-      wrapper,
-    });
-
-    userEvent.click(getByText('PPE'));
-
-    // expect(setSelectedFeatures).toHaveBeenCalledWith(filteredCategories);
-  });
-
-  it.skip('only checks checkboxes where of categories that are present in `selectedCategories`', () => {
-    const testCategories = ['PPE', 'Other'];
-
-    const { getByLabelText } = render(<CheckboxFilters />, {
-      wrapper,
-    });
-
-    testCategories.forEach(cat => {
-      expect(getByLabelText(cat)).toHaveAttribute('checked');
-    });
-
-    CATEGORIES.forEach(cat => {
-      if (!testCategories.includes(cat)) {
-        expect(getByLabelText(cat)).not.toHaveAttribute('checked');
-      }
-    });
-  });
 });
