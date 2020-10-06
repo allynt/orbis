69b53cf719a5debade2d4d337a8076e786732827
Reignable <joebarrett401@gmail.com>
2020-10-06 10:40:20 +0100

    chore(frontend): Cleanup LayersList
    Remove unnecessary return
    Change typing to global Source type to avoid conflict

--- client/src/data-layers/layers-list/layers-list.component.js
+++ client/src/data-layers/layers-list/layers-list.component.js
@@ -3,39 +3,31 @@ import React from 'react';
 import { LayersListItem } from './layers-list-item/layers-list-item.component';
 
 /**
- * @typedef Layer
- * @property {string} name
- * @property {string} source_id
- * @property {{ label: string, sidebar_component: string }} metadata
- */
-/**
  * @param {{
  *   dispatch: import('redux').Dispatch
- *   selectedLayers: Layer[]
+ *   selectedLayers: Source[]
  *   sidebarComponents: {[key: string]: React.LazyExoticComponent<React.ComponentType<any>>}
  * }} props
  */
-export const LayersList = ({ dispatch, selectedLayers, sidebarComponents }) => {
-  return (
-    <>
-      {selectedLayers?.map(selectedLayer => {
-        const Component = sidebarComponents?.[selectedLayer.source_id];
+export const LayersList = ({ dispatch, selectedLayers, sidebarComponents }) => (
+  <>
+    {selectedLayers?.map(selectedLayer => {
+      const Component = sidebarComponents?.[selectedLayer.source_id];
 
-        return (
-          <LayersListItem
-            key={selectedLayer.source_id}
-            title={selectedLayer.metadata.label}
-          >
-            <React.Suspense fallback={<div>Loading...</div>}>
-              {typeof Component === 'function' ? (
-                <Component selectedLayer={selectedLayer} dispatch={dispatch} />
-              ) : (
-                Component
-              )}
-            </React.Suspense>
-          </LayersListItem>
-        );
-      })}
-    </>
-  );
-};
+      return (
+        <LayersListItem
+          key={selectedLayer.source_id}
+          title={selectedLayer.metadata.label}
+        >
+          <React.Suspense fallback={<div>Loading...</div>}>
+            {typeof Component === 'function' ? (
+              <Component selectedLayer={selectedLayer} dispatch={dispatch} />
+            ) : (
+              Component
+            )}
+          </React.Suspense>
+        </LayersListItem>
+      );
+    })}
+  </>
+);
