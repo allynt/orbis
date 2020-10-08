670aa9b0ee955ed6643a65563d15bf6397563438
Reignable <joebarrett401@gmail.com>
2020-10-08 08:08:57 +0100

    chore(frontend): Add more email tests
    IssueID #785

--- client/src/utils/validators/validators.test.js
+++ client/src/utils/validators/validators.test.js
@@ -8,14 +8,29 @@ describe('field validators', () => {
         'test@test.com',
       );
     });
+
     it('is required', async () => {
       await expect(email.validate('')).rejects.toThrow(MESSAGES.email.required);
     });
 
-    it('is a valid email', async () => {
-      await expect(email.validate('testtestcom')).rejects.toThrow(
-        MESSAGES.email.email,
-      );
+    describe('is a valid email', () => {
+      it('missing @ and .', async () => {
+        await expect(email.validate('testtestcom')).rejects.toThrow(
+          MESSAGES.email.email,
+        );
+      });
+
+      it('missing @', async () => {
+        await expect(email.validate('testtest.com')).rejects.toThrow(
+          MESSAGES.email.email,
+        );
+      });
+
+      it('missing .', async () => {
+        await expect(email.validate('test@testcom')).rejects.toThrow(
+          MESSAGES.email.email,
+        );
+      });
     });
   });
 });
