31afde9964caed000bcb41757de63bc322cd3754
Reignable <joebarrett401@gmail.com>
2020-10-09 09:45:02 +0100

    feat(frontend): Bring in password reset confirm from supplier-app
    IssueID #785

--- client/src/accounts/password/reset/password-reset-confirm-form.component.js
+++ client/src/accounts/password/reset/password-reset-confirm-form.component.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 
-import validate from './password-reset-confirm-form.validator';
-
 import {
   Button,
+  Checkbox,
   PasswordField,
   PasswordStrengthMeter,
-  Checkbox,
-  useForm,
   Well,
 } from '@astrosat/astrosat-ui';
 
-import { status } from 'accounts/accounts.slice';
+import { useForm } from 'react-hook-form';
 
 import { LOGIN_URL, TERMS_URL } from 'accounts/accounts.constants';
+import { status } from 'accounts/accounts.slice';
+import { FieldError } from 'accounts/field-error.component';
 
 import formStyles from 'forms.module.css';
 
@@ -47,25 +46,20 @@ const PasswordResetConfirmForm = ({
 }) => {
   const [termsAgreed, setTermsAgreed] = useState(false);
 
-  const { handleChange, handleSubmit, values, errors } = useForm(
-    onSubmit,
-    validate,
-  );
+  const { register, handleSubmit, getValues, errors } = useForm({
+    mode: 'onBlur',
+  });
 
   if (resetStatus === status.COMPLETE) {
     return <PasswordResetSuccessView />;
   }
 
-  function onSubmit() {
-    const data = {
-      ...values,
-      termsAgreed,
-    };
-    confirmResetPassword(data, match.params);
-  }
+  const onSubmit = data => {
+    confirmResetPassword({ ...data, termsAgreed }, match.params);
+  };
 
   return (
-    <form className={formStyles.form} onSubmit={handleSubmit}>
+    <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
       {error && (
         <Well type="error">
           <ul data-testid="error-well">
@@ -78,33 +72,42 @@ const PasswordResetConfirmForm = ({
 
       <div className={formStyles.fields}>
         <div className={formStyles.row}>
+          <label className={formStyles.hiddenLabel} htmlFor="password2">
+            Password Confirmation
+          </label>
           <PasswordField
+            id="new_password1"
             name="new_password1"
-            value={values.new_password1 || ''}
-            onChange={handleChange}
+            ref={register}
             placeholder="New Password"
             required
             autoFocus
           />
         </div>
         {errors.new_password1 && (
-          <p className={formStyles.errorMessage}>{errors.new_password1}</p>
+          <FieldError message={errors.new_password1.message} />
         )}
 
-        <PasswordStrengthMeter password={values.new_password1} />
-
         <div className={formStyles.row}>
+          <label className={formStyles.hiddenLabel} htmlFor="password2">
+            Password Confirmation
+          </label>
           <PasswordField
+            id="new_password2"
             name="new_password2"
-            value={values.new_password2 || ''}
-            onChange={handleChange}
+            ref={register}
             placeholder="New Password Confirmation"
             required
           />
         </div>
         {errors.new_password2 && (
-          <p className={formStyles.errorMessage}>{errors.new_password2}</p>
+          <FieldError message={errors.new_password2.message} />
         )}
+        {errors.new_password2 && errors.new_password2.type === 'validate' && (
+          <FieldError message="Passwords don't match" />
+        )}
+
+        <PasswordStrengthMeter password={getValues().new_password1} />
 
         <div className={formStyles.row}>
           <Checkbox
@@ -123,11 +126,7 @@ const PasswordResetConfirmForm = ({
       <div className={formStyles.buttons}>
         <Button
           type="submit"
-          disabled={
-            !termsAgreed ||
-            Object.keys(errors).length > 0 ||
-            Object.keys(values).length === 0
-          }
+          disabled={!termsAgreed || Object.keys(errors).length > 0}
         >
           Reset Password
         </Button>
--- client/src/accounts/password/reset/password-reset-confirm-form.component.test.js
+++ client/src/accounts/password/reset/password-reset-confirm-form.component.test.js
@@ -1,42 +1,55 @@
 import React from 'react';
 
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import PasswordResetConfirmForm from './password-reset-confirm-form.component';
 import { status } from 'accounts/accounts.slice';
 
+const mockStore = configureMockStore([thunk]);
+
+const PASSWORD_PLACEHOLDER_TEXT = 'New Password';
+const PASSWORD_CONFIRMATION_PLACEHOLDER_TEXT = 'New Password Confirmation';
+const RESET_BUTTON_TEXT = 'Reset Password';
+const PASSWORD_TEXT = 'newpassword';
+const I_AGREE_TEXT = 'I agree with';
+
 const renderComponent = (
-  history,
+  store,
   confirmResetPassword,
   resetStatus,
   match,
   error,
 ) =>
   render(
-    <Router history={history}>
-      <PasswordResetConfirmForm
-        confirmResetPassword={confirmResetPassword}
-        resetStatus={resetStatus}
-        match={match}
-        error={error}
-      />
-    </Router>,
+    <PasswordResetConfirmForm
+      confirmResetPassword={confirmResetPassword}
+      resetStatus={resetStatus}
+      match={match}
+      error={error}
+    />,
+    {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
+    },
   );
 
 describe('Password Reset Form Component', () => {
-  let history = null;
+  let store;
   let confirmResetPassword = null;
   let resetStatus = null;
   let match = null;
   let error = null;
 
   beforeEach(() => {
-    fetch.resetMocks();
+    store = mockStore({
+      app: { config: { passwordMinLength: 2, passwordMaxLength: 50 } },
+    });
 
-    history = createMemoryHistory({ initialEntries: ['/'] });
     confirmResetPassword = jest.fn();
     resetStatus = status.NONE;
     match = {
@@ -48,131 +61,150 @@ describe('Password Reset Form Component', () => {
     error = null;
   });
 
-  afterEach(cleanup);
-
   it('should render a form', () => {
-    const { container, getByText, getByPlaceholderText } = renderComponent(
-      history,
+    const { getByText, getByPlaceholderText } = renderComponent(
+      store,
       confirmResetPassword,
       resetStatus,
       match,
       error,
     );
 
-    expect(container.querySelector('form')).toBeInTheDocument();
-    expect(getByPlaceholderText('New Password')).toBeInTheDocument();
+    expect(getByPlaceholderText(PASSWORD_PLACEHOLDER_TEXT)).toBeInTheDocument();
     expect(
-      getByPlaceholderText('New Password Confirmation'),
+      getByPlaceholderText(PASSWORD_CONFIRMATION_PLACEHOLDER_TEXT),
     ).toBeInTheDocument();
     // Check form submit button
-    expect(getByText('Reset Password')).toBeInTheDocument();
+    expect(getByText(RESET_BUTTON_TEXT)).toBeInTheDocument();
     // Check Terms and Conditions checkbox
-    expect(getByText('I agree with')).toBeInTheDocument();
+    expect(getByText(I_AGREE_TEXT)).toBeInTheDocument();
     // Check link to login view
     expect(getByText('Login')).toBeInTheDocument();
-    expect(getByText('Reset Password')).toHaveAttribute('disabled');
+    expect(getByText(RESET_BUTTON_TEXT)).toHaveAttribute('disabled');
   });
 
-  it('should disable `Reset` button when form is dirty', async () => {
+  it('should disable `Reset Password` button when form is invalid', () => {
     const { getByText, getByPlaceholderText } = renderComponent(
-      history,
+      store,
       confirmResetPassword,
       resetStatus,
       match,
       error,
     );
 
-    const password = getByPlaceholderText('New Password');
+    const password = getByPlaceholderText(PASSWORD_PLACEHOLDER_TEXT);
     expect(password.value).toEqual('');
-    fireEvent.change(password, { target: { value: 'newpassword' } });
-    expect(password.value).toEqual('newpassword');
-    expect(getByText('Reset Password')).toHaveAttribute('disabled');
+    userEvent.type(password, PASSWORD_TEXT);
+    expect(password.value).toEqual(PASSWORD_TEXT);
+    expect(getByText(RESET_BUTTON_TEXT)).toHaveAttribute('disabled');
   });
 
   it('should enable `Reset Password` button when form is valid', () => {
     const { getByText, getByPlaceholderText } = renderComponent(
-      history,
+      store,
       confirmResetPassword,
       resetStatus,
       match,
       error,
     );
 
-    let password = getByPlaceholderText('New Password');
-    fireEvent.change(password, { target: { value: 'newpassword' } });
-    expect(password.value).toEqual('newpassword');
+    let password = getByPlaceholderText(PASSWORD_PLACEHOLDER_TEXT);
+    userEvent.type(password, PASSWORD_TEXT);
+    expect(password.value).toEqual(PASSWORD_TEXT);
 
-    password = getByPlaceholderText('New Password Confirmation');
-    fireEvent.change(password, { target: { value: 'newpassword' } });
-    expect(password.value).toEqual('newpassword');
-    fireEvent.click(getByText('I agree with'));
+    password = getByPlaceholderText(PASSWORD_CONFIRMATION_PLACEHOLDER_TEXT);
+    userEvent.type(password, PASSWORD_TEXT);
+    expect(password.value).toEqual(PASSWORD_TEXT);
+    waitFor(() => getByText(I_AGREE_TEXT));
 
-    expect(getByText('Reset Password')).not.toHaveAttribute('disabled');
+    expect(getByText(RESET_BUTTON_TEXT)).toHaveAttribute('disabled');
+    userEvent.click(getByText(I_AGREE_TEXT));
+
+    expect(getByText(RESET_BUTTON_TEXT)).not.toHaveAttribute('disabled');
   });
 
-  it('should not call `confirmResetPassword` function when form is invalid and `Reset Password` button clicked', () => {
-    fetch.mockResponse(JSON.stringify({}, { status: 200 }));
-
-    const { getByText } = renderComponent(
-      history,
-      confirmResetPassword,
-      resetStatus,
-      match,
-      error,
-    );
-
-    fireEvent.click(getByText('Reset Password'));
-    expect(fetch.mock.calls.length).toBe(0);
-  });
-
-  it('should call `confirmResetPassword` function and redirect to login when form is valid and `Reset Password` button clicked', () => {
-    fetch.mockResponse(JSON.stringify({}, { status: 200 }));
-
+  it('should not call `confirmResetPassword` function when form is invalid and `Reset Password` button clicked', async () => {
     const { getByText, getByPlaceholderText } = renderComponent(
-      history,
+      store,
       confirmResetPassword,
       resetStatus,
       match,
       error,
     );
 
-    fireEvent.change(getByPlaceholderText('New Password'), {
-      target: { value: 'newpassword' },
-    });
-    fireEvent.change(getByPlaceholderText('New Password Confirmation'), {
-      target: { value: 'newpassword' },
-    });
-    fireEvent.click(getByText('I agree with'));
+    userEvent.type(getByPlaceholderText(PASSWORD_PLACEHOLDER_TEXT), 'te');
 
-    fireEvent.click(getByText('Reset Password'));
+    waitFor(async () => await userEvent.tab());
 
-    const value = undefined;
-    expect(confirmResetPassword).toHaveBeenCalledWith(
-      {
-        new_password1: 'newpassword',
-        new_password2: 'newpassword',
-        termsAgreed: true,
-      },
-      {
-        token: 'Test Token',
-        uid: 'Test UID',
-      },
+    userEvent.click(getByText(RESET_BUTTON_TEXT));
+    expect(confirmResetPassword).not.toHaveBeenCalled();
+  });
+
+  it('should call `confirmResetPassword` function when form is valid and `Reset Password` button clicked', async () => {
+    const { getByRole, getByText, getByPlaceholderText } = renderComponent(
+      store,
+      confirmResetPassword,
+      resetStatus,
+      match,
+      error,
+    );
+
+    userEvent.type(
+      getByPlaceholderText(PASSWORD_PLACEHOLDER_TEXT),
+      PASSWORD_TEXT,
+    );
+    userEvent.type(
+      getByPlaceholderText(PASSWORD_CONFIRMATION_PLACEHOLDER_TEXT),
+      PASSWORD_TEXT,
+    );
+
+    userEvent.click(getByText(I_AGREE_TEXT));
+    userEvent.click(getByRole('button', { name: RESET_BUTTON_TEXT }));
+
+    await waitFor(() =>
+      expect(confirmResetPassword).toHaveBeenCalledWith(
+        {
+          new_password1: PASSWORD_TEXT,
+          new_password2: PASSWORD_TEXT,
+          termsAgreed: true,
+        },
+        { token: 'Test Token', uid: 'Test UID' },
+      ),
     );
   });
 
-  it('should not redirect, but display error well if password reset is unsucessful', () => {
+  it('should display error well if password reset is unsuccessful', () => {
     resetStatus = status.NONE;
     error = ['Test Error 1', 'Test Error 2', 'Test Error 3'];
 
     const { getByTestId } = renderComponent(
-      history,
+      store,
       confirmResetPassword,
       resetStatus,
       match,
       error,
     );
 
-    expect(history.location.pathname).toEqual('/');
     expect(getByTestId('error-well')).toBeInTheDocument();
+  });
+
+  describe('Password Reset Success View', () => {
+    it('should show the Password Reset success view', () => {
+      const { getByText } = renderComponent(
+        store,
+        confirmResetPassword,
+        status.COMPLETE,
+        match,
+        error,
+      );
+
+      expect(
+        getByText(
+          'Your password has successfully been reset. Click the button to continue.',
+        ),
+      ).toBeInTheDocument();
+
+      expect(getByText('Continue')).toBeInTheDocument();
+    });
   });
 });
