fe5b183bf64cf0c537e2ede28a4650e5865a6530
Reignable <joebarrett401@gmail.com>
2020-10-28 13:48:07 +0000

    chore(frontend): Remove unused import
    IssueID #850

--- client/src/map/orbs/rice/useRiceOrb.js
+++ client/src/map/orbs/rice/useRiceOrb.js
@@ -1,4 +1,3 @@
-import { LAYER_IDS } from 'map/map.constants';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { paddiesHealthLayer } from './paddies-health';
