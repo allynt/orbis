422fee9b9316f25ecdcc5719de8245ede38d2064
Reignable <joebarrett401@gmail.com>
2020-11-11 08:37:08 +0000

    feat(frontend): Upgrade ResendEmailVerification view
    IssueID #859

--- client/src/accounts/resend-verification-email/resend-verification-email.component.js
+++ client/src/accounts/resend-verification-email/resend-verification-email.component.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import { Button } from '@astrosat/astrosat-ui';
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  Link,
+  Typography,
+} from '@astrosat/astrosat-ui';
 
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 
 import { LOGIN } from 'accounts/accounts.constants';
-import { LoadingSpinner } from 'components/loading-spinner/loading-spinner.component';
-
-import styles from './resend-verification-email.module.css';
 
 /**
  * @param {{
@@ -17,22 +20,37 @@ import styles from './resend-verification-email.module.css';
  * }} props
  */
 const ResendVerificationEmail = ({ email, isLoading = false, onResend }) => (
-  <div className={styles.wrapper}>
-    <h2 className={styles.heading}>Check your email</h2>
-    <p className={styles.paragraph}>
-      We have created an Astrosat ID for your email
-      {email ? <b> {email}</b> : ''}. You should have received an email
-      containing a validation link which will allow you to create your account.
-    </p>
-    <h2 className={styles.heading}>You haven’t received the email?</h2>
-    <p className={styles.paragraph}>Please check your spam or bulk folders.</p>
-    <Button className={styles.button} onClick={onResend}>
-      {isLoading ? <LoadingSpinner /> : 'Resend email'}
-    </Button>
-    <Link className={styles.button} to={LOGIN}>
-      <Button theme="link">Return to login</Button>
-    </Link>
-  </div>
+  <Grid container spacing={2}>
+    <Grid item xs={12}>
+      <Typography variant="h2" gutterBottom>
+        Check your email
+      </Typography>
+      <Typography paragraph>
+        We have created an Astrosat ID for your email
+        {email ? <b> {email}</b> : ''}. You should have received an email
+        containing a validation link which will allow you to create your
+        account.
+      </Typography>
+      <Typography variant="h2" gutterBottom>
+        You haven’t received the email?
+      </Typography>
+      <Typography paragraph>Please check your spam or bulk folders.</Typography>
+    </Grid>
+    <Grid item xs={12} container justify="center">
+      <Button onClick={onResend}>
+        {isLoading ? (
+          <CircularProgress size={24} color="inherit" />
+        ) : (
+          'Resend email'
+        )}
+      </Button>
+    </Grid>
+    <Grid item xs={12} container justify="center">
+      <RouterLink to={LOGIN} component={Link}>
+        Return to login
+      </RouterLink>
+    </Grid>
+  </Grid>
 );
 
 export default ResendVerificationEmail;
