ea5cb8163a7ef9980d26fcef9e81a8a1e2361e1b
Reignable <joebarrett401@gmail.com>
2020-11-11 08:13:34 +0000

    feat(frontend): Upgrade UpdateUserForm
    IssueID #859

--- client/src/accounts/update-user-form/update-user-form.component.js
+++ client/src/accounts/update-user-form/update-user-form.component.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
-import { Button, Textfield } from '@astrosat/astrosat-ui';
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  makeStyles,
+  Box,
+} from '@astrosat/astrosat-ui';
 
 import { useForm } from 'react-hook-form';
-
-import styles from './update-user-form.module.css';
-import formStyles from '../../forms.module.css';
 
 const EMAIL_FIELD_ID = 'email-field';
 const NAME_FIELD_ID = 'name-field';
 
+const useStyles = makeStyles(theme => ({
+  form: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+/**
+ *
+ * @param {{
+ *  user?: User
+ *  updateUser?: (values: {
+ *    email: string
+ *    name: string
+ * }) => void
+ * }} props
+ */
 const UpdateUserForm = ({ user, updateUser }) => {
+  const styles = useStyles();
   const { handleSubmit, register } = useForm({
     defaultValues: { email: user?.email, name: user?.name },
   });
@@ -20,35 +41,39 @@ const UpdateUserForm = ({ user, updateUser }) => {
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <p className={formStyles.paragraph}>Personal Details</p>
-      <div className={formStyles.row}>
-        <label className={formStyles.hiddenLabel} htmlFor={EMAIL_FIELD_ID}>
-          Email
-        </label>
-        <Textfield
-          id={EMAIL_FIELD_ID}
-          name="email"
-          ref={register}
-          placeholder="Email"
-          readOnly
-        />
-      </div>
-      <div className={formStyles.row}>
-        <label className={formStyles.hiddenLabel} htmlFor={NAME_FIELD_ID}>
-          Name
-        </label>
-        <Textfield
-          id={NAME_FIELD_ID}
-          name="name"
-          ref={register}
-          placeholder="Name"
-        />
-      </div>
-      <Button className={styles.submit} type="submit">
-        Update Account
-      </Button>
-    </form>
+    <Box display="flex" flexDirection="column" height="100%">
+      <Grid
+        className={styles.form}
+        container
+        spacing={2}
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Typography variant="h4" component="h1">
+          Personal Details
+        </Typography>
+        <Grid item xs={12}>
+          <TextField
+            id={EMAIL_FIELD_ID}
+            name="email"
+            inputRef={register}
+            label="Email"
+            InputProps={{ readOnly: true }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id={NAME_FIELD_ID}
+            name="name"
+            inputRef={register}
+            label="Name"
+          />
+        </Grid>
+      </Grid>
+      <Box mt="auto">
+        <Button type="submit">Update Account</Button>
+      </Box>
+    </Box>
   );
 };
 
--- client/src/accounts/update-user-form/update-user-form.test.js
+++ client/src/accounts/update-user-form/update-user-form.test.js
@@ -10,23 +10,22 @@ describe('Update User Form Component', () => {
   it('should render a form', () => {
     const user = {};
     const updateUser = jest.fn();
-    const { container, getByText, getByPlaceholderText } = render(
+    const { getByRole } = render(
       <UpdateUserForm user={user} updateUser={updateUser} />,
     );
 
-    expect(container.querySelector('form')).toBeInTheDocument();
-    expect(getByPlaceholderText('Email')).toBeInTheDocument();
-    expect(getByPlaceholderText('Name')).toBeInTheDocument();
-    expect(getByText('Update Account')).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'Email' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'Name' })).toBeInTheDocument();
+    expect(getByRole('button', { name: 'Update Account' })).toBeInTheDocument();
   });
 
   it('should have `Update User` button enabled', () => {
     const user = {};
     const updateUser = jest.fn();
-    const { getByText } = render(
+    const { getByRole } = render(
       <UpdateUserForm user={user} updateUser={updateUser} />,
     );
 
-    expect(getByText('Update Account')).not.toHaveAttribute('disabled');
+    expect(getByRole('button', { name: 'Update Account' })).not.toBeDisabled();
   });
 });
