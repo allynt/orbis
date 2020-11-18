c413244e6a73bf8627769d82ebcb09843bb3f5fd
Reignable <joebarrett401@gmail.com>
2020-11-18 12:23:11 +0000

    fix(frontend): Style CrowdlessSidebarComponent
    IssueID #878

--- client/src/map/orbs/components/crowdless/sidebar.component.js
+++ client/src/map/orbs/components/crowdless/sidebar.component.js
@@ -2,6 +2,8 @@ import { Button } from '@astrosat/astrosat-ui';
 import { LoadingSpinner } from 'components';
 import * as React from 'react';
 import ResultsListItem from './results-list-item/results-list-item.component';
+import { ReactComponent as SearchIcon } from './search.svg';
+import styles from './sidebar.module.css';
 
 /**
  * @param {{
@@ -16,12 +18,20 @@ export const CrowdlessSidebarComponent = ({
   onFindClick,
 }) => (
   <>
-    <Button onClick={onFindClick}>
+    <p className={styles.text}>
+      Please zoom in to the desired area or add area in the search box{' '}
+      <SearchIcon
+        style={{ width: '1em', height: '1em', color: 'var(--color-primary)' }}
+      />{' '}
+      at the top right of the map in order to get most accurate results. Then
+      click the button “Find Supermarkets” below.
+    </p>
+    <Button className={styles.findButton} size="small" onClick={onFindClick}>
       {isLoading ? <LoadingSpinner /> : 'Find Supermarkets'}
     </Button>
     {((isLoading && !results) || results?.length) && (
-      <ul>
-        Places close to you
+      <ul className={styles.list}>
+        <p className={styles.listHeader}>Places close to you</p>
         {isLoading &&
           !results &&
           Array(10)
