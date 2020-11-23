9b26f3e43dbcb9d7eff0310920a5f4de47b4bdfd
Reignable <joebarrett401@gmail.com>
2020-11-23 15:39:50 +0000

    fix(frontend): Add border to icon when selected
    IssueID #929

--- client/src/map/orbs/configurations/crowdlessConfig.js
+++ client/src/map/orbs/configurations/crowdlessConfig.js
@@ -1,9 +1,14 @@
-import { resultsSelector, setSelectedResult } from '../slices/crowdless.slice';
+import {
+  resultsSelector,
+  selectedResultSelector,
+  setSelectedResult,
+} from '../slices/crowdless.slice';
 import iconAtlas from './crowdlessConfig.iconAtlas.svg';
 import iconMapping from './crowdlessConfig.iconMapping.json';
 
 const configuration = ({ id, orbState, dispatch }) => {
   const results = resultsSelector(orbState);
+  const selectedResult = selectedResultSelector(orbState);
   return {
     id,
     data: results?.features,
@@ -12,10 +17,22 @@ const configuration = ({ id, orbState, dispatch }) => {
     iconAtlas,
     iconMapping,
     /** @param {CrowdlessFeature} feature */
-    getIcon: feature => feature.properties.crowdednessCategory,
-    getSize: 60,
+    getIcon: feature =>
+      `${feature.properties.crowdednessCategory}${
+        feature.properties.placeID === selectedResult?.properties?.placeID
+          ? '-selected'
+          : ''
+      }`,
+    getSize: feature =>
+      feature.properties.placeID === selectedResult?.properties?.placeID
+        ? 60 * 1.4
+        : 60,
     pickable: true,
     onClick: pickedInfo => dispatch(setSelectedResult(pickedInfo.object)),
+    updateTriggers: {
+      getIcon: [selectedResult],
+      getSize: [selectedResult],
+    },
   };
 };
 
