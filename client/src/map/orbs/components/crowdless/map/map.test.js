8835e94332f060d24a23cb460ce62e86ad70c41f
Reignable <joebarrett401@gmail.com>
2020-11-23 11:32:04 +0000

    feat(frontend): Create crowdless map component
    IssueID #929

---
+++ client/src/map/orbs/components/crowdless/map/map.test.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { default as CrowdlessMapComponent } from './map.component';
+
+describe('<CrowdlessMapComponent />', () => {
+  it('Shows the crowdedness score', () => {
+    const crowdednessScore = 7357;
+    const { getByText } = render(
+      <CrowdlessMapComponent feature={{ properties: { crowdednessScore } }} />,
+    );
+    expect(getByText(new RegExp(`${crowdednessScore}`))).toBeInTheDocument();
+  });
+
+  it('Shows the place name', () => {
+    const name = 'Test Name';
+    const { getByText } = render(
+      <CrowdlessMapComponent feature={{ properties: { name } }} />,
+    );
+    expect(getByText(name)).toBeInTheDocument();
+  });
+  it('Shows the place address', () => {
+    const address = '1 Test Street';
+    const { getByText } = render(
+      <CrowdlessMapComponent feature={{ properties: { address } }} />,
+    );
+    expect(getByText(address)).toBeInTheDocument();
+  });
+});
