4c518cad785d598e3530ea322bab9eb9e0faa61d
Reignable <joebarrett401@gmail.com>
2020-12-16 11:19:24 +0000

    chore(frontend): Clean out some of the chart theme
    This can be created as an when it's needed
    
    IssueID #950

--- client/src/components/charts/orbisChartTheme.js
+++ client/src/components/charts/orbisChartTheme.js
@@ -55,49 +55,34 @@ const strokeLinejoin = 'round';
 
 /** @type {import('victory').VictoryThemeDefinition} */
 const theme = {
-  area: assign(
-    {
-      style: {
-        data: {
-          fill: white,
-        },
-        labels: baseLabelStyles,
+  axis: {
+    style: {
+      axis: {
+        fill: 'transparent',
+        stroke: white,
+        strokeWidth: 1,
+        strokeLinecap,
+        strokeLinejoin,
       },
-    },
-    baseProps,
-  ),
-  axis: assign(
-    {
-      style: {
-        axis: {
-          fill: 'transparent',
-          stroke: white,
-          strokeWidth: 1,
-          strokeLinecap,
-          strokeLinejoin,
-        },
-        axisLabel: assign({}, centeredLabelStyles, {
-          fontStyle: 'italic',
-          padding: 35,
-        }),
-        grid: {
-          fill: 'none',
-          stroke: grey,
-          opacity: 0.13,
-          strokeDasharray: 5,
-          pointerEvents: 'painted',
-        },
-        ticks: {
-          fill: 'transparent',
-          size: 1,
-          stroke: 'transparent',
-        },
-        tickLabels: assign(baseLabelStyles, { fill: white }),
+      axisLabel: assign({}, centeredLabelStyles, {
+        fontStyle: 'italic',
+        padding: 35,
+      }),
+      grid: {
+        fill: 'none',
+        stroke: grey,
+        opacity: 0.13,
+        strokeDasharray: 5,
+        pointerEvents: 'painted',
       },
-      fixLabelOverlap: true,
+      ticks: {
+        fill: 'transparent',
+        size: 1,
+        stroke: 'transparent',
+      },
+      tickLabels: assign(baseLabelStyles, { fill: white }),
     },
-    baseProps,
-  ),
+  },
   bar: assign(
     {
       style: {
@@ -114,87 +99,7 @@ const theme = {
     },
     baseProps,
   ),
-  boxplot: assign(
-    {
-      style: {
-        max: { padding: 8, stroke: white, strokeWidth: 1 },
-        maxLabels: assign({}, baseLabelStyles, { padding: 3 }),
-        median: { padding: 8, stroke: white, strokeWidth: 1 },
-        medianLabels: assign({}, baseLabelStyles, { padding: 3 }),
-        min: { padding: 8, stroke: white, strokeWidth: 1 },
-        minLabels: assign({}, baseLabelStyles, { padding: 3 }),
-        q1: { padding: 8, fill: grey },
-        q1Labels: assign({}, baseLabelStyles, { padding: 3 }),
-        q3: { padding: 8, fill: grey },
-        q3Labels: assign({}, baseLabelStyles, { padding: 3 }),
-      },
-      boxWidth: 20,
-    },
-    baseProps,
-  ),
-  candlestick: assign(
-    {
-      style: {
-        data: {
-          stroke: white,
-          strokeWidth: 1,
-        },
-        labels: assign({}, baseLabelStyles, { padding: 5 }),
-      },
-      candleColors: {
-        positive: '#ffffff',
-        negative: white,
-      },
-    },
-    baseProps,
-  ),
   chart: baseProps,
-  errorbar: assign(
-    {
-      borderWidth: 8,
-      style: {
-        data: {
-          fill: 'transparent',
-          stroke: white,
-          strokeWidth: 2,
-        },
-        labels: baseLabelStyles,
-      },
-    },
-    baseProps,
-  ),
-  group: assign(
-    {
-      colorScale: colors,
-    },
-    baseProps,
-  ),
-  histogram: assign(
-    {
-      style: {
-        data: {
-          fill: grey,
-          stroke: white,
-          strokeWidth: 2,
-        },
-        labels: baseLabelStyles,
-      },
-    },
-    baseProps,
-  ),
-  legend: {
-    colorScale: colors,
-    gutter: 10,
-    orientation: 'vertical',
-    titleOrientation: 'top',
-    style: {
-      data: {
-        type: 'circle',
-      },
-      labels: baseLabelStyles,
-      title: assign({}, baseLabelStyles, { padding: 5 }),
-    },
-  },
   line: assign(
     {
       style: {
@@ -205,39 +110,6 @@ const theme = {
         },
         labels: baseLabelStyles,
       },
-    },
-    baseProps,
-  ),
-  pie: {
-    style: {
-      data: {
-        padding: 10,
-        stroke: 'transparent',
-        strokeWidth: 1,
-      },
-      labels: assign({}, baseLabelStyles, { padding: 20 }),
-    },
-    colorScale: colors,
-    width: 400,
-    height: 400,
-    padding: 50,
-  },
-  scatter: assign(
-    {
-      style: {
-        data: {
-          fill: white,
-          stroke: 'transparent',
-          strokeWidth: 0,
-        },
-        labels: baseLabelStyles,
-      },
-    },
-    baseProps,
-  ),
-  stack: assign(
-    {
-      colorScale: colors,
     },
     baseProps,
   ),
@@ -253,28 +125,6 @@ const theme = {
     cornerRadius: 5,
     pointerLength: 10,
   },
-  voronoi: assign(
-    {
-      style: {
-        data: {
-          fill: 'transparent',
-          stroke: 'transparent',
-          strokeWidth: 0,
-        },
-        labels: assign({}, baseLabelStyles, {
-          padding: 5,
-          pointerEvents: 'none',
-        }),
-        flyout: {
-          stroke: white,
-          strokeWidth: 1,
-          fill: '#f0f0f0',
-          pointerEvents: 'none',
-        },
-      },
-    },
-    baseProps,
-  ),
 };
 
 export default theme;
