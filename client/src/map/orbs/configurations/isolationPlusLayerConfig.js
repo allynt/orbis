11e8251122974d349e449980af9a3aaccc3bfd7d
Slawomir Dyk <slawomir.dyk@astrosat.net>
2020-12-18 09:19:15 +0000

    fix(frontend): Added properties isolationPlusLayerConfig file #issueID 951

--- client/src/map/orbs/configurations/isolationPlusLayerConfig.js
+++ client/src/map/orbs/configurations/isolationPlusLayerConfig.js
@@ -14,6 +14,7 @@ const configuration = ({
   dispatch,
   orbState,
   authToken,
+  pickedInfo,
 }) => {
   const source = activeSources?.find(source => source.source_id === id);
   const selectedProperty = propertySelector(orbState);
@@ -37,7 +38,10 @@ const configuration = ({
     uniqueIdProperty: source.metadata.uniqueIdProperty,
     pickable: true,
     autoHighlight: true,
+    highlightColor: [0, 0, 0, 65],
     onClick: info => dispatch(setPickedInfo(info)),
+    getLineColor: [246, 190, 0, 255],
+    getLineWidth: d => (isEqual(pickedInfo?.object, d) ? 3 : 0),
     filled: true,
     getFillColor: d => [
       // @ts-ignore
@@ -46,6 +50,9 @@ const configuration = ({
         : [0, 0, 0]),
       150,
     ],
+    stroked: true,
+    // getLineWidth: 1,
+    lineWidthUnits: 'pixels',
     getFilterValue: d => Math.round(d.properties[selectedProperty.name]),
     filterRange: filterRange || [
       selectedPropertyMetadata?.min,
@@ -54,6 +61,7 @@ const configuration = ({
     updateTriggers: {
       getFillColor: [selectedProperty, filterRange],
       getFilterValue: [selectedProperty],
+      getLineWidth: [pickedInfo],
     },
     extensions: [new DataFilterExtension({ filterSize: 1 })],
   };
