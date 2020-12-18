70a1d0ac69e38046189148f8183c66ae8aee2fcc
Reignable <joebarrett401@gmail.com>
2020-12-18 13:55:15 +0000

    feat(frontend): Convert DeleteUserForm
    IssueID #860

--- client/src/admin/delete-user-form/delete-user-form.component.js
+++ client/src/admin/delete-user-form/delete-user-form.component.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { Button } from '@astrosat/astrosat-ui';
+import { Button, Link, Typography } from '@astrosat/astrosat-ui';
 
-import styles from './delete-user-form.module.css';
+import { Form } from 'components';
 
 export const DeleteUserForm = ({ user, deleteUser, close }) => {
   const handleClick = user => {
@@ -11,17 +11,21 @@ export const DeleteUserForm = ({ user, deleteUser, close }) => {
   };
 
   return (
-    <div className={styles.form}>
-      <p className={styles.message}>
-        Do you really want to deactivate the <strong>{user.user.name}</strong>{' '}
-        license and send deactivation email to the user’s email address?
-      </p>
-      <div className={styles.buttons}>
+    <Form>
+      <Form.Row>
+        <Typography>
+          Do you really want to deactivate the <b>{user.user.name}</b> license
+          and send deactivation email to the user’s email address?
+        </Typography>
+      </Form.Row>
+      <Form.Row centered>
         <Button onClick={() => handleClick(user)}>Yes, Send</Button>
-        <Button theme="link" onClick={() => close()}>
+      </Form.Row>
+      <Form.Row centered>
+        <Link component="button" onClick={() => close()}>
           Cancel
-        </Button>
-      </div>
-    </div>
+        </Link>
+      </Form.Row>
+    </Form>
   );
 };
