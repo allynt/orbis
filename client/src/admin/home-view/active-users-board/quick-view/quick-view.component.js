a851c51716fc63238e5f0353430e7c1b0fb2d064
Reignable <joebarrett401@gmail.com>
2020-12-21 12:21:33 +0000

    feat(frontend): Convert QuickView
    IssueID #860

--- client/src/admin/home-view/active-users-board/quick-view/quick-view.component.js
+++ client/src/admin/home-view/active-users-board/quick-view/quick-view.component.js
@@ -1,22 +1,101 @@
+import {
+  Divider as DefaultDivider,
+  makeStyles,
+  styled,
+  Typography,
+} from '@astrosat/astrosat-ui';
+import clsx from 'clsx';
 import React from 'react';
 
-import styles from './quick-view.module.css';
+const Wrapper = styled('div')(({ theme }) => ({
+  maxWidth: theme.typography.pxToRem(700),
+  display: 'grid',
+  gridTemplateColumns: 'max-content 1fr max-content 1fr max-content',
+  gridTemplateRows: '1fr 1fr',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: `0 ${theme.spacing(5)}px`,
+}));
 
-const QuickView = ({ data }) => (
-  <div className={styles.quickView}>
-    <div className={styles.column}>
-      <h2 className={styles.value}>{data?.active ? data.active : '-'}</h2>
-      <h3 className={styles.title}>Active Users</h3>
-    </div>
-    <div className={styles.column}>
-      <h2 className={styles.value}>{data?.pending ? data.pending : '-'}</h2>
-      <h3 className={styles.title}>Pending Invitations</h3>
-    </div>
-    <div className={styles.column}>
-      <h2 className={styles.value}>{data?.available ? data.available : '-'}</h2>
-      <h3 className={styles.title}>Licences Available</h3>
-    </div>
-  </div>
-);
+const Divider = styled(DefaultDivider)({
+  margin: '0 auto',
+  gridRow: '1 / -1',
+});
+
+const useStyles = makeStyles({
+  active: {
+    gridColumn: '1',
+  },
+  pending: {
+    gridColumn: '3',
+  },
+  available: {
+    gridColumn: '5',
+  },
+  value: {
+    gridRow: '1',
+  },
+});
+
+const QuickView = ({ data }) => {
+  const styles = useStyles();
+  return (
+    <Wrapper>
+      <Typography
+        className={clsx(styles.active, styles.value)}
+        align="center"
+        component="p"
+        gutterBottom
+        variant="h1"
+      >
+        {data?.active ?? '-'}
+      </Typography>
+      <Typography
+        className={styles.active}
+        align="center"
+        component="p"
+        variant="h2"
+      >
+        Active Users
+      </Typography>
+      <Divider orientation="vertical" />
+      <Typography
+        className={clsx(styles.pending, styles.value)}
+        align="center"
+        component="p"
+        gutterBottom
+        variant="h1"
+      >
+        {data?.pending ?? '-'}
+      </Typography>
+      <Typography
+        className={styles.pending}
+        align="center"
+        component="p"
+        variant="h2"
+      >
+        Pending Invitations
+      </Typography>
+      <Divider orientation="vertical" />
+      <Typography
+        className={clsx(styles.available, styles.value)}
+        align="center"
+        component="p"
+        gutterBottom
+        variant="h1"
+      >
+        {data?.available ?? '-'}
+      </Typography>
+      <Typography
+        className={styles.available}
+        align="center"
+        component="p"
+        variant="h2"
+      >
+        Licences Available
+      </Typography>
+    </Wrapper>
+  );
+};
 
 export default QuickView;
