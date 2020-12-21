30f8994c243890ec5c40a663991fa8d941f0243c
Allyn Treshansky <allyn.treshansky@gmail.com>
2020-12-21 13:13:08 +0000

    chore(backend): changed data that is tracked for user logins
    Reduced the amount of data that is stored for tracking user logins (b/c
    of GDPR).

--- server/orbis/views/views_auth.py
+++ server/orbis/views/views_auth.py
@@ -3,7 +3,6 @@ import logging
 
 from rest_framework.utils import encoders
 
-from astrosat_users.serializers import UserSerializerLite
 from astrosat_users.views import LoginView as AstrosatUsersLoginView
 
 db_logger = logging.getLogger("db")
@@ -15,7 +14,13 @@ class LoginView(AstrosatUsersLoginView):
         response = super().get_success_response()
 
         # log this event in the db...
-        user_data = UserSerializerLite(instance=self.user).data
+        user = self.user
+        customer = user.customers.first()
+        user_data = {
+            "userId": user.uuid,
+            "customerId": customer.id if customer else None,
+            "customerName": customer.name if customer else None,
+        }
         db_logger.info(
             json.dumps(user_data, cls=encoders.JSONEncoder),
             extra={"tags": ["USER_LOGIN"]},
