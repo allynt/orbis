15c330bbc404732ae0f422ddaf016e1bf31219ae
Reignable <joebarrett401@gmail.com>
2020-12-22 15:25:23 +0000

    feat(frontend): Convert PendingInvitationsBoard
    IssueID #860

--- client/src/admin/home-view/pending-invitations-board/pending-invitations-board.component.js
+++ client/src/admin/home-view/pending-invitations-board/pending-invitations-board.component.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 
 import { format } from 'date-fns';
 
-import { Button, OptionsIcon } from '@astrosat/astrosat-ui';
+import {
+  Button,
+  OptionsIcon,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+} from '@astrosat/astrosat-ui';
 
 import OptionsDropdown from '../options-dropdown/options-dropdown.component';
 
@@ -10,8 +17,79 @@ import { getUserLicences, getLicenceInfo } from '../../licence-utils';
 
 import styles from './pending-invitations.module.css';
 import tableStyles from '../../table.module.css';
+import { AdminTableCell } from 'admin/admin-table/admin-table-cell.component';
 
 const DATE_FORMAT = 'k:mm d MMMM yyyy';
+
+const TableHeader = () => (
+  <TableHead>
+    <TableRow>
+      <AdminTableCell align="left">Pending Invitations</AdminTableCell>
+      <AdminTableCell align="left">Email</AdminTableCell>
+      <AdminTableCell align="left">Licence Type</AdminTableCell>
+      <AdminTableCell align="left">Invitation Sent</AdminTableCell>
+      <AdminTableCell align="left">Invited</AdminTableCell>
+      <AdminTableCell align="left">&nbsp;</AdminTableCell>
+    </TableRow>
+  </TableHead>
+);
+
+const PendingUserRow = ({
+  user,
+  dropdown,
+  customer,
+  onResendInvitationClick,
+  setDropdown,
+  onWithdrawInvitationClick,
+}) => {
+  const selected = dropdown === user;
+  const date = format(new Date(user.invitation_date), DATE_FORMAT);
+  let licences = null;
+  if (customer && customer.licences) {
+    licences = getUserLicences(user, customer);
+  }
+  return (
+    <TableRow>
+      <AdminTableCell>{user.user.name}</AdminTableCell>
+      <AdminTableCell>{user.user.email}</AdminTableCell>
+      <AdminTableCell>{getLicenceInfo(licences)}</AdminTableCell>
+      <AdminTableCell>{date}</AdminTableCell>
+      <AdminTableCell>
+        <Button
+          className={styles.resendInvitation}
+          size="small"
+          onClick={() => {
+            onResendInvitationClick(user);
+          }}
+        >
+          Resend Invitation
+        </Button>
+      </AdminTableCell>
+      <AdminTableCell>
+        <OptionsIcon
+          data-testid="options-icon"
+          onClick={() => setDropdown(selected ? null : user)}
+        />
+        {selected && (
+          <OptionsDropdown
+            className={styles.withdrawDropdown}
+            onClickAway={() => setDropdown(null)}
+          >
+            <button
+              className={tableStyles.optionsButton}
+              onClick={() => {
+                onWithdrawInvitationClick(user);
+                setDropdown(null);
+              }}
+            >
+              Withdraw
+            </button>
+          </OptionsDropdown>
+        )}
+      </AdminTableCell>
+    </TableRow>
+  );
+};
 
 export const PendingInvitationsBoard = ({
   pendingUsers,
@@ -22,93 +100,29 @@ export const PendingInvitationsBoard = ({
   const [dropdown, setDropdown] = useState(null);
 
   return (
-    <table className={tableStyles.table}>
-      <thead className={tableStyles.thead}>
-        <tr className={tableStyles.theadr}>
-          <th align="left" className={tableStyles.th}>
-            Pending Invitations
-          </th>
-          <th align="left" className={tableStyles.th}>
-            Email
-          </th>
-          <th align="left" className={tableStyles.th}>
-            Licence Type
-          </th>
-          <th align="left" className={tableStyles.th}>
-            Invitation Sent
-          </th>
-          <th align="left" className={tableStyles.th}>
-            Invited
-          </th>
-          <th align="left" className={tableStyles.th}>
-            &nbsp;
-          </th>
-        </tr>
-      </thead>
-      <tbody>
+    <Table>
+      <TableHeader />
+      <TableBody>
         {pendingUsers && pendingUsers.length > 0 ? (
-          pendingUsers.map(user => {
-            const selected = dropdown === user;
-            const date = format(new Date(user.invitation_date), DATE_FORMAT);
-            let licences = null;
-            if (customer && customer.licences) {
-              licences = getUserLicences(user, customer);
-            }
-            return (
-              <tr key={user.id} className={tableStyles.tr}>
-                <td className={tableStyles.td}>{user.user.name}</td>
-                <td className={tableStyles.td}>{user.user.email}</td>
-                <td className={tableStyles.td}>{getLicenceInfo(licences)}</td>
-                <td className={tableStyles.td}>{date}</td>
-                <td className={tableStyles.td}>
-                  <Button
-                    className={styles.resendInvitation}
-                    size="small"
-                    onClick={() => {
-                      onResendInvitationClick(user);
-                    }}
-                  >
-                    Resend Invitation
-                  </Button>
-                </td>
-                <td
-                  className={`${tableStyles.td} ${tableStyles.optionsColumn}`}
-                >
-                  <OptionsIcon
-                    data-testid="options-icon"
-                    classes={`${tableStyles.optionsIcon} ${
-                      selected && tableStyles.optionsIconSelected
-                    }`}
-                    onClick={() => setDropdown(selected ? null : user)}
-                  />
-                  {selected && (
-                    <OptionsDropdown
-                      className={styles.withdrawDropdown}
-                      onClickAway={() => setDropdown(null)}
-                    >
-                      <button
-                        className={tableStyles.optionsButton}
-                        onClick={() => {
-                          onWithdrawInvitationClick(user);
-                          setDropdown(null);
-                        }}
-                      >
-                        Withdraw
-                      </button>
-                    </OptionsDropdown>
-                  )}
-                </td>
-              </tr>
-            );
-          })
+          pendingUsers.map(user => (
+            <PendingUserRow
+              key={user.id}
+              customer={customer}
+              dropdown={dropdown}
+              onResendInvitationClick={onResendInvitationClick}
+              onWithdrawInvitationClick={onWithdrawInvitationClick}
+              setDropdown={setDropdown}
+              user={user}
+            />
+          ))
         ) : (
-          <tr className={tableStyles.tr}>
-            <td align="center" colSpan={5} className={tableStyles.td}>
+          <TableRow>
+            <AdminTableCell align="center" colSpan={6}>
               No Pending Users
-            </td>
-          </tr>
+            </AdminTableCell>
+          </TableRow>
         )}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 };
