9c7869a899763fad5bb816d7d22e56e95b185de6
Reignable <joebarrett401@gmail.com>
2020-12-22 08:30:45 +0000

    feat(frontend): Convert AdministratorProfile
    IssueID #860

--- client/src/admin/corporate-view/adminstrator-profile/administrator-profile.component.stories.js
+++ client/src/admin/corporate-view/adminstrator-profile/administrator-profile.component.stories.js
@@ -11,6 +11,8 @@ const user = {
   name: 'John Smith',
   email: 'jsmith@gmail.com',
   phone: '12345678910',
+  avatar:
+    'https://pbs.twimg.com/profile_images/1218555839826579458/GL8pjTKD_400x400.jpg',
   customers: [
     {
       name: 'cyberdyne',
