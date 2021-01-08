97892a1f9e6bd862ad5ef8677ba004ff2358d627
Rory MacGregor <rorymacgregor@astrosat.net>
2021-01-08 10:23:58 +0000

    fix(frontend): Upgrade of components
    Components upgraded, I think all of them now, but I need to reboot my machine, so committing so I don't lose any progress.

--- client/src/landing/landing.component.js
+++ client/src/landing/landing.component.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-import { Redirect, Link } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Link,
+  Typography,
+  makeStyles,
+  styled,
+} from '@astrosat/astrosat-ui';
+
+import { Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { format } from 'date-fns';
@@ -13,27 +22,72 @@ import {
   selectBookmark,
 } from '../bookmarks/bookmark.slice';
 
-import { Button } from '@astrosat/astrosat-ui';
+import newUserBackgroundImage from './landing-image.png';
 
 import { ReactComponent as OrbisLogoLight } from '../orbis-light.svg';
 import { ReactComponent as OrbisLogoDark } from '../orbis-dark.svg';
 
-import styles from './landing.module.css';
+const useStyles = makeStyles(() => ({
+  container: {
+    backgroundColor: '#fff',
+  },
+  imageContainer: {
+    backgroundColor: '#fff',
+    backgroundImage: `url(${newUserBackgroundImage})`,
+  },
+  logo: {
+    width: '10rem',
+    color: '#787c82',
+  },
+  itemImage: {
+    height: '12rem',
+    alignSelf: 'center',
+    boxShadow: '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)',
+    borderRadius: '1rem',
+    overflow: 'hidden',
+    opacity: '0.7',
+    cursor: 'pointer',
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  creationDate: {
+    opacity: '0.5',
+    fontSize: '0.75rem',
+    padding: '0.3rem 0',
+    margin: '0',
+  },
+  paragraph: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: '1.25rem',
+  },
+}));
+
+const createLogo = logo => {
+  const Component = styled(logo)(({ theme }) => ({
+    height: theme.typography.pxToRem(80),
+    color: theme.palette.text.primary,
+  }));
+
+  return <Component />;
+};
 
 const ViewAllItems = ({ items, chooseBookmark, toggle }) => (
-  <div className={styles.content}>
-    <div className={styles.header}>
-      <h1>View All</h1>
-      <Button theme="link" onClick={toggle}>
-        Back to menu
-      </Button>
-    </div>
+  <Box margin="1rem 0" width="100%">
+    <Box display="flex" justifyContent="space-between" marginBottom="1rem">
+      <Typography variant="h2" gutterBottom>
+        View All
+      </Typography>
+      <Link onClick={toggle}>Back to menu</Link>
+    </Box>
     <Items items={items} chooseItem={chooseBookmark} />
-  </div>
+  </Box>
 );
 
 const Items = ({ items, chooseItem }) => {
   const [item, setItem] = useState(null);
+  const styles = useStyles();
 
   if (item) {
     chooseItem(item);
@@ -43,66 +97,138 @@ const Items = ({ items, chooseItem }) => {
     return <Redirect to={`/map/${queryString}`} />;
   }
   return (
-    <div className={styles.items}>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      width="100%"
+      flexWrap="wrap"
+      overflow="overlay"
+      maxHeight="35rem"
+    >
       {items.map(item => {
         const date = format(new Date(item.created), DATE_FORMAT);
 
         return (
-          <div className={styles.item} key={item.title}>
-            <div className={styles.image} onClick={() => setItem(item)}>
+          <Box key={item.title} width="17rem">
+            <Box className={styles.itemImage} onClick={() => setItem(item)}>
               <picture>
                 <img src={item.thumbnail} alt={item.title}></img>
               </picture>
-            </div>
-            <div className={styles.info}>
-              <div>
-                <h3 className={styles.title}>{item.title}</h3>
-                <p className={styles.creationDate}>{`Created ${date}`}</p>
-              </div>
-            </div>
-          </div>
+            </Box>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              minHeight="1.5rem"
+              padding="0.3rem 0"
+            >
+              <Box component="div">
+                <Box
+                  component="h3"
+                  padding="0.3rem 0"
+                  margin="0"
+                  fontSize="inherit"
+                  font-weight="inherit"
+                >
+                  {item.title}
+                </Box>
+                <Box component="p" className={styles.creationDate}>
+                  {`Created ${date}`}
+                </Box>
+              </Box>
+            </Box>
+          </Box>
         );
       })}
-    </div>
+    </Box>
   );
 };
 
-const NewUserLanding = () => (
-  <div className={styles.splash}>
-    <div className={styles.splashHeader}>
-      <OrbisLogoLight className={styles.logo} />
-    </div>
+const NewUserLanding = () => {
+  const styles = useStyles();
+  return (
+    <Box
+      className={styles.imageContainer}
+      position="relative"
+      height="100vh"
+      width="100vw"
+    >
+      <Box position="absolute" top="1rem" left="8rem">
+        {createLogo(OrbisLogoLight)}
+      </Box>
 
-    <div className={styles.splashContent}>
-      <div className={styles.journey}>
-        <h1>ORBIS JOURNEY</h1>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        alignItems="center"
+        height="100%"
+        width="100%"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="flex-end"
+          alignItems="center"
+          marginRight="15rem"
+          color="#fff"
+        >
+          <Box
+            component="h1"
+            fontSize="3.5rem"
+            fontWeight="bold"
+            marginBottom="2rem"
+          >
+            ORBIS JOURNEY
+          </Box>
 
-        <div className={styles.journeyText}>
-          <p>Your Earth Observation journey starts here</p>
-          <p>Click Browse Map below to start</p>
-        </div>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+            alignItems="center"
+            minHeight="4rem"
+            marginBottom="2rem"
+          >
+            <Typography variant="subtitle1" className={styles.paragraph}>
+              Your Earth Observation journey starts here
+            </Typography>
+            <Typography variant="subtitle1" className={styles.paragraph}>
+              Click Browse Map below to start
+            </Typography>
+          </Box>
 
-        <Link to="/map">
-          <Button theme="tertiary" data-testid="browse-map">
-            Browse Map
-          </Button>
-        </Link>
-      </div>
-    </div>
-  </div>
-);
+          <Link href="/map">
+            <Button data-testid="browse-map">Browse Map</Button>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 const ExistingUserLanding = ({ bookmarks, chooseBookmark }) => {
   const recentItems = bookmarks.slice(0, MAX_VISIBLE_BOOKMARKS);
   const [viewAllItems, setViewAllItems] = useState(false);
+  const styles = useStyles();
 
   const toggle = () => setViewAllItems(!viewAllItems);
 
   return (
-    <div className={styles.landingContent}>
-      <div className={styles.banner}>
-        <OrbisLogoDark className={styles.logo} />
-      </div>
+    <Box
+      className={styles.container}
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-around"
+      alignItems="center"
+      position="relative"
+      minHeight="100%"
+      width="100%"
+      maxWidth="86rem"
+      color="#101010"
+      padding="0 2.5rem"
+    >
+      <Box display="flex" width="100%">
+        {createLogo(OrbisLogoDark)}
+      </Box>
 
       {viewAllItems ? (
         <ViewAllItems
@@ -111,31 +237,41 @@ const ExistingUserLanding = ({ bookmarks, chooseBookmark }) => {
           toggle={toggle}
         />
       ) : (
-        <div className={styles.content}>
-          <div className={styles.header}>
-            <h1>Your Maps</h1>
+        <Box margin="1rem 0" width="100%">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            marginBottom="1rem"
+          >
+            <Typography variant="h2" gutterBottom>
+              Your Maps
+            </Typography>
             {bookmarks.length > MAX_VISIBLE_BOOKMARKS && (
-              <Button theme="link" onClick={toggle}>
-                View all
-              </Button>
+              <Link onClick={toggle}>View all</Link>
             )}
-          </div>
+          </Box>
           <Items items={recentItems} chooseItem={chooseBookmark} />
-        </div>
+        </Box>
       )}
 
-      <div className={styles.buttonContainer}>
-        <Link to="/map">
-          <Button theme="tertiary">Browse Map</Button>
+      <Box
+        display="flex"
+        justifyContent="center"
+        align-items="flex-start"
+        height="5rem"
+      >
+        <Link href="/map">
+          <Button>Browse Map</Button>
         </Link>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
 const Landing = () => {
   const dispatch = useDispatch();
   const bookmarks = useSelector(baseSelector)?.bookmarks;
+  const styles = useStyles();
 
   const chooseBookmark = bookmark => dispatch(selectBookmark(bookmark));
 
@@ -146,7 +282,13 @@ const Landing = () => {
   }, [bookmarks, dispatch]);
 
   return (
-    <div className={styles.landing}>
+    <Box
+      className={styles.container}
+      display="flex"
+      justifyContent="center"
+      height="100%"
+      minWidth="75%"
+    >
       {bookmarks?.length > 0 ? (
         <ExistingUserLanding
           bookmarks={bookmarks}
@@ -155,7 +297,7 @@ const Landing = () => {
       ) : (
         <NewUserLanding />
       )}
-    </div>
+    </Box>
   );
 };
 
