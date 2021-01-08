76f8c2465fad26eef14d53c8739511e5e4baf89e
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-01-08 11:59:34 +0000

    fix(frontend): Rebuild css module for the side.panel.component.js IssueID #862

--- client/src/components/side-panel/side-panel.component.js
+++ client/src/components/side-panel/side-panel.component.js
@@ -1,8 +1,48 @@
 import clsx from 'clsx';
 import * as React from 'react';
 
-import styles from './side-panel.module.css';
+import { makeStyles } from '@astrosat/astrosat-ui';
 
+const useStyles = makeStyles(theme => ({
+  header: {
+    width: '100%',
+    padding: theme.spacing(2),
+    borderBottom: `2px solid ${theme.palette.primary.main}`,
+  },
+  content: {
+    padding: theme.spacing(1),
+  },
+  open: {
+    transform: 'translateX(0)',
+    '& .left': {
+      marginRight: 'calc(-1 * 20rem)',
+    },
+    '& .right': {
+      marginLeft: 'calc(-1 * 20rem)',
+    },
+  },
+  left: {
+    borderTopRightRadius: '1rem',
+    borderBottomRightRadius: '1rem',
+    transform: 'translateX(0)',
+    marginRight: 'calc(-1 * 20rem)',
+  },
+  right: {
+    borderTopLeftRadius: '1rem',
+    borderBottomLeftRadius: '1rem',
+    transform: 'translateX(20rem)',
+    marginLeft: 'calc(-1 * 20rem)',
+  },
+  sidePanel: {
+    width: '20rem',
+    height: '100%',
+    backgroundColor: theme.palette.secondary.main,
+    color: '#ffffff',
+    flexShrink: 0,
+    transition: 'transform 250ms ease-in-out, margin 300ms ease-in-out',
+    zIndex: 3,
+  },
+}));
 /**
  * @param {{
  *  children?: React.ReactNode
@@ -20,13 +60,16 @@ export const SidePanel = ({
   header,
   open = false,
   orientation = 'left',
-}) => (
-  <div
-    className={clsx(className, styles.sidePanel, styles[orientation], {
-      [styles.open]: open,
-    })}
-  >
-    {header && <div className={styles.header}>{header}</div>}
-    <div className={clsx(styles.content, contentClassName)}>{children}</div>
-  </div>
-);
+}) => {
+  const style = useStyles({});
+  return (
+    <div
+      className={clsx(className, style.sidePanel, style[orientation], {
+        [style.open]: open,
+      })}
+    >
+      {header && <div className={style.header}>{header}</div>}
+      <div className={clsx(style.content, contentClassName)}>{children}</div>
+    </div>
+  );
+};
