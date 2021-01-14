49b7af42cab1c0e2f001d5567b4cec9798c2b150
Reignable <joebarret401@gmail.com>
2021-01-14 10:18:00 +0000

    fix(frontend): Convert accounts wrapper

--- client/src/accounts/wrapper.component.js
+++ client/src/accounts/wrapper.component.js
@@ -1,16 +1,57 @@
 import React from 'react';
 
-import { ReactComponent as OrbisLogo } from '../orbis-dark.svg';
+import { OrbisLogo } from 'components';
 
 import styles from './wrapper.module.css';
+import {
+  Container,
+  makeStyles,
+  Paper,
+  ThemeProvider,
+} from '@astrosat/astrosat-ui';
+import background from './background.png';
 
-const Wrapper = ({ children }) => (
-  <div className={styles.page}>
-    <div className={styles.container}>
-      <OrbisLogo className={styles.logo} />
-      {children}
+const useStyles = makeStyles(theme => ({
+  page: {
+    display: 'grid',
+    placeItems: 'center',
+    width: '100%',
+    height: '100vh',
+    padding: theme.spacing(4),
+    background: `url(${background})`,
+    backgroundSize: 'cover',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    overflowY: 'auto',
+    padding: theme.spacing(4, 12, 4),
+    borderRadius: theme.shape.borderRadius * 3,
+  },
+  logo: {
+    width: '11.5rem',
+    minHeight: '6rem',
+    margin: `0 auto ${theme.spacing(3)} auto`,
+  },
+}));
+
+const Wrapper = ({ children }) => {
+  const styles = useStyles();
+  return (
+    <div className={styles.page}>
+      <ThemeProvider theme="light">
+        <Paper
+          elevation={20}
+          component={Container}
+          maxWidth="sm"
+          className={styles.container}
+        >
+          <OrbisLogo className={styles.logo} />
+          {children}
+        </Paper>
+      </ThemeProvider>
     </div>
-  </div>
-);
+  );
+};
 
 export default Wrapper;
