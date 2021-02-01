a62c0b80ba91335d78c181b96e81e31d9230c04c
Reignable <joebarret401@gmail.com>
2021-02-01 08:51:58 +0000

    chore(frontend): Add tests for actionForHelpConfig

--- client/src/map/orbs/configurations/actionForHelpConfig.js
+++ client/src/map/orbs/configurations/actionForHelpConfig.js
@@ -5,17 +5,19 @@ import iconAtlas from './actionForHelpConfig.iconAtlas.svg';
 import { setPickedObjects } from 'map/orbs/slices/action-for-help.slice';
 import { easeInOutCubic } from 'utils/easingFunctions';
 
+/**
+ * @typedef {import('typings/orbis').GeoJsonFeature<{type?: string, Type?: string}>} ActionForHelpFeature
+ */
+
 const configuration = ({ id, data, activeSources, dispatch, setViewState }) => {
+  /** @param {import('typings/orbis').PickedMapFeature} info */
   const handleLayerClick = info => {
     if (info.object.properties.cluster) {
       if (info.object.properties.expansion_zoom <= MAX_ZOOM)
         setViewState({
           longitude: info.object.geometry.coordinates[0],
           latitude: info.object.geometry.coordinates[1],
-          zoom:
-            info.object.properties.expansion_zoom >= MAX_ZOOM
-              ? MAX_ZOOM
-              : info.object.properties.expansion_zoom,
+          zoom: info.object.properties.expansion_zoom,
           transitionDuration: 1000,
           transitionEasing: easeInOutCubic,
           transitionInterpolator: new FlyToInterpolator(),
@@ -24,6 +26,12 @@ const configuration = ({ id, data, activeSources, dispatch, setViewState }) => {
     } else dispatch(setPickedObjects([info.object]));
   };
 
+  /** @param {ActionForHelpFeature} feature */
+  const getIcon = feature => feature.properties.type || feature.properties.Type;
+
+  /** @param {ActionForHelpFeature} feature */
+  const getIconSize = feature => (feature.properties.Type ? 15 : 60);
+
   return {
     id,
     iconMapping,
@@ -31,8 +39,8 @@ const configuration = ({ id, data, activeSources, dispatch, setViewState }) => {
     data,
     visible: !!activeSources?.find(source => source.source_id === id),
     onClick: handleLayerClick,
-    getIcon: feature => feature.properties.type || feature.properties.Type,
-    getIconSize: feature => (feature.properties.Type ? 15 : 60),
+    getIcon,
+    getIconSize,
   };
 };
 
---
+++ client/src/map/orbs/configurations/actionForHelpConfig.test.js
@@ -0,0 +1,102 @@
+// @ts-nocheck
+
+import { MAX_ZOOM } from 'map/map.constants';
+import { setPickedObjects } from '../slices/action-for-help.slice';
+import configFn from './actionForHelpConfig';
+
+const PERSON = {
+    properties: {
+      Type: 'person',
+    },
+  },
+  INFRASTRUCTURE = {
+    properties: {
+      type: 'hospital',
+    },
+  };
+
+const source = {
+  source_id: 'test/layer',
+  test: 123,
+};
+
+const setup = (activeSources = [source]) => {
+  const dispatch = jest.fn();
+  const setViewState = jest.fn();
+  const utils = configFn({
+    id: 'test/layer',
+    dispatch,
+    setViewState,
+    activeSources,
+  });
+  return { ...utils, dispatch, setViewState };
+};
+
+describe('actionForHelpConfig', () => {
+  describe('visible', () => {
+    it('is true if activeSources includes the layer source', () => {
+      const { visible } = setup();
+      expect(visible).toBe(true);
+    });
+
+    it('is false if the layer source is not found in activeSources', () => {
+      const { visible } = setup([{ source_id: 'not/layer' }]);
+      expect(visible).toBe(false);
+    });
+  });
+
+  describe('getIcon', () => {
+    it('Returns the `type` property for infrastructure features', () => {
+      const { getIcon } = setup();
+      expect(getIcon(INFRASTRUCTURE)).toBe(INFRASTRUCTURE.properties.type);
+    });
+
+    it('Returns the `Type` property for person features', () => {
+      const { getIcon } = setup();
+      expect(getIcon(PERSON)).toBe(PERSON.properties.Type);
+    });
+  });
+
+  describe('getIconSize', () => {
+    it('Returns 15 for person features', () => {
+      const { getIconSize } = setup();
+      expect(getIconSize(PERSON)).toBe(15);
+    });
+    it('Returns 60 for other features', () => {
+      const { getIconSize } = setup();
+      expect(getIconSize(INFRASTRUCTURE)).toBe(60);
+    });
+  });
+
+  describe('onClick', () => {
+    it(`Calls setViewState if the feature is a cluster and expansion zoom is less than or equal to ${MAX_ZOOM}`, () => {
+      const { onClick, setViewState } = setup();
+      onClick({
+        object: {
+          geometry: { coordinates: [1, 2] },
+          properties: { cluster: true, expansion_zoom: MAX_ZOOM - 5 },
+        },
+      });
+      expect(setViewState).toBeCalledWith(
+        expect.objectContaining({ longitude: 1, latitude: 2 }),
+      );
+    });
+
+    it(`Dispatches the ${setPickedObjects.type} action if the feature is a cluster and expansion zoom is greater than ${MAX_ZOOM}`, () => {
+      const objects = ['hello', 'there'];
+      const { onClick, dispatch } = setup();
+      onClick({
+        object: { properties: { cluster: true, expansion_zoom: MAX_ZOOM + 2 } },
+        objects,
+      });
+      expect(dispatch).toHaveBeenCalledWith(setPickedObjects(objects));
+    });
+
+    it(`Dispatches the ${setPickedObjects.type} action if the feature is not a cluster`, () => {
+      const object = { properties: { cluster: false, test: 'hello' } };
+      const { onClick, dispatch } = setup();
+      onClick({ object });
+      expect(dispatch).toHaveBeenCalledWith(setPickedObjects([object]));
+    });
+  });
+});
