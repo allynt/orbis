e8eb5e7f4b078781277ee860b24657d511bdff37
Reignable <joebarret401@gmail.com>
2021-02-01 09:18:58 +0000

    fix(frontend): Remove the need for action for help slice
    In creating some of the newest MSL and VE layers
    a method of generically storing clicked features was created.
    As this is all the Action for Help redux slice was doing, I've converted
    Action for help to use this generic state and remvoed the specific slice.

--- client/src/map/orbs/components/ActionForHelpMapComponent.js
+++ client/src/map/orbs/components/ActionForHelpMapComponent.js
@@ -2,20 +2,26 @@ import { FeatureDetail, Popup } from 'components';
 import { pickBy } from 'lodash';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  pickedObjectsSelector,
-  setPickedObjects,
-} from '../slices/action-for-help.slice';
+import { clickedFeaturesSelector, setClickedFeatures } from '../orbReducer';
 
-const ActionForHelpMapComponent = () => {
-  const pickedObjects = useSelector(pickedObjectsSelector);
+const ActionForHelpMapComponent = ({ source }) => {
+  const pickedObjects = useSelector(state =>
+    clickedFeaturesSelector(source?.source_id)(state?.orbs),
+  );
   const dispatch = useDispatch();
-  if (!pickedObjects.length) return null;
+  if (!pickedObjects?.length) return null;
   return (
     <Popup
       longitude={pickedObjects[0]?.geometry.coordinates[0]}
       latitude={pickedObjects[0]?.geometry.coordinates[1]}
-      onClose={() => dispatch(setPickedObjects([]))}
+      onClose={() =>
+        dispatch(
+          setClickedFeatures({
+            source_id: source.source_id,
+            clickedFeatures: [],
+          }),
+        )
+      }
       captureScroll
     >
       <FeatureDetail
