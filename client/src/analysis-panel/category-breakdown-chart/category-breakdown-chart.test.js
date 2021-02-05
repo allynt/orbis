369c43217959bd8423fe679dcbd4d09f59ea5316
Reignable <joebarret401@gmail.com>
2021-02-05 10:28:36 +0000

    chore(frontend): Add more unit tests
    ORB-87

--- client/src/analysis-panel/category-breakdown-chart/category-breakdown-chart.test.js
+++ client/src/analysis-panel/category-breakdown-chart/category-breakdown-chart.test.js
@@ -1,7 +1,11 @@
+// @ts-nocheck
+
 import * as React from 'react';
+
 import { render } from '@testing-library/react';
-import { CategoryBreakdownChart } from './category-breakdown-chart.component';
 import userEvent from '@testing-library/user-event';
+
+import { CategoryBreakdownChart } from './category-breakdown-chart.component';
 
 const PROPERTY = {
     type: 'discrete',
@@ -46,15 +50,20 @@ const PROPERTY = {
     },
   ];
 
-const renderComponent = () =>
+const renderComponent = ({ clickedFeatures = CLICKED_FEATURES } = {}) =>
   render(
     <CategoryBreakdownChart
       selectedProperty={PROPERTY}
-      clickedFeatures={CLICKED_FEATURES}
+      clickedFeatures={clickedFeatures}
     />,
   );
 
 describe('<CategoryBreakdownChart />', () => {
+  it('returns null if clickedFeatures has no length', () => {
+    const { queryByText } = renderComponent({ clickedFeatures: [] });
+    expect(queryByText(PROPERTY.label)).not.toBeInTheDocument();
+  });
+
   it('Shows the property label', () => {
     const { getByText } = renderComponent();
     expect(getByText(PROPERTY.label)).toBeInTheDocument();
@@ -70,11 +79,49 @@ describe('<CategoryBreakdownChart />', () => {
   it('Shows the total number of areas when no segment is clicked', () => {
     const { getByText } = renderComponent();
     expect(getByText('3')).toBeInTheDocument();
+    expect(getByText('Areas')).toBeInTheDocument();
   });
 
   it('Shows the count of areas in a segment when that segment is clicked', () => {
     const { getAllByRole, getByText } = renderComponent();
     userEvent.click(getAllByRole('presentation')[0]);
     expect(getByText('1 / 3')).toBeInTheDocument();
+  });
+
+  it('Unsets the selected segment if the same segment is clicked', () => {
+    const { getAllByRole, getByText } = renderComponent();
+    userEvent.click(getAllByRole('presentation')[0]);
+    expect(getByText('1 / 3')).toBeInTheDocument();
+    userEvent.click(getAllByRole('presentation')[0]);
+    expect(getByText('3')).toBeInTheDocument();
+  });
+
+  it('Shows area if only one area is clicked', () => {
+    const { getByText } = renderComponent({
+      clickedFeatures: [{ object: { properties: { fruit: 'Limes' } } }],
+    });
+    expect(getByText('1')).toBeInTheDocument();
+    expect(getByText('Area')).toBeInTheDocument();
+  });
+
+  it('Unsets the selected segment if its category is removed from the features', () => {
+    const { getByText, getAllByRole, rerender } = renderComponent();
+    userEvent.click(getAllByRole('presentation')[0]);
+    expect(getByText('1 / 3')).toBeInTheDocument();
+    const [, ...rest] = CLICKED_FEATURES;
+    rerender(
+      <CategoryBreakdownChart
+        selectedProperty={PROPERTY}
+        clickedFeatures={rest}
+      />,
+    );
+    expect(getByText('2')).toBeInTheDocument();
+  });
+
+  it('Shows Area if only one area is clicked', () => {
+    const { getByText } = renderComponent({
+      clickedFeatures: [{ object: { properties: { fruit: 'Apples' } } }],
+    });
+    expect(getByText(/area/i)).toBeInTheDocument();
   });
 });
