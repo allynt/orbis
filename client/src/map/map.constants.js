58a7426fffd88b85ed58d170396537022465b85f
Rory MacGregor <rorymacgregor@astrosat.net>
2021-02-05 13:01:33 +0000

    fix(frontend): Error in saving last commit, uppercasing constant.
    This just uppercases the constant in the map component.

--- client/src/map/map.constants.js
+++ client/src/map/map.constants.js
@@ -1,7 +1,7 @@
 export const VECTOR = 'vector';
 export const RASTER = 'raster';
 export const MAX_ZOOM = 20;
-export const defaultDecimalPrecision = 2;
+export const DEFAULT_DECIMAL_PRECISION = 2;
 
 export const regions = [
   {
