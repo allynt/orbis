d3fd907e0f1681de67d827d25ac410423b74ac74
Reignable <joebarret401@gmail.com>
2021-02-10 14:42:23 +0000

    chore(frontend): Style FeatureDialog
    ORB-159

--- client/src/components/feature-dialog/feature-dialog.component.js
+++ client/src/components/feature-dialog/feature-dialog.component.js
@@ -1,20 +1,38 @@
 import {
   CloseIcon,
   Dialog,
+  DialogContent,
   DialogTitle,
+  Divider,
   IconButton,
   makeStyles,
   Typography,
 } from '@astrosat/astrosat-ui';
 import { omit, pick } from 'lodash';
 import * as React from 'react';
+import { isRealValue } from 'utils/isRealValue';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   closeButton: {
     position: 'absolute',
     right: 0,
   },
-});
+  content: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 50%',
+    padding: 0,
+  },
+  divider: {
+    gridColumn: '1/-1',
+  },
+  item: {
+    padding: theme.spacing(4, 5),
+  },
+  key: {
+    ...theme.typography.h2,
+    padding: theme.spacing(4, 10),
+  },
+}));
 
 /**
  * @template P
@@ -45,27 +63,32 @@ export const FeatureDialog = ({
   const title = !!titleProperty ? feature.properties[titleProperty] : 'Feature';
 
   const renderableProperties = React.useMemo(() => {
-    let properties = feature.properties;
+    let properties = feature?.properties;
     // @ts-ignore
     if (!!titleProperty) properties = omit(properties, titleProperty);
     // @ts-ignore
     if (blacklist?.length) properties = omit(properties, blacklist);
     if (whitelist?.length) properties = pick(properties, whitelist);
     return properties;
-  }, [blacklist, feature.properties, titleProperty, whitelist]);
+  }, [blacklist, feature, titleProperty, whitelist]);
 
-  return (
-    <Dialog open={open}>
+  return !!feature ? (
+    <Dialog open={open} maxWidth="md" fullWidth>
       <IconButton className={styles.closeButton} onClick={onClose}>
         <CloseIcon titleAccess="Close" />
       </IconButton>
       <DialogTitle>{title}</DialogTitle>
-      {Object.entries(renderableProperties).map(([key, value]) => (
-        <>
-          <Typography>{key}</Typography>
-          <Typography>{value.toString()}</Typography>
-        </>
-      ))}
+      <DialogContent className={styles.content}>
+        {Object.entries(renderableProperties).map(([key, value]) => (
+          <>
+            <Typography className={styles.key}>{key}</Typography>
+            <Typography className={styles.item}>
+              {isRealValue(value) ? value.toString() : '-'}
+            </Typography>
+            <Divider className={styles.divider} />
+          </>
+        ))}
+      </DialogContent>
     </Dialog>
-  );
+  ) : null;
 };
---
+++ client/src/components/feature-dialog/feature-dialog.stories.js
@@ -0,0 +1,52 @@
+import faker from 'faker/locale/en_GB';
+import React from 'react';
+import { FeatureDialog } from './feature-dialog.component';
+
+export default { title: 'Components/Feature Dialog', args: { open: true } };
+
+const Template = args => <FeatureDialog {...args} />;
+
+export const Default = Template.bind({});
+
+export const Feature = Template.bind({});
+Feature.args = {
+  feature: {
+    properties: {
+      Company: faker.company.companyName(),
+      Address: faker.fake('{{address.streetAddress}}, {{address.city}}'),
+      Telephone: faker.phone.phoneNumber(),
+      Website: faker.internet.url(),
+    },
+  },
+};
+
+export const TitleProperty = Template.bind({});
+TitleProperty.args = {
+  ...Feature.args,
+  titleProperty: 'Company',
+};
+
+export const Blacklist = Template.bind({});
+Blacklist.args = {
+  ...Feature.args,
+  blacklist: ['Telephone', 'Company'],
+};
+
+export const Whitelist = Template.bind({});
+Whitelist.args = {
+  ...Feature.args,
+  whitelist: ['Company', 'Telephone'],
+};
+
+export const FalsyContent = Template.bind({});
+FalsyContent.args = {
+  feature: {
+    properties: {
+      False: false,
+      Zero: 0,
+      Undefined: undefined,
+      Null: null,
+      EmptyString: '',
+    },
+  },
+};
