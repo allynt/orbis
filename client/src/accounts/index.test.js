2d302afe2883e9f481ca1428af70eefdec8c0dfa
Reignable <joebarret401@gmail.com>
2021-03-01 10:06:43 +0000

    chore(frontend): Add more tests for accounts index

--- client/src/accounts/index.test.js
+++ client/src/accounts/index.test.js
@@ -9,6 +9,7 @@ import Accounts from '.';
 import {
   CONFIRM_EMAIL,
   LOGIN,
+  PASSWORD_CHANGE,
   PASSWORD_RESET,
   PASSWORD_RESET_REQUEST,
   REGISTER,
@@ -17,13 +18,31 @@ import {
   REGISTER_CUSTOMER_USER,
   RESEND,
 } from './accounts.constants';
-import { resendVerificationEmail } from './accounts.slice';
+import {
+  changePassword,
+  changePasswordSuccess,
+  confirmResetPassword,
+  passwordResetRequestedSuccess,
+  registerCustomer,
+  registerCustomerSuccess,
+  resendVerificationEmail,
+  resendVerificationEmailSuccess,
+  resetPassword,
+  resetPasswordSuccess,
+} from './accounts.slice';
 
 const mockStore = configureMockStore([thunk]);
 
 const renderComponent = (initialEntries = ['']) => {
   const store = mockStore({
-    app: { apiUrl: '' },
+    app: {
+      apiUrl: '',
+      config: {
+        passwordMinLength: 0,
+        passwordMaxLength: 255,
+        passwordStrength: 1,
+      },
+    },
     accounts: { user: { email: 'test@test.com' } },
   });
   const utils = render(<Accounts />, {
@@ -36,7 +55,7 @@ const renderComponent = (initialEntries = ['']) => {
   return { store, ...utils };
 };
 
-describe('Accounts index', () => {
+describe.only('Accounts index', () => {
   it(`Shows journey selection when route is ${REGISTER}`, async () => {
     const { getAllByRole } = renderComponent([REGISTER]);
     await waitFor(() =>
@@ -44,11 +63,28 @@ describe('Accounts index', () => {
     );
   });
 
-  it(`Shows CustomerRegistration when route is ${REGISTER_CUSTOMER}`, async () => {
-    const { getAllByRole } = renderComponent([REGISTER_CUSTOMER]);
-    await waitFor(() =>
-      expect(getAllByRole('textbox').length).toBeGreaterThanOrEqual(1),
-    );
+  describe(`${REGISTER_CUSTOMER}`, () => {
+    it(`Shows CustomerRegistration when route is ${REGISTER_CUSTOMER}`, async () => {
+      const { getAllByRole } = renderComponent([REGISTER_CUSTOMER]);
+      await waitFor(() =>
+        expect(getAllByRole('textbox').length).toBeGreaterThanOrEqual(1),
+      );
+    });
+
+    it(`dispatches ${registerCustomer.name} when submitted`, async () => {
+      fetch.mockResponse(JSON.stringify({}));
+      const { getByRole, store } = renderComponent([REGISTER_CUSTOMER]);
+      userEvent.type(
+        getByRole('textbox', { name: /organisation\sname/i }),
+        'Test Company',
+      );
+      userEvent.click(getByRole('button', { name: /next/i }));
+      await waitFor(() =>
+        expect(store.getActions()).toEqual(
+          expect.arrayContaining([registerCustomerSuccess()]),
+        ),
+      );
+    });
   });
 
   it(`Shows UserRegistration when route is ${REGISTER_CUSTOMER_USER}`, async () => {
@@ -76,18 +112,73 @@ describe('Accounts index', () => {
     },
   );
 
-  it(`Shows PasswordResetRequest when route is ${PASSWORD_RESET_REQUEST}`, async () => {
-    const { getAllByRole } = renderComponent([PASSWORD_RESET_REQUEST]);
-    await waitFor(() => expect(getAllByRole('textbox').length).toBe(1));
+  describe(`${PASSWORD_CHANGE}`, () => {
+    it(`dispatches ${changePassword.name} action when submitted`, async () => {
+      const { getByLabelText, getByRole, store } = renderComponent([
+        PASSWORD_CHANGE,
+      ]);
+      userEvent.type(getByLabelText(/old\spassword/i), 'thisisanoldpassword');
+      userEvent.type(getByLabelText(/new\spassword$/i), 'thisismynewpassword');
+      userEvent.type(
+        getByLabelText(/new\spassword\sconfirmation/i),
+        'thisismynewpassword',
+      );
+      userEvent.click(getByRole('checkbox'));
+      userEvent.click(getByRole('button', { name: /change\spassword/i }));
+      await waitFor(() =>
+        expect(store.getActions()).toEqual(
+          expect.arrayContaining([changePasswordSuccess()]),
+        ),
+      );
+    });
   });
 
-  it(`Shows PasswordResetForm when route is ${PASSWORD_RESET}`, async () => {
-    const { getByRole } = renderComponent([PASSWORD_RESET]);
-    await waitFor(() =>
-      expect(
-        getByRole('button', { name: 'Reset Password' }),
-      ).toBeInTheDocument(),
-    );
+  describe(`${PASSWORD_RESET_REQUEST}`, () => {
+    it(`Shows PasswordResetRequest when route is ${PASSWORD_RESET_REQUEST}`, async () => {
+      const { getAllByRole } = renderComponent([PASSWORD_RESET_REQUEST]);
+      await waitFor(() => expect(getAllByRole('textbox').length).toBe(1));
+    });
+
+    it(`dispatches ${resetPassword.name} action when submitted`, async () => {
+      fetch.once(JSON.stringify({}));
+      const { getByRole, store } = renderComponent([PASSWORD_RESET_REQUEST]);
+      userEvent.type(getByRole('textbox'), 'test@test.com');
+      userEvent.click(getByRole('button'));
+      await waitFor(() =>
+        expect(store.getActions()).toEqual(
+          expect.arrayContaining([resetPasswordSuccess()]),
+        ),
+      );
+    });
+  });
+
+  describe(`${PASSWORD_RESET}`, () => {
+    it(`Shows PasswordResetForm when route is ${PASSWORD_RESET}`, async () => {
+      const { getByRole } = renderComponent([PASSWORD_RESET]);
+      await waitFor(() =>
+        expect(
+          getByRole('button', { name: 'Reset Password' }),
+        ).toBeInTheDocument(),
+      );
+    });
+
+    it(`dispatches the ${confirmResetPassword.name} action when filled out correctly`, async () => {
+      fetch.once(JSON.stringify({}));
+      const { getByRole, getByLabelText, store } = renderComponent([
+        PASSWORD_RESET,
+      ]);
+      userEvent.type(getByLabelText(/new\spassword$/i), 'pandaconcretespoon');
+      userEvent.type(
+        getByLabelText(/new\spassword\sconfirmation/i),
+        'pandaconcretespoon',
+      );
+      userEvent.click(getByRole('button', { name: /reset\spassword/i }));
+      await waitFor(() =>
+        expect(store.getActions()).toEqual(
+          expect.arrayContaining([passwordResetRequestedSuccess()]),
+        ),
+      );
+    });
   });
 
   describe(`${RESEND}`, () => {
@@ -96,15 +187,13 @@ describe('Accounts index', () => {
       expect(getByRole('button', { name: 'Resend email' })).toBeInTheDocument();
     });
 
-    it(`dispatches ${resendVerificationEmail.name} action when button is clicked`, () => {
+    it(`dispatches ${resendVerificationEmail.name} action when button is clicked`, async () => {
       const { getByRole, store } = renderComponent([RESEND]);
       userEvent.click(getByRole('button', { name: 'Resend email' }));
-      expect(store.getActions()).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            ...resendVerificationEmail('test@test.com'),
-          }),
-        ]),
+      await waitFor(() =>
+        expect(store.getActions()).toEqual(
+          expect.arrayContaining([resendVerificationEmailSuccess()]),
+        ),
       );
     });
   });
