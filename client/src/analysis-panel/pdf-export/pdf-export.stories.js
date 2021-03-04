257ce36de2a0e91346de26567000a841adfca932
Rory MacGregor <rorymacgregor@astrosat.net>
2021-03-04 17:54:41 +0000

    fix(frontend): Add stories for PDF component
    Added stories for the PDF component.

---
+++ client/src/analysis-panel/pdf-export/pdf-export.stories.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import faker from 'faker';
+
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+
+import PDF from './pdf-export.component';
+
+const mockStore = configureMockStore();
+
+export default { title: 'Analysis Panel/PDF Export' };
+
+const defaultUser = { name: 'John Smith', email: 'johnsmith@gmail.com' };
+
+const generateFeatures = n => {
+  return new Array(n).fill(undefined).map(feat => ({
+    object: {
+      properties: {
+        area_name: 'test_area_name',
+        population: 1200,
+        households: 260,
+        '% of people aged 0-17': 13.6,
+        '% of people aged 18-39': 11.6,
+        '% of people aged 40-64': 44.7,
+        '% of people aged 65+': 30.1,
+      },
+    },
+  }));
+};
+
+const defaultState = {
+  screenshot: undefined,
+  property: {
+    source_id: 'astrosat/isolation_plus/age_census/r4v1',
+    source: 'testsourcenameasdfadfasdfasdfasdfasdfasdfasdfasdfasdfasdf',
+    details: 'This is a test description',
+    name: '% of people aged 0-17',
+    label: 'Test label',
+    aggregation: 'mean',
+    aggregates: { GB: 20.4, England: 20.8, Scotland: 18.8, Wales: 20.3 },
+    precision: 1,
+    breakdown: [
+      '% of people aged 0-17',
+      '% of people aged 18-39',
+      '% of people aged 40-64',
+      '% of people aged 65+',
+    ],
+  },
+  clickedFeatures: generateFeatures(3),
+};
+
+const Template = ({ user = defaultUser, state = defaultState }) => (
+  <Provider
+    store={mockStore({
+      orbs: {
+        isolationPlus: state,
+      },
+    })}
+  >
+    <PDF user={user} />
+  </Provider>
+);
+
+export const Default = Template.bind({});
+
+export const NoUserName = Template.bind({});
+NoUserName.args = {
+  user: { email: 'johnsmith@gmail.com' },
+};
+
+export const LotsOfFeatures = Template.bind({});
+LotsOfFeatures.args = {
+  state: {
+    ...defaultState,
+    clickedFeatures: generateFeatures(20),
+  },
+};
+
+export const TimeSeriesData = Template.bind({});
+TimeSeriesData.args = {
+  state: {
+    ...defaultState,
+    timeseries: true,
+    timeseries_latest_timestamp: 396,
+  },
+};
