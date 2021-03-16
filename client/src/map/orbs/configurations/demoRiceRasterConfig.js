68ad8a5e6e874b15ca53b5ecb659f6feddc55eb2
Reignable <joebarret401@gmail.com>
2021-03-16 15:33:16 +0000

    fix(frontend): Make work with static data server
    ORB-387

--- client/src/map/orbs/configurations/demoRiceRasterConfig.js
+++ client/src/map/orbs/configurations/demoRiceRasterConfig.js
@@ -1,15 +1,19 @@
-export default ({ id, data, activeSources }) => {
+import { find } from 'lodash';
+
+export default ({ id, data, activeSources, authToken }) => {
   const defaultColumn = 'ndvi';
   const defaultDate = '20200725';
   const source = activeSources.find(s => s.source_id === id);
   return {
     id,
     image: `${data}/${defaultColumn}_${defaultDate}.png`,
-    bounds: [
-      100.42684677700454,
-      5.839335465057107,
-      100.48749559928604,
-      5.914446003530912,
-    ],
+    bounds: find(source.metadata.properties, { name: defaultColumn }).bounds,
+    loadOptions: {
+      fetch: {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      },
+    },
   };
 };
