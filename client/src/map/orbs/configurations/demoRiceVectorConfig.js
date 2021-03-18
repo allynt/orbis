a029830ac8a5e7bb77142e92efd50284626a6271
Reignable <joebarret401@gmail.com>
2021-03-18 10:10:58 +0000

    fix(frontend): Add extrusions
    ORB-387

--- client/src/map/orbs/configurations/demoRiceVectorConfig.js
+++ client/src/map/orbs/configurations/demoRiceVectorConfig.js
@@ -1,19 +1,26 @@
 import { format } from 'date-fns';
 import { find, get } from 'lodash';
 import { ColorScale } from 'utils/ColorScale';
-import { otherSelector } from '../orbReducer';
+import {
+  extrudedModeSelector,
+  extrusionScaleSelector,
+  otherSelector,
+} from '../orbReducer';
 
 const DEFAULT_COLUMN = 'rgb',
   DEFAULT_DATE = 1583971200000;
 
 /** @type {import("typings/orbis").LayerConfiguration} */
 export default ({ id, data, orbState, activeSources }) => {
+  const extruded = extrudedModeSelector(orbState);
+  const elevationScale = extrusionScaleSelector(orbState);
   const source = find(activeSources, { source_id: id });
   const other = otherSelector(`${source.authority}/${source.namespace}/*/*`)(
     orbState,
   );
   const column = get(other, 'column', DEFAULT_COLUMN),
     date = get(other, 'date', DEFAULT_DATE);
+  /** @type {import("typings/orbis").ContinuousProperty} */
   const property = find(source.metadata.properties, { name: column });
   const colorScale = new ColorScale({
     color: property?.application.orbis.display.color,
@@ -22,22 +29,36 @@ export default ({ id, data, orbState, activeSources }) => {
   });
 
   /** @param {{properties: any}} feature */
-  const getFillColor = feature => {
-    return property
+  const getFillColor = feature =>
+    property
       ? colorScale?.get(
           find(feature?.properties[property?.name], tv => {
             return tv.timestamp.includes(format(new Date(date), 'yyyy-MM-dd'));
           })?.value,
         )
       : [255, 255, 255, 255];
-  };
+
+  const getElevation = feature =>
+    property
+      ? find(feature?.properties[property?.name], tv => {
+          return tv.timestamp.includes(format(new Date(date), 'yyyy-MM-dd'));
+        })?.value
+      : 1;
 
   return {
     id,
     data,
+    extruded,
+    elevationScale: 100 * elevationScale,
+    getElevation,
     getFillColor,
     updateTriggers: {
+      getElevation: [column, date],
       getFillColor: [column, date],
+    },
+    transitions: {
+      getElevation: 150,
+      getFillColor: 150,
     },
   };
 };
