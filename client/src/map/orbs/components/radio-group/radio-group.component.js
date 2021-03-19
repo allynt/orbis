9b2cd522d9c2d53aff8115844e1d4f5daa41fde4
Reignable <joebarret401@gmail.com>
2021-03-19 15:29:46 +0000

    chore(frontend): Turn radio group into generic component
    ORB-387

---
+++ client/src/map/orbs/components/radio-group/radio-group.component.js
@@ -0,0 +1,49 @@
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Radio,
+} from '@astrosat/astrosat-ui';
+import clsx from 'clsx';
+import React from 'react';
+
+const useStyles = makeStyles(theme => ({
+  noMinWidth: { minWidth: '0' },
+  avatar: { margin: theme.spacing(0, 2) },
+}));
+
+export const RadioGroup = ({
+  defaultValue,
+  value: valueProp = defaultValue,
+  onChange,
+  options,
+}) => {
+  const styles = useStyles();
+
+  return (
+    <List>
+      {options.map(({ value: optionValue, label, image }) => (
+        <ListItem
+          key={optionValue}
+          button
+          onClick={() => onChange(optionValue)}
+          selected={valueProp === optionValue}
+        >
+          <ListItemIcon className={clsx({ [styles.noMinWidth]: !!image })}>
+            <Radio tabIndex={-1} checked={valueProp === optionValue} />
+          </ListItemIcon>
+          {!!image ? (
+            <ListItemAvatar className={clsx(styles.noMinWidth, styles.avatar)}>
+              <Avatar src={image} />
+            </ListItemAvatar>
+          ) : null}
+          <ListItemText primary={label} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
