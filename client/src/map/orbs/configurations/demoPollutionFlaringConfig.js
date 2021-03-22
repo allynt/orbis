7f8d95fac8e8c92058a1bda2f155cf37a2341f92
Reignable <joebarret401@gmail.com>
2021-03-22 09:41:47 +0000

    feat(frontend): Add date stepping to flaring layer
    ORB-387

--- client/src/map/orbs/configurations/demoPollutionFlaringConfig.js
+++ client/src/map/orbs/configurations/demoPollutionFlaringConfig.js
@@ -1,13 +1,25 @@
-import { layersVisibilitySelector } from '../orbReducer';
+import { format, subHours } from 'date-fns';
+import { filter } from 'lodash';
+import { layersVisibilitySelector, otherSelector } from '../orbReducer';
 
 /** @type {import("typings/orbis").LayerConfiguration} */
 export default ({ id, data, orbState }) => {
   const visible = layersVisibilitySelector(id)(orbState);
-  console.log(data);
+  const other = otherSelector(id)(orbState);
+
   return {
     id,
     visible,
-    data: data?.features,
+    data: other?.date
+      ? filter(data?.features, {
+          properties: {
+            timestamp: format(
+              subHours(new Date(other?.date), 1),
+              'yyyy-MM-dd HH:mm:ss',
+            ),
+          },
+        })
+      : data?.features,
     getPosition: d => d.geometry.coordinates,
   };
 };
