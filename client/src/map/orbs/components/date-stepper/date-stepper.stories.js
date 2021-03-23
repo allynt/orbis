ab903d0feb78f27b8a17bab1d26df575a49ef5a5
Reignable <joebarret401@gmail.com>
2021-03-23 12:56:31 +0000

    chore(frontend): Add story for DateStepper
    ORB-387

---
+++ client/src/map/orbs/components/date-stepper/date-stepper.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { DateStepper } from './date-stepper.component';
+
+export default {
+  title: 'Sidebar Components/Date Stepper',
+  argType: { onChange: { action: true } },
+};
+
+const Template = args => <DateStepper {...args} />;
+
+export const Default = Template.bind();
+Default.args = {
+  defaultValue: new Date(2020, 0).getTime(),
+  dates: [
+    { value: new Date(2020, 0).getTime(), label: '1/2020' },
+    { value: new Date(2020, 1).getTime(), label: '2/2020' },
+    { value: new Date(2020, 2).getTime(), label: '3/2020' },
+  ],
+  min: new Date(2020, 0).getTime(),
+  max: new Date(2020, 11).getTime(),
+};
