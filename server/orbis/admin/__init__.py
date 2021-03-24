bcf80096acf67b1e5446ca7d2f6b80cffa20f0a6
Allyn Treshansky <allyn.treshansky@gmail.com>
2021-03-24 08:39:06 +0000

    feat(backend): adding models for terms & privacy docuemnts

--- server/orbis/admin/__init__.py
+++ server/orbis/admin/__init__.py
@@ -1,7 +1,7 @@
 from .admin_settings import OrbisSettingsAdmin
 from .admin_users import OrbisUserProfileAdmin
+from .admin_documents import TermsDocumentAdmin, PrivacyDocumentAdmin
 from .admin_orbs import OrbAdmin, LicenceAdmin, DataScopeAdmin
-
 from .admin_orders import OrderTypeAdmin, OrderAdmin, OrderItemAdmin
 from .admin_satellites import (
     SatelliteAdmin,
--- server/orbis/models/__init__.py
+++ server/orbis/models/__init__.py
@@ -1,5 +1,6 @@
 from .models_settings import OrbisSettings
 from .models_users import OrbisUserProfile
+from .models_documents import TermsDocument, PrivacyDocument
 from .models_orbs import Access, Orb, DataScope, Licence
 from .models_orders import OrderType, Order, OrderItem
 from .models_satellites import (
