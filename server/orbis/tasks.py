3c8affe40da338963cce5cc0bfc209c75df15032
Rikki Guy <git@rikkiguy.me.uk>
2021-03-26 14:56:49 +0000

    [ORB 223] User/Customer metrics (#1149)
    * fix(analytics): Add user metrics task
    
    * chore(analytics): Add new metrics to ES schema
    
    * chore(analytics): Update comments
    
    * chore(analytics): Rename log stream to "analytics"
    
    As these are the events being sent by the application, as opposed to
    events sent from any other source e.g. healthchecker
    
    * chore(analytics): Specify stream in ES schema

--- server/orbis/tasks.py
+++ server/orbis/tasks.py
@@ -1,9 +1,64 @@
+import logging
+import json
+from rest_framework.utils.encoders import JSONEncoder
 from celery import shared_task
 
 from django.contrib.auth import get_user_model
+from django.db.models import Count, Q
+from orbis.models import LicencedCustomer as Customer
 
 User = get_user_model()
+
+metrics_logger = logging.getLogger("metrics")
 
 @shared_task()
 def orbis_debug_task(*args, **kwargs):
     print(f"Orbis - Task Args: {args!r} {kwargs!r}")
+
+
+@shared_task()
+def log_metrics():
+
+    #
+    # These are potentially heavy hitting database queries (full table scans).
+    # If we get a lot of users or customers, we may need to optimise.
+    #
+
+    # One Document for Global Metrics
+    global_metrics = {
+        "type": "orbisGlobalMetrics",
+        "orbisGlobalMetrics": {
+            "totalUserCount": User.objects.count(),
+
+            "totalCustomerCount": Customer.objects.count(),
+            "activeCustomerCount": Customer.objects.filter(is_active=True).count(),
+            "inactiveCustomerCount": Customer.objects.filter(is_active=False).count(),
+        },
+    }
+
+    metrics_logger.info(
+        json.dumps(global_metrics, cls=JSONEncoder),
+    )
+
+    # One Document per Customer for Customer Metrics
+    customers_query = Customer.objects.filter(
+        is_active=True,
+    ).annotate(
+        user_count=Count('users'),
+    ).only(
+        'id', 'name',
+    )
+
+    for customer in customers_query:
+        customer_metrics = {
+            "type": "orbisCustomerMetrics",
+            "orbisCustomerMetrics": {
+                "customerId": customer.id,
+                "customerName": customer.name,
+                "totalUserCount": customer.user_count,
+            },
+        }
+
+        metrics_logger.info(
+            json.dumps(customer_metrics, cls=JSONEncoder),
+        )
