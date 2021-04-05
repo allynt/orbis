63bce43adc24e97e59e4808773aafd4951063fa9
Reignable <joebarret401@gmail.com>
2021-04-05 13:32:09 +0100

    chore(frontend): Fix PropertyTimeSeriesChart Stories

--- client/src/analysis-panel/property-time-series-chart/property-time-series-chart.stories.js
+++ client/src/analysis-panel/property-time-series-chart/property-time-series-chart.stories.js
@@ -1,14 +1,10 @@
-import faker from 'faker';
 import * as React from 'react';
 
-import { Provider } from 'react-redux';
-import configureMockStore from 'redux-mock-store';
+import isChromatic from 'chromatic/isChromatic';
+import addDays from 'date-fns/addDays';
+import faker from 'faker';
 
 import { PropertyTimeSeriesChart } from './property-time-series-chart.component';
-import addDays from 'date-fns/addDays';
-import isChromatic from 'chromatic/isChromatic';
-
-const mockStore = configureMockStore();
 
 if (isChromatic()) faker.seed(1);
 
@@ -16,76 +12,46 @@ export default { title: 'Analysis Panel/Property Time Series Chart' };
 
 const selectedProperty = { name: 'Dogs' };
 
-const Template = args => {
-  return (
-    <Provider
-      store={mockStore({
-        orbs: {
-          isolationPlus: {
-            property: selectedProperty,
-            ...args.orbState,
-          },
-        },
-      })}
-    >
-      <PropertyTimeSeriesChart {...args} />
-    </Provider>
-  );
-};
+const Template = args => <PropertyTimeSeriesChart {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  orbState: {},
-};
 
 export const OneFeature = Template.bind({});
-
-const oneFeature = [
-  {
-    object: {
-      properties: {
-        Dogs: Array(20)
-          .fill(undefined)
-          .map((_, i) => ({
-            timestamp: addDays(new Date(2077, 10, 24), i).toISOString(),
-            value: faker.random.number(100),
-          })),
-      },
-    },
-  },
-];
-
 OneFeature.args = {
   selectedProperty,
-  clickedFeatures: oneFeature,
-  orbState: {
-    clickedFeatures: oneFeature,
-  },
+  clickedFeatures: [
+    {
+      object: {
+        properties: {
+          Dogs: Array(20)
+            .fill(undefined)
+            .map((_, i) => ({
+              timestamp: addDays(new Date(2077, 10, 24), i).toISOString(),
+              value: faker.random.number(100),
+            })),
+        },
+      },
+    },
+  ],
 };
 
 export const MultipleFeatures = Template.bind({});
-
-const multipleFeatures = Array(3)
-  .fill(undefined)
-  .map(() => ({
-    object: {
-      properties: {
-        Dogs: Array(20)
-          .fill(undefined)
-          .map((_, i) => ({
-            timestamp: addDays(new Date(2077, 10, 24), i).toISOString(),
-            value: faker.random.number(100),
-          })),
-      },
-    },
-  }));
-
 MultipleFeatures.args = {
   selectedProperty,
-  clickedFeatures: multipleFeatures,
-  orbState: {
-    clickedFeatures: multipleFeatures,
-  },
+  clickedFeatures: Array(3)
+    .fill(undefined)
+    .map(() => ({
+      object: {
+        properties: {
+          Dogs: Array(20)
+            .fill(undefined)
+            .map((_, i) => ({
+              timestamp: addDays(new Date(2077, 10, 24), i).toISOString(),
+              value: faker.random.number(100),
+            })),
+        },
+      },
+    })),
 };
 
 export const Averaged = Template.bind({});
