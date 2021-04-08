b0840c86484c2f1a39fbb9afec0df4a664fffa54
Reignable <joebarret401@gmail.com>
2021-04-08 14:29:54 +0100

    chore(frontend): Add stories for ImageList
    ORB-468

---
+++ client/src/components/image-list/image-list-item/image-list-item.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { CogIcon } from '@astrosat/astrosat-ui';
+
+import faker from 'faker/locale/en_GB';
+
+import { ImageListItem } from './image-list-item.component';
+
+export default {
+  title: 'Components/Image List/Image List Item',
+  argTypes: { onChange: { action: true } },
+};
+
+const Template = args => <ImageListItem {...args} />;
+
+export const Nothing = Template.bind({});
+
+export const Image = Template.bind({});
+Image.args = {
+  src: faker.image.image(),
+  alt: 'Some Random Image alt text',
+  text: 'Some Random Image',
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  icon: <CogIcon />,
+  text: 'Using an Icon',
+};
+
+export const Selected = Template.bind({});
+Selected.args = {
+  ...Icon.args,
+  text: 'This is selected',
+  value: 1,
+  selectedValue: 1,
+};
---
+++ client/src/components/image-list/image-list.stories.js
@@ -0,0 +1,42 @@
+import { CogIcon, EyeIcon, PinIcon } from '@astrosat/astrosat-ui';
+import faker from 'faker/locale/en_GB';
+import React from 'react';
+import { ImageListItem } from './image-list-item/image-list-item.component';
+import { ImageList } from './image-list.component';
+
+export default {
+  title: 'Components/Image List',
+  argTypes: { onChange: { action: true } },
+};
+
+export const WithImages = args => (
+  <ImageList {...args} name="test-group">
+    {Array(10)
+      .fill(undefined)
+      .map(() => (
+        <ImageListItem
+          key={faker.random.uuid()}
+          src={faker.image.animals()}
+          text={faker.commerce.productName()}
+          value={faker.commerce.productName()}
+        />
+      ))}
+  </ImageList>
+);
+
+const icons = [<CogIcon />, <EyeIcon />, <PinIcon />];
+
+export const WithIcons = args => (
+  <ImageList {...args} name="test-group">
+    {Array(10)
+      .fill(undefined)
+      .map(() => (
+        <ImageListItem
+          key={faker.random.uuid()}
+          icon={faker.random.arrayElement(icons)}
+          text={faker.commerce.productName()}
+          value={faker.commerce.productName()}
+        />
+      ))}
+  </ImageList>
+);
