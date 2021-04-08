fc9987b6c0c38a928f816ca75ba78bf17fad0fa4
Reignable <joebarret401@gmail.com>
2021-04-08 09:15:05 +0100

    chore(frontend): Remove old annotations stuff
    ORB-468

--- client/src/annotations/color-picker.component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { SketchPicker } from 'react-color';
-
-const ColorPicker = ({ colour, setColour }) => (
-  <SketchPicker color={colour} onChangeComplete={c => setColour(c)} />
-);
-
-export default ColorPicker;
