96130570e5e2aecc10f4bc547a95cd72c421fd6e
Reignable <joebarret401@gmail.com>
2021-04-22 08:57:42 +0100

    chore(frontend): Fix useSelectionTools tests
    ORB-365

--- client/src/map/useSelectionTools.test.js
+++ client/src/map/useSelectionTools.test.js
@@ -1,15 +1,27 @@
+import React from 'react';
 import { fireEvent } from '@testing-library/dom';
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook as tlRenderHook } from '@testing-library/react-hooks';
 import { useSelectionTools } from './useSelectionTools';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+
+const mockStore = configureMockStore();
+
+const renderHook = ({ defaultIsTriggerKeyHeld = undefined } = {}) =>
+  tlRenderHook(() => useSelectionTools({ defaultIsTriggerKeyHeld }), {
+    wrapper: ({ children }) => (
+      <Provider store={mockStore()}>{children}</Provider>
+    ),
+  });
 
 describe('useSelectionTools', () => {
   it('Does not return a selection layer if a key is not held', () => {
-    const { result } = renderHook(() => useSelectionTools());
+    const { result } = renderHook();
     expect(result.current.selectionLayer).toBeFalsy();
   });
 
   it('Returns a selection layer when control is held', () => {
-    const { result } = renderHook(() => useSelectionTools());
+    const { result } = renderHook();
     act(() => {
       fireEvent.keyDown(document, { key: 'Control' });
     });
@@ -17,7 +29,7 @@ describe('useSelectionTools', () => {
   });
 
   it('Returns a selection layer when Cmd is held on Mac', () => {
-    const { result } = renderHook(() => useSelectionTools());
+    const { result } = renderHook();
     navigator.__defineGetter__('appVersion', () => 'MacOS');
     act(() => {
       fireEvent.keyDown(document, { metaKey: true });
@@ -26,9 +38,7 @@ describe('useSelectionTools', () => {
   });
 
   it('Stops returning the selection layer when control is released', () => {
-    const { result } = renderHook(() =>
-      useSelectionTools({ defaultIsTriggerKeyHeld: true }),
-    );
+    const { result } = renderHook();
     act(() => {
       fireEvent.keyUp(document, { key: 'Control' });
     });
@@ -36,9 +46,7 @@ describe('useSelectionTools', () => {
   });
 
   it('Stops returning the selection layer when Cmd is released on Mac', () => {
-    const { result } = renderHook(() =>
-      useSelectionTools({ defaultIsTriggerKeyHeld: true }),
-    );
+    const { result } = renderHook();
     navigator.__defineGetter__('appVersion', () => 'MacOS');
     act(() => {
       fireEvent.keyUp(document, { metaKey: true });
