8d44d2991d2cd1b5eb09f9f37d5e4286a66c9f46
Reignable <joebarret401@gmail.com>
2021-05-04 14:51:56 +0100

    chore(frontend): Correct key
    ORB-290

--- client/src/analysis-panel/clicked-features-summary/components/clicked-feature-chips.component.js
+++ client/src/analysis-panel/clicked-features-summary/components/clicked-feature-chips.component.js
@@ -28,7 +28,7 @@ export const ClickedFeatureChips = ({
         <Collapse in={open} collapsedHeight="1.6rem">
           <Grid container spacing={1}>
             {clickedFeatures?.map(feature => (
-              <Fade in key={feature.index}>
+              <Fade in key={feature.object.properties[fallbackProperty]}>
                 <TooltipChip
                   feature={feature}
                   isOnlyFeature={clickedFeatures.length === 1}
