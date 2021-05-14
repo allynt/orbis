2a0cb07832e0179b59544931cb84100ffb0993de
Reignable <joebarret401@gmail.com>
2021-05-14 11:01:24 +0100

    chore(frontend): Create material based range slider
    ORB-483

---
+++ client/src/components/material-colormap-range-slider/material-colormap-range-slider.component.js
@@ -0,0 +1,114 @@
+import { makeStyles, Slider } from '@astrosat/astrosat-ui';
+import { scaleLinear } from 'd3-scale';
+import React, { forwardRef } from 'react';
+import { ColorScale } from 'utils/ColorScale';
+
+const useStyles = makeStyles(({ palette, typography: { pxToRem } }) => ({
+  root: {
+    height: pxToRem(42),
+  },
+  rail: {
+    height: pxToRem(42),
+    opacity: 1,
+    border: `1px solid ${palette.grey[300]}`,
+    backgroundImage: props =>
+      `linear-gradient(to right, ${props.colorScale
+        .getGradient('hex')
+        .map(stop => `${stop.color} ${stop.stop}%`)
+        .join(', ')})`,
+  },
+  thumb: {
+    height: pxToRem(54),
+    width: pxToRem(10),
+    borderRadius: '100vh',
+  },
+  mark: { display: 'none' },
+  markLabel: { top: '-0.5em' },
+}));
+
+/**
+ * @typedef {{
+ *   clipMax: number
+ *   clipMin: number
+ *   color: ColorScale['color']
+ *   max: number
+ *   min: number
+ *   onChange: (value: [number, number]) => void
+ *   precision?: number
+ *   reversed?: boolean
+ *   type?: import('typings/orbis').PropertyType
+ *   value?: [number, number]
+ * }} ColormapRangeSliderProps
+ */
+
+export const MaterialColormapRangeSlider = forwardRef(
+  /**
+   * @param {ColormapRangeSliderProps} props
+   * @param {import('react').ForwardedRef<HTMLSpanElement>} ref
+   */
+  (
+    {
+      max: maxProp,
+      min: minProp,
+      clipMax: clipMaxProp,
+      clipMin: clipMinProp,
+      color,
+      onChange,
+      precision,
+      reversed,
+      type,
+      value,
+    },
+    ref,
+  ) => {
+    let min = minProp,
+      max = maxProp,
+      clipMin,
+      clipMax;
+    if (type === 'decile') {
+      min = 1;
+      max = 10;
+    }
+    clipMin = min;
+    clipMax = max;
+
+    const colorScale = new ColorScale({
+      color,
+      domain: [min, max],
+      clip: [clipMin, clipMax],
+      reversed,
+    });
+    const scale = scaleLinear().domain([min, max]);
+    const classes = useStyles({ colorScale });
+
+    const hasMoved = value[0] !== min || value[1] !== max;
+
+    /**
+     * @param {React.ChangeEvent<{}>} _event
+     * @param {[number, number]} value
+     * @returns
+     */
+    const handleChange = (_event, value) => onChange(value);
+
+    return (
+      <Slider
+        ref={ref}
+        classes={classes}
+        max={max}
+        min={min}
+        onChange={handleChange}
+        value={value}
+        track={false}
+        marks={
+          hasMoved
+            ? [
+                { value: value[0], label: value[0] },
+                { value: value[1], label: value[1] },
+              ]
+            : scale.ticks(3).map(tick => ({ value: tick, label: tick }))
+        }
+        step={precision ? Number(`1e-${precision}`) : 1}
+      />
+    );
+  },
+);
---
+++ client/src/components/material-colormap-range-slider/material-colormap-range-slider.stories.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { MaterialColormapRangeSlider } from './material-colormap-range-slider.component';
+import { action } from '@storybook/addon-actions';
+
+const COLOR_MAPS = [
+  'OrRd',
+  'PuBu',
+  'BuPu',
+  'Oranges',
+  'BuGn',
+  'YlOrBr',
+  'YlGn',
+  'Reds',
+  'RdPu',
+  'Greens',
+  'YlGnBu',
+  'Purples',
+  'GnBu',
+  'Greys',
+  'YlOrRd',
+  'PuRd',
+  'Blues',
+  'PuBuGn',
+  'Spectral',
+  'RdYlGn',
+  'RdBu',
+  'PiYG',
+  'PRGn',
+  'RdYlBu',
+  'BrBG',
+  'RdGy',
+  'PuOr',
+  'Set2',
+  'Accent',
+  'Set1',
+  'Set3',
+  'Dark2',
+  'Paired',
+  'Pastel2',
+  'Pastel1',
+];
+
+export default {
+  title: 'Components/MaterialColorMapRangeSlider',
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: COLOR_MAPS,
+      },
+      defaultValue: 'Spectral',
+    },
+  },
+};
+
+const Template = args => {
+  const [value, setValue] = React.useState([args.min ?? 1, args.max ?? 10]);
+  return (
+    <div style={{ padding: '2rem' }}>
+      <MaterialColormapRangeSlider
+        onChange={setValue}
+        value={value}
+        {...args}
+      />
+    </div>
+  );
+};
+
+export const Percentage = Template.bind({});
+Percentage.args = {
+  type: 'continuous',
+  units: '%',
+  min: 0,
+  max: 100,
+};
+
+export const Decile = Template.bind({});
+Decile.args = {
+  snap: true,
+  type: 'decile',
+};
+
+export const Continuous = Template.bind({});
+Continuous.args = { type: 'continuous', min: 300, max: 1000 };
+
+export const NegativeDomain = Template.bind({});
+NegativeDomain.args = { type: 'continuous', min: -100, max: 100 };
+
+export const SmallDomain = Template.bind({});
+SmallDomain.args = { min: 0.1, max: 0.9, precision: 2 };
+
+export const Reversed = Template.bind({});
+Reversed.args = {
+  ...Continuous.args,
+  reversed: true,
+};
+
+export const ReversedDecile = Template.bind({});
+ReversedDecile.args = {
+  ...Decile.args,
+  reversed: true,
+};
+
+export const Clipped = Template.bind({});
+Clipped.args = {
+  min: 0,
+  max: 100,
+  clipMin: 20,
+  clipMax: 70,
+};
+
+export const LongValues = Template.bind({});
+LongValues.args = {
+  min: -1000000,
+  max: 1000000,
+};
