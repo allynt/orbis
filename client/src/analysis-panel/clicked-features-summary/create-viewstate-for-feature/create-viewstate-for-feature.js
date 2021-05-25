ec316e0c84f5eb2b52da2597ab726427c6d48e14
Rory MacGregor <rorymacgregor@astrosat.net>
2021-05-25 13:17:00 +0100

    fix(frontend): Some refactoring of the viewstate/feature function
    Made the ponts data a bit neater through destructuring.

--- client/src/analysis-panel/clicked-features-summary/create-viewstate-for-feature/create-viewstate-for-feature.js
+++ client/src/analysis-panel/clicked-features-summary/create-viewstate-for-feature/create-viewstate-for-feature.js
@@ -1,16 +1,17 @@
 import { bbox } from '@turf/turf';
 import { fitBounds } from '@math.gl/web-mercator';
 
-export const createViewstateForFeature = ({ feature, viewport }) => {
+export const createViewstateForFeature = ({
+  feature,
+  viewport,
+  padding = 50,
+}) => {
   if (!feature || !viewport) return;
 
   const { width, height } = viewport,
     points = bbox(feature?.object?.geometry);
 
-  const minX = points[0],
-    maxX = points[2],
-    minY = points[1],
-    maxY = points[3];
+  const [minX, minY, maxX, maxY] = points;
 
   const bounds = [
     [minX, minY],
@@ -21,7 +22,7 @@ export const createViewstateForFeature = ({ feature, viewport }) => {
     bounds,
     width,
     height,
-    padding: 50,
+    padding,
   });
 
   return {
