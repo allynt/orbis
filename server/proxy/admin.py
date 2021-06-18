d7942cd0812ae029d8903e42c0b9beaf147621d9
Allyn Treshansky <allyn.treshansky@gmail.com>
2021-06-18 12:14:41 +0100

    feat(backend): added authentication for remote proxy APIs
    Added authentication for remote proxy APIs.
    
    Refactored adapter for Spire API.

--- server/proxy/admin.py
+++ server/proxy/admin.py
@@ -1,17 +1,19 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.forms import Select, ModelForm
+from django.utils.translation import gettext as _
 
 from astrosat.admin import JSONAdminWidget
 
-from proxy.models import ProxyDataSource
 from proxy.adapters import PROXY_DATA_ADAPTER_REGISTRY
+from proxy.authentication import ProxyAuthentication
+from proxy.models import ProxyDataSource
 
 
 class ProxyDataSourceAdminForm(ModelForm):
     """
-    A custom Admin Form that makes the JSONField pretty and
-    restricts the adapter name to only those adapters defined
-    in the PROXY_DATA_ADAPTER_REGISTRY
+    A custom Admin Form that makes the proxy_params and proxy_authentication_token
+    fields pretty and restricts the adapter name to only those adapters defined in
+    the PROXY_DATA_ADAPTER_REGISTRY
     """
     class Meta:
         model = ProxyDataSource
@@ -20,6 +22,9 @@ class ProxyDataSourceAdminForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["proxy_params"].widget = JSONAdminWidget()
+        self.fields["proxy_authentication_token"].widget.attrs.update({
+            "cols": 80, "class": "vLargeTextField"
+        })
         self.fields["adapter_name"].widget = Select(
             choices=((adapter_name, adapter_name)
                      for adapter_name in sorted(PROXY_DATA_ADAPTER_REGISTRY))
@@ -41,3 +46,62 @@ class ProxyDataSourceAdmin(admin.ModelAdmin):
         "name",
         "version",
     )
+
+    def save_model(self, request, obj, form, change):
+
+        authentication_type = obj.proxy_authentication_type
+
+        if authentication_type == ProxyAuthentication.AuthenticationTypes.BASIC:
+            if not (
+                obj.proxy_authentication_username and
+                obj.proxy_authentication_password
+            ):
+                msg = _(
+                    "Basic Authentication requires a username and password."
+                )
+                self.message_user(request, msg, level=messages.WARNING)
+            if obj.proxy_authentication_token:
+                msg = _("Basic Authentication will not use a token")
+                self.message_user(request, msg, level=messages.WARNING)
+
+        elif authentication_type == ProxyAuthentication.AuthenticationTypes.BEARER:
+
+            if not obj.proxy_authentication_token:
+                msg = _("Bearer Authentication requires a token")
+                self.message_user(request, msg, level=messages.WARNING)
+            if (
+                obj.proxy_authentication_username or
+                obj.proxy_authentication_password
+            ):
+                msg = _(
+                    "Bearer Authentication will not use username nor password."
+                )
+                self.message_user(request, msg, level=messages.WARNING)
+
+        elif authentication_type == ProxyAuthentication.AuthenticationTypes.APIKEY:
+
+            if not obj.proxy_authentication_token:
+                msg = _("ApiKey Authentication requires a token")
+                self.message_user(request, msg, level=messages.WARNING)
+            if (
+                obj.proxy_authentication_username or
+                obj.proxy_authentication_password
+            ):
+                msg = _(
+                    "ApiKey Authentication will not use username nor password."
+                )
+                self.message_user(request, msg, level=messages.WARNING)
+
+        elif authentication_type == ProxyAuthentication.AuthenticationTypes.URL_PARAM:
+
+            if (
+                obj.proxy_authentication_token or
+                obj.proxy_authentication_username or
+                obj.proxy_authentication_password
+            ):
+                msg = _(
+                    "URL-PARAM Authentication will not use token nor username nor password."
+                )
+                self.message_user(request, msg, level=messages.WARNING)
+
+        return super().save_model(request, obj, form, change)
---
+++ server/proxy/authentication.py
@@ -0,0 +1,46 @@
+from base64 import b64encode
+from requests.auth import AuthBase
+
+from django.db.models import TextChoices
+
+
+class ProxyAuthentication(AuthBase):
+    AuthenticationTypes = TextChoices(
+        # TODO: ADD OTHER TYPES
+        "AuthenticationTypes",
+        ["BASIC", "BEARER", "APIKEY", "URL_PARAM"]
+    )
+
+    def __init__(self, proxy_data_source):
+        self.proxy_data_source = proxy_data_source
+
+    def __call__(self, request):
+        proxy_authentication_type = self.proxy_data_source.proxy_authentication_type
+        if not proxy_authentication_type:
+            return request
+
+        if proxy_authentication_type == self.AuthenticationTypes.URL_PARAM:
+            pass
+
+        elif proxy_authentication_type == self.AuthenticationTypes.BASIC:
+            request.headers["Authorization"] = "Basic " + b64encode(
+                b":".join(
+                    self.proxy_data_source.proxy_authentication_username,
+                    self.proxy_data_source.proxy_authentication_password,
+                )
+            ).strip()
+
+        elif proxy_authentication_type == self.AuthenticationTypes.BEARER:
+            request.headers[
+                "Authorization"
+            ] = "Bearer " + self.proxy_data_source.proxy_authentication_token
+
+        elif proxy_authentication_type == self.AuthenticationTypes.APIKEY:
+            request.headers[
+                "Authorization"
+            ] = "api-key" + self.proxy_data_source.proxy_authentication_token
+
+        else:
+            raise NotImplementedError(proxy_authentication_type)
+
+        return request
