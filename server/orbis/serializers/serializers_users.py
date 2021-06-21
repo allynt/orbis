904bf9ac01fe9458797e74667e5475588bba64b3
Allyn Treshansky <allyn.treshansky@gmail.com>
2021-06-21 20:54:06 +0100

    feat(backend): pass context to nested serializers
    A recent change to django-astrosat-users passes `user` to the context of
    all UserProfileSerializers.  This PR passes `user.orbis_profile` to the
    OrbisUserProfileSerializer which in turn passes it to
    OrbisUserFeedbackRecordSerializer.

--- server/orbis/serializers/serializers_users.py
+++ server/orbis/serializers/serializers_users.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from astrosat.serializers import ContextVariableDefault
+from astrosat.serializers import ContextVariableDefault, WritableNestedListSerializer
 
 from orbis.models import OrbisUserProfile, OrbisUserFeedbackRecord
 
@@ -9,17 +9,34 @@ class OrbisUserFeedbackRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrbisUserFeedbackRecord
         fields = [
+            "id",
             "timestamp",
             "provided_feedback",
             "source_ids",
             "profile",
         ]
+        list_serializer_class = WritableNestedListSerializer
+
+    id = serializers.IntegerField(read_only=True)
 
     profile = serializers.PrimaryKeyRelatedField(
         default=ContextVariableDefault("profile", raise_error=True),
         queryset=OrbisUserProfile.objects.all(),
         write_only=True,
     )
+
+    def to_internal_value(self, data):
+        # put the read-only "id" field back, since it's used to
+        # identify unique models in WriteableNestedListSerializer
+        id_field_name = "id"
+        internal_value = super().to_internal_value(data)
+        if id_field_name not in internal_value and id_field_name in data:
+            id_field_serializer = self.fields[id_field_name]
+            internal_value[id_field_name
+                          ] = id_field_serializer.to_internal_value(
+                              data[id_field_name]
+                          )
+        return internal_value
 
 
 class OrbisUserProfileSerializer(serializers.ModelSerializer):
@@ -35,3 +52,29 @@ class OrbisUserProfileSerializer(serializers.ModelSerializer):
         ]
 
     feedback_records = OrbisUserFeedbackRecordSerializer(many=True)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        # get the profile so that any ContextVariableDefault fields
+        # in nested serializers work (in-case I don't access those
+        # nested serializers via a custom view such as OrbisUserFeedbackView)
+
+        self.fields["feedback_records"].context.update({
+            "profile": self.instance or self.context["user"].orbis_profile
+        })
+
+    def update(self, instance, validated_data):
+
+        feedback_records_serializer = self.fields["feedback_records"]
+        feedback_records_data = validated_data.pop(
+            feedback_records_serializer.source
+        )
+        feedback_records_serializer.crud(
+            instances=instance.feedback_records.all(),
+            validated_data=feedback_records_data,
+            delete_missing=False,
+        )
+        user_profile = super().update(instance, validated_data)
+        return user_profile
--- server/orbis/views/__init__.py
+++ server/orbis/views/__init__.py
@@ -1,6 +1,6 @@
 from .views_data import TokenView, DataSourceView
 from .views_customers import CustomerCreateView, CustomerUpdateView, CustomerUserListView, CustomerUserDetailView, CustomerUserInviteView, CustomerUserOnboardView
-from .views_users import OrbisUserFeedbackView
+from .views_users import OrbisUserFeedbackRecordView
 from .views_documents import DocumentView
 from .views_orbs import OrbListView
 from .views_orders import OrderViewSet
