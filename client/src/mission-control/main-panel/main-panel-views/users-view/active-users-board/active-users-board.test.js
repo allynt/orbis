8046e98682e2516d045914cb3a4b43cf743fe257
Rory MacGregor <rorymacgregor@astrosat.net>
2021-06-23 09:55:41 +0100

    fix(frontend): Add tests and stories
    Added test and story files for the Mission Control components.

---
+++ client/src/mission-control/main-panel/main-panel-views/users-view/active-users-board/active-users-board.test.js
@@ -0,0 +1,185 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { customer, activeUsers } from '../../test-story-data';
+import { ActiveUsersBoard } from './active-users-board.component';
+
+describe('ActiveUsersBoard', () => {
+  const cases = [
+    ['names', 'name'],
+    ["email address'", 'email'],
+  ];
+  const onChangeRoleClick = jest.fn();
+  const onEditUserClick = jest.fn();
+  const onDeleteUserClick = jest.fn();
+
+  it.each(cases)("Displays all active user's %s", (_, text) => {
+    const { getByText } = render(
+      <ActiveUsersBoard
+        activeCustomerUsers={activeUsers}
+        customer={customer}
+      />,
+    );
+    activeUsers.forEach(user =>
+      expect(getByText(user.user[text])).toBeInTheDocument(),
+    );
+  });
+
+  it('Displays user Orb licence names', () => {
+    const { getAllByText } = render(
+      <ActiveUsersBoard
+        activeCustomerUsers={activeUsers}
+        customer={customer}
+      />,
+    );
+
+    expect(getAllByText('Oil, Rice')[0]).toBeInTheDocument();
+  });
+
+  it('Displays a placeholder when customer is present but has no licences', () => {
+    const { getAllByText, queryByText } = render(
+      <ActiveUsersBoard
+        activeCustomerUsers={activeUsers}
+        customer={{ name: 'Customer Name' }}
+      />,
+    );
+
+    expect(queryByText('No licences')).not.toBeInTheDocument();
+
+    activeUsers.forEach((_, i) =>
+      expect(getAllByText('Not currently available')[i]).toBeInTheDocument(),
+    );
+  });
+
+  it('Displays a placeholder when user has no licences', () => {
+    const TEST_USER = {
+      ...activeUsers[0],
+      id: '99',
+    };
+
+    const { getByText, queryByText } = render(
+      <ActiveUsersBoard
+        activeCustomerUsers={[TEST_USER]}
+        customer={customer}
+      />,
+    );
+
+    expect(queryByText('Not currently available')).not.toBeInTheDocument();
+    expect(getByText('No licences')).toBeInTheDocument();
+  });
+
+  it('Disables `Change Role` button when only 1 admin remains', () => {
+    const { getByRole } = render(
+      <ActiveUsersBoard
+        activeCustomerUsers={[
+          { type: 'MANAGER', user: { id: '123', name: 'John Smith' } },
+          { type: 'MEMBER', user: { id: '456', name: 'Steve Brown' } },
+        ]}
+        oneAdminRemaining={true}
+        customer={{ name: 'Customer Name' }}
+      />,
+    );
+    expect(getByRole('button', { name: 'Admin' })).toBeDisabled();
+  });
+
+  it('Calls `changeRole` function with user when buttons are clicked', () => {
+    const MEMBER = { type: 'MEMBER', user: { id: '123', name: 'Steve Brown' } };
+
+    const { getByText, getAllByText } = render(
+      <ActiveUsersBoard
+        activeCustomerUsers={[
+          { type: 'MANAGER', user: { id: '456', name: 'John Smith' } },
+          MEMBER,
+        ]}
+        customer={{ name: 'Customer Name' }}
+        onChangeRoleClick={onChangeRoleClick}
+      />,
+    );
+
+    userEvent.click(getByText('Standard'));
+
+    expect(getAllByText('Admin').length).toEqual(2);
+
+    userEvent.click(getAllByText('Admin')[1]);
+    expect(onChangeRoleClick).toHaveBeenCalledWith(MEMBER);
+  });
+
+  it('Calls `editCustomerUser` function with user when buttons are clicked', () => {
+    const USER = { type: 'MEMBER', user: { id: '123', name: 'Steve Brown' } };
+
+    const { getByText, getAllByTestId } = render(
+      <ActiveUsersBoard
+        currentUser={{ id: '456' }}
+        activeCustomerUsers={[
+          { type: 'MANAGER', user: { id: '456', name: 'John Smith' } },
+          USER,
+        ]}
+        customer={{ name: 'Customer Name' }}
+        onEditUserClick={onEditUserClick}
+      />,
+    );
+
+    userEvent.click(getAllByTestId('options-icon')[1]);
+    expect(getByText('Edit')).toBeInTheDocument();
+
+    userEvent.click(getByText('Edit'));
+    expect(onEditUserClick).toHaveBeenCalledWith(USER);
+  });
+
+  it('Does not show `Delete User` button for currently logged-in user', () => {
+    const { queryByText, getByText, getAllByTestId } = render(
+      <ActiveUsersBoard
+        currentUser={{ id: '123' }}
+        activeCustomerUsers={[
+          { type: 'MANAGER', user: { id: '123', name: 'John Smith' } },
+          { type: 'MEMBER', user: { id: '456', name: 'Steve Brown' } },
+        ]}
+      />,
+    );
+
+    userEvent.click(getAllByTestId('options-icon')[0]);
+    expect(queryByText('Delete User')).not.toBeInTheDocument();
+
+    userEvent.click(getAllByTestId('options-icon')[1]);
+    expect(getByText('Delete User')).toBeInTheDocument();
+  });
+
+  it('Calls `deleteCustomerUser` function with user when buttons are clicked', () => {
+    const USER = { type: 'MEMBER', user: { id: '123', name: 'Steve Brown' } };
+
+    const { getByText, getAllByTestId } = render(
+      <ActiveUsersBoard
+        currentUser={{ id: '456' }}
+        activeCustomerUsers={[
+          { type: 'MANAGER', user: { id: '456', name: 'John Smith' } },
+          USER,
+        ]}
+        customer={{ name: 'Customer Name' }}
+        onDeleteUserClick={onDeleteUserClick}
+      />,
+    );
+
+    userEvent.click(getAllByTestId('options-icon')[1]);
+    expect(getByText('Delete User')).toBeInTheDocument();
+
+    userEvent.click(getByText('Delete User'));
+    expect(onDeleteUserClick).toHaveBeenCalledWith(USER);
+  });
+
+  describe('Displays a placeholder when there are no active users', () => {
+    const cases = [
+      ['undefined', undefined],
+      ['null', null],
+      ['empty array', []],
+    ];
+
+    it.each(cases)('%s', (_, value) => {
+      const { getByText } = render(
+        <ActiveUsersBoard activeCustomerUsers={value} customer={customer} />,
+      );
+      expect(getByText('No Active Users')).toBeInTheDocument();
+    });
+  });
+});
---
+++ client/src/mission-control/main-panel/main-panel-views/users-view/pending-invitations-board/pending-invitations-board.test.js
@@ -0,0 +1,128 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { customer, pendingUsers } from '../../test-story-data';
+import { PendingInvitationsBoard } from './pending-invitations-board.component';
+
+describe('PendingUsersBoard', () => {
+  const cases = [
+    ['names', 'name'],
+    ["email address'", 'email'],
+  ];
+  const onWithdrawInvitationClick = jest.fn();
+  const onResendInvitationClick = jest.fn();
+  it.each(cases)("Displays all pending user's %s", (_, text) => {
+    const { getByText } = render(
+      <PendingInvitationsBoard
+        pendingUsers={pendingUsers}
+        customer={customer}
+      />,
+    );
+    pendingUsers.forEach(user =>
+      expect(getByText(user.user[text])).toBeInTheDocument(),
+    );
+  });
+
+  it('Displays user Orb licence names', () => {
+    const { getAllByText } = render(
+      <PendingInvitationsBoard
+        pendingUsers={pendingUsers}
+        customer={customer}
+      />,
+    );
+
+    expect(getAllByText('Oil, Rice')[0]).toBeInTheDocument();
+  });
+
+  it('Displays a placeholder if no customer is present', () => {
+    const { getAllByText } = render(
+      <PendingInvitationsBoard pendingUsers={pendingUsers} customer={null} />,
+    );
+
+    pendingUsers.forEach((user, i) =>
+      expect(getAllByText('Not currently available')[i]).toBeInTheDocument(),
+    );
+  });
+
+  it('Displays a placeholder when customer is present but has no licences', () => {
+    const { getAllByText, queryByText } = render(
+      <PendingInvitationsBoard
+        pendingUsers={pendingUsers}
+        customer={{ name: 'Customer Name' }}
+      />,
+    );
+
+    expect(queryByText('No licences')).not.toBeInTheDocument();
+
+    pendingUsers.forEach((user, i) =>
+      expect(getAllByText('Not currently available')[i]).toBeInTheDocument(),
+    );
+  });
+
+  it('Displays a placeholder when user has no licences', () => {
+    const TEST_USER = {
+      ...pendingUsers[0],
+      id: '99',
+    };
+
+    const { getByText, queryByText } = render(
+      <PendingInvitationsBoard
+        pendingUsers={[TEST_USER]}
+        customer={customer}
+      />,
+    );
+
+    expect(queryByText('Not currently available')).not.toBeInTheDocument();
+    expect(getByText('No licences')).toBeInTheDocument();
+  });
+
+  it('Calls resendInvitation when `Resend Invitation` button is clicked', () => {
+    const { getAllByText } = render(
+      <PendingInvitationsBoard
+        pendingUsers={pendingUsers}
+        customer={customer}
+        onResendInvitationClick={onResendInvitationClick}
+      />,
+    );
+
+    const resendInvitationButton = getAllByText('Resend Invitation')[0];
+    expect(resendInvitationButton).toBeInTheDocument();
+    userEvent.click(resendInvitationButton);
+    expect(onResendInvitationClick).toHaveBeenCalledWith(pendingUsers[0]);
+  });
+
+  it('Opens `Withdraw Invitation` dialog when button is clicked', () => {
+    const { getByText, getAllByTestId } = render(
+      <PendingInvitationsBoard
+        pendingUsers={pendingUsers}
+        customer={customer}
+        onWithdrawInvitationClick={onWithdrawInvitationClick}
+      />,
+    );
+
+    userEvent.click(getAllByTestId('options-icon')[0]);
+
+    const optionsDropdownButton = getByText('Withdraw');
+
+    expect(optionsDropdownButton).toBeInTheDocument();
+    userEvent.click(optionsDropdownButton);
+    expect(onWithdrawInvitationClick).toHaveBeenCalledWith(pendingUsers[0]);
+  });
+
+  describe('Displays a placeholder when there are no pending users', () => {
+    const cases = [
+      ['undefined', undefined],
+      ['null', null],
+      ['empty array', []],
+    ];
+
+    it.each(cases)('%s', (_, value) => {
+      const { getByText } = render(
+        <PendingInvitationsBoard pendingUsers={value} customer={customer} />,
+      );
+      expect(getByText('No Pending Users')).toBeInTheDocument();
+    });
+  });
+});
