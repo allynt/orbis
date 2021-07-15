ddedc347c70db481820d14c0468efbb6bb134611
Rory MacGregor <rorymacgregor@astrosat.net>
2021-07-15 14:26:50 +0100

    fix(frontend): Fix up and style content wrapper
    Adde styles, correct SVG icons and logic to content wrapper component.

--- client/src/mission-control/content-wrapper.component.js
+++ client/src/mission-control/content-wrapper.component.js
@@ -1,25 +1,44 @@
 import React from 'react';
 
-import { makeStyles, Paper, Typography } from '@astrosat/astrosat-ui';
+import { makeStyles, Paper, Box, Typography } from '@astrosat/astrosat-ui';
+
+import { ReactComponent as NumberHalo } from './number-halo.svg';
 
 const useTitleStyles = makeStyles(theme => ({
-  h1: {
-    borderBottom: `1px solid ${theme.palette.primary.main}`,
-    paddingBottom: theme.spacing(0.25),
+  title: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     margin: 'auto',
     marginBottom: theme.spacing(4),
     marginTop: theme.spacing(2),
     width: 'fit-content',
-    textAlign: 'center',
+  },
+  haloIcon: {
+    height: theme.spacing(7),
+    width: theme.spacing(7),
+    marginRight: theme.spacing(2),
+  },
+  number: {
+    position: 'absolute',
+    height: 'fit-content',
+    left: '1.16rem',
+    bottom: '0.45rem',
+  },
+  h1: {
+    borderBottom: `1px solid ${theme.palette.primary.main}`,
+    padding: '0.25rem',
   },
 }));
 
 const useWrapperStyles = makeStyles(theme => ({
   wrapper: {
     padding: theme.spacing(2),
-    borderRadius: '1rem',
+    borderRadius: theme.spacing(2),
     width: '100%',
-    maxHeight: '45rem',
+    maxHeight: theme.spacing(90),
     overflowY: 'auto',
     '& + &': {
       marginTop: theme.spacing(3),
@@ -27,20 +46,22 @@ const useWrapperStyles = makeStyles(theme => ({
   },
 }));
 
-const Title = ({ children }) => {
+const Title = ({ children, number }) => {
   const styles = useTitleStyles({});
   return (
-    <Typography variant="h1" classes={styles}>
-      {children}
-    </Typography>
+    <h1 className={styles.title}>
+      <span className={styles.number}>{number}</span>
+      <NumberHalo color="#fff" className={styles.haloIcon} />
+      <span className={styles.h1}>{children}</span>
+    </h1>
   );
 };
 
-const ContentWrapper = ({ children, title }) => {
+const ContentWrapper = ({ children, title, number }) => {
   const styles = useWrapperStyles({});
   return (
     <Paper className={styles.wrapper} elevation={5}>
-      <Title>{title}</Title>
+      <Title number={number}>{title}</Title>
       {children}
     </Paper>
   );
