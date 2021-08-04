0831a2ba8be5c9edeb368a7e45f4e0733094ee97
Reignable <joebarret401@gmail.com>
2021-08-04 14:12:38 +0100

    chore(frontend):Fix SidePanelListItem tests
    ORB-534

--- client/src/mission-control/side-panel/side-panel-list-item.test.js
+++ client/src/mission-control/side-panel/side-panel-list-item.test.js
@@ -1,59 +1,29 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 
 import SidePanelListItem from './side-panel-list-item.component';
 
-const VIEW = 'Users',
-  DEFAULT_VIEW = 'DefaultView';
+const VIEW = 'Users';
 
-const renderComponent = ({ view = VIEW, mainPanelView = DEFAULT_VIEW }) => {
-  const onClick = jest.fn();
+const renderComponent = ({ view = VIEW } = {}) => {
   const Icon = () => <h1>I Am an icon</h1>;
   const utils = render(
-    <SidePanelListItem
-      view={view}
-      mainPanelView={mainPanelView}
-      onClick={onClick}
-      Icon={Icon}
-    />,
+    <SidePanelListItem view={view} Icon={Icon} to="/route" />,
+    {
+      wrapper: MemoryRouter,
+    },
   );
 
-  return { ...utils, onClick };
+  return { ...utils };
 };
 
 describe('SidePanelListItem', () => {
   it('renders a SidePanelListItem', () => {
-    const { getByText, getByLabelText } = renderComponent({});
+    const { getByText, getByLabelText } = renderComponent();
 
     expect(getByText(VIEW)).toBeInTheDocument();
     expect(getByLabelText(`${VIEW} Icon`)).toBeInTheDocument();
-  });
-
-  it('calls the onClick when the button is clicked', () => {
-    const { getByText, onClick } = renderComponent({});
-
-    userEvent.click(getByText(VIEW));
-
-    expect(onClick).toHaveBeenCalledWith(VIEW);
-  });
-
-  it('calls the click handler when the icon is clicked', () => {
-    const { getByLabelText, onClick } = renderComponent({});
-
-    userEvent.click(getByLabelText(`${VIEW} Icon`));
-
-    expect(onClick).toHaveBeenCalledWith(VIEW);
-  });
-
-  it('does not call the click handler if already selected', () => {
-    const { getByLabelText, onClick } = renderComponent({
-      mainPanelView: 'Users',
-    });
-
-    userEvent.click(getByLabelText(`${VIEW} Icon`));
-
-    expect(onClick).not.toHaveBeenCalled();
   });
 });
