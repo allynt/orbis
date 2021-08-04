1686ed8a0402348ffb6ebc614bbad61e67009fe8
Reignable <joebarret401@gmail.com>
2021-08-04 08:22:24 +0100

    chore(frontend): Improve padding in wrapper
    ORB-529

--- client/src/mission-control/store/orbs/wrapper.component.js
+++ client/src/mission-control/store/orbs/wrapper.component.js
@@ -3,11 +3,9 @@ import { Container, styled } from '@astrosat/astrosat-ui';
 export const Wrapper = styled(Container)(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   borderRadius: theme.shape.borderRadius,
-  paddingTop: theme.spacing(2),
+  paddingBlock: theme.spacing(2),
+  paddingInline: `min(5vw, ${theme.spacing(9)})`,
   [theme.breakpoints.up('sm')]: {
     paddingBottom: theme.spacing(9),
-  },
-  [theme.breakpoints.up('lg')]: {
-    paddingInline: theme.spacing(9),
   },
 }));
