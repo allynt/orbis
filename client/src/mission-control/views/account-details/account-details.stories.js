c5f25bd94168bce4b25c7cea6a827e2a30936755
Reignable <joebarret401@gmail.com>
2021-08-16 14:44:48 +0100

    chore(frontend): Add redux to stories
    ORB-530

--- client/src/mission-control/views/account-details/account-details.stories.js
+++ client/src/mission-control/views/account-details/account-details.stories.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+import createMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import { AccountDetailsComponent } from './account-details.component';
 
 export default { title: 'Mission Control/Account Details' };
 
-const Template = args => <AccountDetailsComponent {...args} />;
+const mockStore = createMockStore([thunk]);
+
+const Template = args => (
+  <Provider
+    store={mockStore({
+      accounts: {
+        user: { name: 'John Smith' },
+      },
+      missionControl: {
+        currentCustomer: { official_name: 'Test Org', id: 'test-id-123' },
+      },
+    })}
+  >
+    <AccountDetailsComponent {...args} />
+  </Provider>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
