c2e550c1b341b6bb6176ea2d365e31c80f1ceb3e
Reignable <joebarret401@gmail.com>
2021-08-19 11:50:19 +0100

    chore(frontend): Adjust Orbs to reduce illusion
    ORB-534

--- client/src/mission-control/views/store/orbs/orbs.component.js
+++ client/src/mission-control/views/store/orbs/orbs.component.js
@@ -25,15 +25,63 @@ export const Orbs = ({ orbs = [], isLoading = false }) => {
   if (smDown) cols = 2;
   if (xsDown) cols = 1;
 
+  const max = {
+    x: (orbs.length - 1) % cols,
+    y: Math.floor((orbs.length - 1) / cols),
+  };
+
   return (
     <Wrapper title="Orbis Store">
-      <ImageList cols={cols} gap={16} rowHeight="auto">
+      <ImageList cols={cols} gap={8} rowHeight="auto">
         {!isLoading
-          ? orbs.map(orb => (
-              <ImageListItem key={orb.id}>
-                <OrbCard orb={orb} />
-              </ImageListItem>
-            ))
+          ? orbs.map((orb, index) => {
+              const coords = { x: index % cols, y: Math.floor(index / cols) };
+              return (
+                <ImageListItem key={orb.id}>
+                  {/* <pre>{JSON.stringify(coords, null, 2)}</pre>
+                  <pre>
+                    {JSON.stringify(
+                      Object.entries({
+                        '↖️': index === 0,
+                        '↗️':
+                          coords.y === 0 &&
+                          (max.y === 0
+                            ? coords.x === max.x
+                            : index + 1 === cols),
+                        '↘️':
+                          (coords.x === max.x && coords.y === max.y) ||
+                          (orbs.length % cols !== 0 &&
+                            coords.y === max.y - 1 &&
+                            coords.x + 1 === cols),
+                        '↙️': coords.x === 0 && coords.y === max.y,
+                      })
+                        .map(([key, value]) => (value ? key : false))
+                        .filter(value => !!value),
+                    )}
+                  </pre> */}
+                  <OrbCard
+                    orb={orb}
+                    style={{
+                      borderTopLeftRadius: index === 0 ? 10 : 0,
+                      borderTopRightRadius:
+                        coords.y === 0 &&
+                        (max.y === 0 ? coords.x === max.x : index + 1 === cols)
+                          ? 10
+                          : 0,
+                      borderBottomRightRadius:
+                        (coords.x === max.x && coords.y === max.y) ||
+                        (orbs.length % cols !== 0 &&
+                          coords.y === max.y - 1 &&
+                          coords.x + 1 === cols)
+                          ? 10
+                          : 0,
+                      borderBottomLeftRadius:
+                        coords.x === 0 && coords.y === max.y ? 10 : 0,
+                    }}
+                  />
+                </ImageListItem>
+              );
+            })
           : Array(3)
               .fill()
               .map((_, i) => (
