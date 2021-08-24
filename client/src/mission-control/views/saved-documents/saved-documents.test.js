5a06b033a0f99f75dd0e051419ddcf3c7643cd44
Rory MacGregor <rorymacgregor@astrosat.net>
2021-08-24 09:18:07 +0100

    fix(frontend): Update tests, remove unnecessary
    Fixed test suite, and removed oned rendered unneeded by the react-table.

--- client/src/mission-control/views/saved-documents/saved-documents.test.js
+++ client/src/mission-control/views/saved-documents/saved-documents.test.js
@@ -2,31 +2,19 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
-import createMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import faker from 'faker/locale/en_GB';
 
-import SavedDocuments, { TEST_DOCUMENTS } from './saved-documents.component';
+import SavedDocuments from './saved-documents.component';
 
-const mockStore = createMockStore([thunk]);
+const TEST_DOCUMENTS = new Array(20).fill().map(() => ({
+  id: faker.random.uuid(),
+  title: faker.name.jobTitle(),
+  date: faker.date.past().toISOString(),
+  url: faker.image.imageUrl(),
+}));
 
-const DEFAULT_ROUTE = 'mission-control/saved-documents';
-
-const renderComponent = ({ initialEntries = [DEFAULT_ROUTE] }) => {
-  const utils = render(
-    <Provider
-      store={mockStore({
-        accounts: {
-          user: { id: 1 },
-        },
-      })}
-    >
-      <MemoryRouter initialEntries={initialEntries}>
-        <SavedDocuments />
-      </MemoryRouter>
-    </Provider>,
-  );
+const renderComponent = () => {
+  const utils = render(<SavedDocuments documents={TEST_DOCUMENTS} />);
   return { ...utils };
 };
 
@@ -34,41 +22,11 @@ describe('<Saved Documents />', () => {
   it('renders a saved documents table', () => {
     const { getByText } = renderComponent({});
 
+    userEvent.click(getByText('5'));
+    userEvent.click(getByText('20'));
+
     TEST_DOCUMENTS.forEach(doc => {
       expect(getByText(doc.title)).toBeInTheDocument();
-      expect(getByText(doc.date)).toBeInTheDocument();
     });
-  });
-
-  it('sorts alphabetically when icon is clicked', () => {
-    const { getByRole, getAllByRole } = renderComponent({});
-
-    userEvent.click(getByRole('button', { name: 'Name' }));
-
-    const [, ...rows] = getAllByRole('row');
-
-    const sortedRowNames = ['Test-title-1', 'Test-title-2', 'Test-title-3'];
-
-    rows.forEach((row, i) => {
-      expect(row.firstChild).toHaveTextContent(sortedRowNames[i]);
-    });
-  });
-
-  it('sorts by date when icon is clicked', () => {
-    const { getByRole, getAllByRole } = renderComponent({});
-
-    userEvent.click(getByRole('button', { name: 'Date' }));
-
-    const [, ...rows] = getAllByRole('row');
-
-    const sortedRowNames = ['01-05-2020', '02-05-2020', '03-05-2020'];
-
-    rows.forEach((row, i) => {
-      expect(row.secondChild).toHaveTextContent(sortedRowNames[i]);
-    });
-  });
-
-  it('opens document in new tab when PDF icon is clicked', () => {
-    const { getByText } = renderComponent({});
   });
 });
