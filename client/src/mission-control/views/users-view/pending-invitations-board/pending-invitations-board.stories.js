73343ab86e8d4fe04f4990ed5df3b0b1e22786b1
Reignable <joebarret401@gmail.com>
2021-08-24 09:42:01 +0100

    chore(frontend): Make more dynamic story data

--- client/src/mission-control/views/users-view/pending-invitations-board/pending-invitations-board.stories.js
+++ client/src/mission-control/views/users-view/pending-invitations-board/pending-invitations-board.stories.js
@@ -1,18 +1,56 @@
 import React from 'react';
 
-import { customer, pendingUsers } from 'mission-control/test-story-data';
+import faker from 'faker/locale/en_GB';
+
+import { Wrapper } from 'mission-control/shared-components/wrapper.component';
 
 import { PendingInvitationsBoard } from './pending-invitations-board.component';
 
 export default {
   title: 'Mission Control/Users/Pending Users Board',
   argTypes: {
-    onResendInvitationClick: { action: 'onResendInvitationClick' },
-    onWithdrawInvitationClick: { action: 'onWithdrawInvitationClick' },
+    onResendInvitationClick: { action: true },
+    onWithdrawInvitationClick: { action: true },
   },
 };
 
-const Template = args => <PendingInvitationsBoard {...args} />;
+const pendingUsers = Array(20)
+  .fill()
+  .map(() => {
+    const firstName = faker.name.firstName(),
+      lastName = faker.name.lastName();
+    return {
+      id: faker.random.uuid(),
+      status: 'PENDING',
+      licences: Array(1 + faker.random.number(5))
+        .fill()
+        .map(() => faker.random.uuid()),
+      invitation_date: faker.date.past().toISOString(),
+      user: {
+        name: `${firstName} ${lastName}`,
+        email: faker.internet.email(firstName, lastName),
+      },
+    };
+  });
+
+const licenceIds = pendingUsers.flatMap(({ licences }) => licences);
+const orbs = Array(20)
+  .fill()
+  .map(() => faker.company.bsNoun());
+
+const customer = {
+  licences: licenceIds.map(id => ({
+    id,
+    orb: faker.random.arrayElement(orbs),
+    customer_user: pendingUsers.find(user => user.licences.includes(id)).id,
+  })),
+};
+
+const Template = args => (
+  <Wrapper title="Pending Invitations">
+    <PendingInvitationsBoard {...args} />
+  </Wrapper>
+);
 
 export const NoUsers = Template.bind({});
 NoUsers.args = {
