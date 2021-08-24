64eb0a27dc8169f857ba556fc09d0cfc1d54fe34
Rory MacGregor <rorymacgregor@astrosat.net>
2021-08-24 10:22:16 +0100

    fix(frontend): Remove faker date from tests, check formatted date
    Removed faker from test data, and also added a check for date in the table rows.

--- client/src/mission-control/views/saved-documents/saved-documents.test.js
+++ client/src/mission-control/views/saved-documents/saved-documents.test.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import faker from 'faker/locale/en_GB';
+import { format } from 'date-fns';
 
 import SavedDocuments from './saved-documents.component';
 
-const TEST_DOCUMENTS = new Array(20).fill().map(() => ({
-  id: faker.random.uuid(),
-  title: faker.name.jobTitle(),
-  date: faker.date.past().toISOString(),
-  url: faker.image.imageUrl(),
+const TEST_DOCUMENTS = new Array(20).fill().map((_, i) => ({
+  id: `${i}`,
+  title: `job-title-${i}`,
+  date: new Date(2020, 0, i).toISOString(),
+  url: `url-${i}`,
 }));
 
 const renderComponent = () => {
@@ -25,8 +25,10 @@ describe('<Saved Documents />', () => {
     userEvent.click(getByText('5'));
     userEvent.click(getByText('20'));
 
-    TEST_DOCUMENTS.forEach(doc => {
-      expect(getByText(doc.title)).toBeInTheDocument();
+    TEST_DOCUMENTS.forEach(({ title, date }) => {
+      const displayDate = format(new Date(date), 'dd-MM-yyyy');
+      expect(getByText(title)).toBeInTheDocument();
+      expect(getByText(displayDate)).toBeInTheDocument();
     });
   });
 });
