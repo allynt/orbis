46f2a8180acfa734a3843301976feebe007b74c9
Reignable <joebarret401@gmail.com>
2021-09-10 15:57:09 +0100

    chore(frontend): Convert Profile tests using new render util

--- client/src/accounts/profile/profile.test.js
+++ client/src/accounts/profile/profile.test.js
@@ -1,34 +1,23 @@
 // @ts-nocheck
 import React from 'react';
 
-import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Provider } from 'react-redux';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 
 import { logout, updateUser } from 'accounts/accounts.slice';
+import { render, waitFor } from 'test/test-utils';
 
 import Profile from './profile.component';
 
-const mockStore = configureMockStore([thunk]);
-
-const renderComponent = () => {
-  const store = mockStore({
-    accounts: {
-      userKey: '123',
-      user: { email: 'fred@gmail.com', name: 'Fred' },
-    },
-  });
-  const utils = render(<Profile />, {
-    wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
-  });
-  return { store, ...utils };
+const state = {
+  accounts: {
+    userKey: '123',
+    user: { email: 'fred@gmail.com', name: 'Fred' },
+  },
 };
 
 describe('<Profile />', () => {
   it('dispatches the logout action when logout is clicked', () => {
-    const { getByRole, store } = renderComponent();
+    const { getByRole, store } = render(<Profile />, { state });
     userEvent.click(getByRole('button', { name: /logout/i }));
     expect(store.getActions()).toEqual(
       expect.arrayContaining([expect.objectContaining({ ...logout() })]),
@@ -37,7 +26,7 @@ describe('<Profile />', () => {
 
   it('dispatches the updateUser action when the user is updated', async () => {
     fetch.once(JSON.stringify({}));
-    const { getByRole, store } = renderComponent();
+    const { getByRole, store } = render(<Profile />, { state });
     userEvent.type(getByRole('textbox', { name: /name/i }), 'John');
     userEvent.click(getByRole('button', { name: /update\saccount/i }));
     await waitFor(() =>
