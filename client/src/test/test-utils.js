4462f4f3eeb37e6840870cc2f15a73eb3360d8c3
Reignable <joebarret401@gmail.com>
2021-09-10 14:11:10 +0100

    chore(frontend): Add router to render

--- client/src/test/test-utils.js
+++ client/src/test/test-utils.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { render as rtlRender } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router';
 import createMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
@@ -12,18 +14,24 @@ const mockStore = createMockStore([thunk]);
 /**
  * @param {React.ReactElement} ui
  * @param {(Omit<import('@testing-library/react').RenderOptions, 'queries'>
- *  & {state?: Partial<import('react-redux').DefaultRootState>
- * })} options
+ *  & {
+ *      state?: Partial<import('react-redux').DefaultRootState>
+ *      history?: import('history').MemoryHistoryBuildOptions
+ *    }
+ * )} options
  */
-function render(ui, { state = {}, ...options } = {}) {
+function render(ui, { state = {}, history: historyOptions, ...options } = {}) {
   const store = mockStore(state);
+  const history = createMemoryHistory(historyOptions);
   const Wrapper = ({ children }) => (
     <Provider store={store}>
-      <MapProvider>{children}</MapProvider>
+      <Router history={history}>
+        <MapProvider>{children}</MapProvider>
+      </Router>
     </Provider>
   );
   const utils = rtlRender(ui, { wrapper: Wrapper, ...options });
-  return { ...utils, store };
+  return { ...utils, history, store };
 }
 
 export * from '@testing-library/react';
