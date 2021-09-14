13ec2509bdd154c4f36ae05176277f1396199c80
Reignable <joebarret401@gmail.com>
2021-09-14 12:08:24 +0100

    ORB-701: chore(frontend): Better naming, maybe?
    ORB-701

--- client/src/map/orbs/configurations/hexagonLayerConfig.js
+++ client/src/map/orbs/configurations/hexagonLayerConfig.js
@@ -38,7 +38,7 @@ export default ({
     value = new Date(value).toISOString();
   }
   const columnName = `${value ?? defaultValue}${columnSuffix}`;
-  const getValue = objects =>
+  const getColumnValue = objects =>
     objects.reduce((sum, obj) => sum + obj[columnName], 0);
 
   return {
@@ -63,8 +63,8 @@ export default ({
     pickable: true,
     radius: 5000,
     getPosition: d => [d.longitude, d.latitude],
-    getColorValue: getValue,
-    getElevationValue: getValue,
+    getColorValue: getColumnValue,
+    getElevationValue: getColumnValue,
     onDataLoad: () =>
       dispatch(
         setOther({ key: id, other: { ...otherState, isLoading: false } }),
