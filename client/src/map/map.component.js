3aff7b82b68031511b7df33139fe0f2ae6ff7964
Reignable <joebarret401@gmail.com>
2021-09-14 15:18:47 +0100

    ORB-705: fix(frontend): Add pointer cursor when user is hovering data
    ORB-705

--- client/src/map/map.component.js
+++ client/src/map/map.component.js
@@ -192,14 +192,19 @@ const Map = ({
     setViewState(rest);
   };
 
-  const getCursor = useCallback(
-    ({ isDragging }) => {
+  const getBottomMapCursor = useCallback(({ isDragging, isHovering }) => {
+    if (isHovering) return 'pointer';
+    return isDragging ? 'grabbing' : 'grab';
+  }, []);
+
+  const getTopMapCursor = useCallback(
+    cursorState => {
       if (drawingToolsEnabled && editableLayer?.state?.cursor)
         return editableLayer?.state?.cursor;
       if (drawAoiLayer) return drawAoiLayer?.state?.cursor;
-      return isDragging ? 'grabbing' : 'grab';
+      return getBottomMapCursor(cursorState);
     },
-    [drawAoiLayer, drawingToolsEnabled, editableLayer],
+    [drawAoiLayer, drawingToolsEnabled, editableLayer, getBottomMapCursor],
   );
 
   const mapProps = {
@@ -262,6 +267,7 @@ const Map = ({
           selectedSceneLayer,
           selectionLayer,
         ]}
+        getCursor={getBottomMapCursor}
         effects={[lightingEffect]}
         glOptions={{
           preserveDrawingBuffer: true,
@@ -289,7 +295,7 @@ const Map = ({
           viewState={viewState}
           onViewStateChange={handleViewStateChange}
           layers={[drawAoiLayer, editableLayer]}
-          getCursor={getCursor}
+          getCursor={getTopMapCursor}
           style={{ pointerEvents: topMapIsController ? 'all' : 'none' }}
           glOptions={{
             preserveDrawingBuffer: true,
