8a04467704a2cbb4499a270a0c6d9bcf067c49b1
Reignable <joebarret401@gmail.com>
2021-09-16 15:11:12 +0100

    ORB-702: feat(frontend): Add ColorLegend to available sidebar components
    ORB-702

--- client/src/components/color-legend/color-legend.component.js
+++ client/src/components/color-legend/color-legend.component.js
@@ -11,39 +11,42 @@ import {
 import { InfoButtonTooltip } from 'components';
 
 /**
- * @param {{categories: {[category: string]: {color?: string, description?: string}}}} props
+ * @param {{categories: {[category: string]: {label?: string, color?: string, description?: string}}}} props
  */
 export const ColorLegend = ({ categories }) => (
   <List>
-    {Object.entries(categories).map(([category, { color, description }]) => {
-      const ariaLabel = { 'aria-label': category };
-      const ariaLabelProp = !description
-        ? ariaLabel
-        : { ContainerProps: ariaLabel };
+    {Object.entries(categories).map(
+      ([category, { label: suppliedLabel, color, description }]) => {
+        const label = suppliedLabel || category;
+        const ariaLabel = { 'aria-label': label };
+        const ariaLabelProp = !description
+          ? ariaLabel
+          : { ContainerProps: ariaLabel };
 
-      return (
-        <ListItem key={category} {...ariaLabelProp}>
-          <ListItemIcon>
-            {color ? (
-              <span
-                role="presentation"
-                style={{
-                  width: '1rem',
-                  height: '1rem',
-                  borderRadius: '50%',
-                  backgroundColor: color,
-                }}
-              />
+        return (
+          <ListItem key={category} {...ariaLabelProp}>
+            <ListItemIcon>
+              {color ? (
+                <span
+                  role="presentation"
+                  style={{
+                    width: '1rem',
+                    height: '1rem',
+                    borderRadius: '50%',
+                    backgroundColor: color,
+                  }}
+                />
+              ) : null}
+            </ListItemIcon>
+            <ListItemText primary={label} />
+            {description ? (
+              <ListItemSecondaryAction>
+                <InfoButtonTooltip tooltipContent={description} />
+              </ListItemSecondaryAction>
             ) : null}
-          </ListItemIcon>
-          <ListItemText primary={category} />
-          {description ? (
-            <ListItemSecondaryAction>
-              <InfoButtonTooltip tooltipContent={description} />
-            </ListItemSecondaryAction>
-          ) : null}
-        </ListItem>
-      );
-    })}
+          </ListItem>
+        );
+      },
+    )}
   </List>
 );
