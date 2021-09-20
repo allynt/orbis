5cb7c9add2d119ce17b130f3a7441b5f6908c175
Mark Small <mark.small@astrosat.space>
2021-09-20 15:53:49 +0100

    chore(backend): Remove commented out code
    Deleted obsolete code.
    
    IssueID ORB-707

--- server/orbis/views/views_storage.py
+++ server/orbis/views/views_storage.py
@@ -1,6 +1,3 @@
-from django.utils.functional import cached_property
-from django.shortcuts import get_object_or_404
-
 from astrosat.decorators import swagger_fake
 
 from rest_framework import mixins, viewsets
@@ -23,12 +20,3 @@ class DataStorageViewSet(
         """
         current_user = self.request.user
         return current_user.storage.all()
-
-    # def destroy(self, request, *args, **kwargs):
-    #     # Delete associated DataSource.
-    #     print(f"SELF: {self.__dict__}", flush=True)
-    #     print(f"REQUEST: {request.__dict__}", flush=True)
-    #     print(f"ARGS: {args}", flush=True)
-    #     print(f"KWARGS: {kwargs}", flush=True)
-
-    #     super().destroy(request, *args, **kwargs)
