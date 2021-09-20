a34cf0f8fc2e38c501c4269b0ba4a26b327f6e32
Mark Small <mark.small@astrosat.space>
2021-09-20 12:46:31 +0100

    chore(backend): Remove unused import
    Removed unused import.
    
    IssueID ORB-707

--- server/satellites/models/models_satellites.py
+++ server/satellites/models/models_satellites.py
@@ -5,7 +5,7 @@ from django.contrib.gis.db import models as gis_models
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models.fields import related
+
 from django.utils.text import slugify
 from django.utils.translation import gettext as _
 
