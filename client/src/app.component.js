d95362e1db020176a099ed6c1bbdb2353c71f25e
Reignable <joebarret401@gmail.com>
2021-09-23 09:42:41 +0100

    chore(frontend): Code split App

--- client/src/app.component.js
+++ client/src/app.component.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { Box } from '@astrosat/astrosat-ui';
+import { Box, Fade, LoadMask } from '@astrosat/astrosat-ui';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
@@ -8,7 +8,6 @@ import ReactTooltip from 'react-tooltip';
 
 import apiClient from 'api-client';
 
-import Accounts from './accounts';
 import { userKeySelector, userSelector } from './accounts/accounts.selectors';
 import { fetchCurrentUser } from './accounts/accounts.slice';
 import {
@@ -16,9 +15,11 @@ import {
   logUserTracking,
   userTrackingIntervalSelector,
 } from './app.slice';
-import LandingView from './landing/landing.component';
-import MapLayout from './map';
 import PrivateRoute from './utils/private-route.component';
+
+const Accounts = React.lazy(() => import('accounts'));
+const LandingView = React.lazy(() => import('landing/landing.component'));
+const MapLayout = React.lazy(() => import('map'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -54,16 +55,25 @@ const App = () => {
   return (
     <Box width="100vw" height="100vh" style={{ backgroundColor: '#ffffff' }}>
       <ReactTooltip />
-
-      <Switch>
-        <PrivateRoute exact path="/" user={user} component={LandingView} />
-        <Route path="/accounts" component={Accounts} />
-        <PrivateRoute
-          path={['/map', '/mission-control']}
-          user={user}
-          component={MapLayout}
-        />
-      </Switch>
+      <React.Suspense
+        fallback={
+          <Fade in>
+            <div>
+              <LoadMask style={{ zIndex: 1 }} open />
+            </div>
+          </Fade>
+        }
+      >
+        <Switch>
+          <PrivateRoute exact path="/" user={user} component={LandingView} />
+          <Route path="/accounts" component={Accounts} />
+          <PrivateRoute
+            path={['/map', '/mission-control']}
+            user={user}
+            component={MapLayout}
+          />
+        </Switch>
+      </React.Suspense>
     </Box>
   );
 };
