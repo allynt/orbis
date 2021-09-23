5efd152ab78ded0023a2b3907ac4191b52bb78f7
Rory MacGregor <rorymacgregor@astrosat.net>
2021-09-23 16:51:06 +0100

    fix(frontend): Update PendingInvitationBoard
    Updated the pending invitations board to use the new test structure.

--- client/src/mission-control/views/users-view/pending-invitations-board/pending-invitations-board.test.js
+++ client/src/mission-control/views/users-view/pending-invitations-board/pending-invitations-board.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-
 import {
-  customer,
-  pendingUsers,
+  customer as testCustomer,
+  pendingUsers as testPendingUsers,
 } from 'mission-control/views/users-view/test-story-data';
+import { render, screen, userEvent } from 'test/test-utils';
 
 import { PendingInvitationsBoard } from './pending-invitations-board.component';
+
+const setup = ({
+  pendingUsers = testPendingUsers,
+  customer = testCustomer,
+  ...rest
+}) => {
+  render(
+    <PendingInvitationsBoard
+      pendingUsers={pendingUsers}
+      customer={customer}
+      {...rest}
+    />,
+  );
+};
 
 describe('PendingUsersBoard', () => {
   const cases = [
@@ -17,101 +29,74 @@ describe('PendingUsersBoard', () => {
   ];
   const onWithdrawInvitationClick = jest.fn();
   const onResendInvitationClick = jest.fn();
+
   it.each(cases)("Displays all pending user's %s", (_, text) => {
-    const { getByText } = render(
-      <PendingInvitationsBoard
-        pendingUsers={pendingUsers}
-        customer={customer}
-      />,
-    );
-    pendingUsers.forEach(user =>
-      expect(getByText(user.user[text])).toBeInTheDocument(),
+    setup({});
+    testPendingUsers.forEach(user =>
+      expect(screen.getByText(user.user[text])).toBeInTheDocument(),
     );
   });
 
   it('Displays user Orb licence names', () => {
-    const { getAllByText } = render(
-      <PendingInvitationsBoard
-        pendingUsers={pendingUsers}
-        customer={customer}
-      />,
-    );
+    setup({});
 
-    expect(getAllByText('Oil, Rice')[0]).toBeInTheDocument();
+    expect(screen.getAllByText('Oil, Rice')[0]).toBeInTheDocument();
   });
 
   it('Displays a placeholder if no customer is present', () => {
-    const { getAllByText } = render(
-      <PendingInvitationsBoard pendingUsers={pendingUsers} customer={null} />,
-    );
+    setup({ customer: null });
 
-    pendingUsers.forEach((user, i) =>
-      expect(getAllByText('Not currently available')[i]).toBeInTheDocument(),
+    testPendingUsers.forEach((_, i) =>
+      expect(
+        screen.getAllByText('Not currently available')[i],
+      ).toBeInTheDocument(),
     );
   });
 
   it('Displays a placeholder when customer is present but has no licences', () => {
-    const { getAllByText, queryByText } = render(
-      <PendingInvitationsBoard
-        pendingUsers={pendingUsers}
-        customer={{ name: 'Customer Name' }}
-      />,
-    );
+    setup({ customer: { name: 'Customer Name' } });
 
-    expect(queryByText('No licences')).not.toBeInTheDocument();
+    expect(screen.queryByText('No licences')).not.toBeInTheDocument();
 
-    pendingUsers.forEach((user, i) =>
-      expect(getAllByText('Not currently available')[i]).toBeInTheDocument(),
+    testPendingUsers.forEach((_, i) =>
+      expect(
+        screen.getAllByText('Not currently available')[i],
+      ).toBeInTheDocument(),
     );
   });
 
   it('Displays a placeholder when user has no licences', () => {
     const TEST_USER = {
-      ...pendingUsers[0],
+      ...testPendingUsers[0],
       id: '99',
     };
 
-    const { getByText, queryByText } = render(
-      <PendingInvitationsBoard
-        pendingUsers={[TEST_USER]}
-        customer={customer}
-      />,
-    );
+    setup({ pendingUsers: [TEST_USER] });
 
-    expect(queryByText('Not currently available')).not.toBeInTheDocument();
-    expect(getByText('No licences')).toBeInTheDocument();
+    expect(
+      screen.queryByText('Not currently available'),
+    ).not.toBeInTheDocument();
+    expect(screen.getByText('No licences')).toBeInTheDocument();
   });
 
   it('Calls resendInvitation when `Resend Invitation` button is clicked', () => {
-    const { getAllByText } = render(
-      <PendingInvitationsBoard
-        pendingUsers={pendingUsers}
-        customer={customer}
-        onResendInvitationClick={onResendInvitationClick}
-      />,
-    );
+    setup({ onResendInvitationClick });
 
-    const resendInvitationButton = getAllByText('Resend Invitation')[0];
+    const resendInvitationButton = screen.getAllByText('Resend Invitation')[0];
     expect(resendInvitationButton).toBeInTheDocument();
     userEvent.click(resendInvitationButton);
-    expect(onResendInvitationClick).toHaveBeenCalledWith(pendingUsers[0]);
+    expect(onResendInvitationClick).toHaveBeenCalledWith(testPendingUsers[0]);
   });
 
   it('Opens `Withdraw Invitation` dialog when button is clicked', () => {
-    const { getByText, getAllByTestId } = render(
-      <PendingInvitationsBoard
-        pendingUsers={pendingUsers}
-        customer={customer}
-        onWithdrawInvitationClick={onWithdrawInvitationClick}
-      />,
-    );
+    setup({ onWithdrawInvitationClick });
 
-    userEvent.click(getAllByTestId('options-icon')[0]);
+    userEvent.click(screen.getAllByTestId('options-icon')[0]);
 
-    const optionsDropdownButton = getByText('Withdraw');
+    const optionsDropdownButton = screen.getByText('Withdraw');
 
     expect(optionsDropdownButton).toBeInTheDocument();
     userEvent.click(optionsDropdownButton);
-    expect(onWithdrawInvitationClick).toHaveBeenCalledWith(pendingUsers[0]);
+    expect(onWithdrawInvitationClick).toHaveBeenCalledWith(testPendingUsers[0]);
   });
 });
