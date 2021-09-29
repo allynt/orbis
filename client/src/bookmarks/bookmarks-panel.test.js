832d7501b55f663938a46d02835bd0d63c20bd4c
Rory MacGregor <rorymacgregor@astrosat.net>
2021-09-29 17:53:08 +0100

    fix(frontend): Upgrade bookmarks panel tests
    Removed the `setup` function and rendered the component in each test, only passing the props required.

--- client/src/bookmarks/bookmarks-panel.test.js
+++ client/src/bookmarks/bookmarks-panel.test.js
@@ -1,42 +1,27 @@
 import * as React from 'react';
 
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { Provider } from 'react-redux';
-import createMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-
-import { MapProvider } from 'MapContext';
+import { render, screen, userEvent } from 'test/test-utils';
 
 import BookmarksPanel from './bookmarks-panel.component';
 import { selectBookmark } from './bookmarks.slice';
 
-const mockStore = createMockStore([thunk]);
-
-const setup = (initialState = {}) => {
-  const store = mockStore(initialState);
-  const utils = render(
-    <Provider store={store}>
-      <MapProvider>
-        <BookmarksPanel />
-      </MapProvider>
-    </Provider>,
-  );
-  return { ...utils, store };
-};
-
 describe('<BookmarksPanel />', () => {
   it('shows the new bookmark form', () => {
-    const { getByRole } = setup({
-      accounts: { userKey: '' },
-      bookmarks: { bookmarks: [] },
+    render(<BookmarksPanel />, {
+      state: {
+        accounts: { userKey: '' },
+        bookmarks: { bookmarks: [] },
+      },
     });
-    expect(getByRole('textbox', { name: 'Title' })).toBeInTheDocument();
-    expect(getByRole('textbox', { name: 'Description' })).toBeInTheDocument();
+
+    expect(screen.getByRole('textbox', { name: 'Title' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('textbox', { name: 'Description' }),
+    ).toBeInTheDocument();
   });
 
   it('shows a list of the bookmarks in state', () => {
-    const initialState = {
+    const state = {
       bookmarks: {
         bookmarks: [
           { id: '1', title: 'Bookmark 1' },
@@ -44,23 +29,25 @@ describe('<BookmarksPanel />', () => {
         ],
       },
     };
-    const { getByText } = setup(initialState);
 
-    initialState.bookmarks.bookmarks
+    render(<BookmarksPanel />, { state });
+
+    state.bookmarks.bookmarks
       .map(bm => bm.title)
       .forEach(title => {
-        expect(getByText(title)).toBeInTheDocument();
+        expect(screen.getByText(title)).toBeInTheDocument();
       });
   });
 
   it('calls the selectBookmark action', () => {
-    const initialState = {
+    const state = {
       bookmarks: {
         bookmarks: [{ id: '1', title: 'Bookmark 1' }],
       },
     };
 
-    const testBookmark = initialState.bookmarks.bookmarks[0];
+    const testBookmark = state.bookmarks.bookmarks[0];
+
     const action = expect.objectContaining({
       type: selectBookmark.pending.type,
       meta: expect.objectContaining({
@@ -69,8 +56,10 @@ describe('<BookmarksPanel />', () => {
         }),
       }),
     });
-    const { getByRole, store } = setup(initialState);
-    userEvent.click(getByRole('button', { name: 'Load' }));
+
+    const { store } = render(<BookmarksPanel />, { state });
+
+    userEvent.click(screen.getByRole('button', { name: 'Load' }));
     expect(store.getActions()).toContainEqual(action);
   });
 });
