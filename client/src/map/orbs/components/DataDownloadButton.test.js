bea7a56ab34c08e7b37c49bdb09f863f9422c914
Rory MacGregor <rorymacgregor@astrosat.net>
2021-10-05 19:45:10 +0100

    fix(frontend): Upgrade data download button tests
    Upgraded to use the new helper methods, streamlined tests.

--- client/src/map/orbs/components/DataDownloadButton.test.js
+++ client/src/map/orbs/components/DataDownloadButton.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import { render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import fetch from 'jest-fetch-mock';
-import { Provider } from 'react-redux';
-import configureMockStore from 'redux-mock-store';
 
 import { ADMIN_STATUS } from 'mission-control/mission-control.constants';
+import { render, waitFor, screen, userEvent } from 'test/test-utils';
 
 import DataDownloadButton, {
   getFilenameFromHeader,
@@ -14,50 +11,50 @@ import DataDownloadButton, {
 
 jest.mock('file-saver');
 
-const mockStore = configureMockStore();
-
-const renderComponent = ({
+const setup = ({
   url = 'http://test.com/download',
   state = {
     accounts: { user: { customers: [{ type: ADMIN_STATUS.manager }] } },
   },
   adminOnly = undefined,
-} = {}) =>
-  render(<DataDownloadButton url={url} adminOnly={adminOnly} />, {
-    wrapper: ({ children }) => (
-      <Provider store={mockStore(state)}>{children}</Provider>
-    ),
+} = {}) => {
+  return render(<DataDownloadButton url={url} adminOnly={adminOnly} />, {
+    state,
   });
+};
 
 describe('<DataDownloadButton />', () => {
   it("warns if url isn't supplied and renders nothing", () => {
     console.warn = jest.fn();
-    const { queryByRole } = renderComponent({ url: null });
-    expect(queryByRole('button')).not.toBeInTheDocument();
+    setup({ url: null });
+
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
     expect(console.warn).toHaveBeenCalled();
   });
 
   it('does not render if the user is not an admin at all', () => {
-    const { queryByRole } = renderComponent({
+    setup({
       state: {
         accounts: { user: { customers: [{ type: ADMIN_STATUS.member }] } },
       },
     });
-    expect(queryByRole('button')).not.toBeInTheDocument();
+
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
   });
 
   it('renders if adminOnly is false', () => {
-    const { getByRole } = renderComponent({
+    setup({
       adminOnly: false,
       state: {
         accounts: { user: { customers: [{ type: ADMIN_STATUS.member }] } },
       },
     });
-    expect(getByRole('button')).toBeInTheDocument();
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
   it('renders if the user is an admin of any customers', () => {
-    const { getByRole } = renderComponent({
+    setup({
       state: {
         accounts: {
           user: {
@@ -69,12 +66,15 @@ describe('<DataDownloadButton />', () => {
         },
       },
     });
-    expect(getByRole('button')).toBeInTheDocument();
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
   it('fetches the file when the button is clicked', async () => {
-    const { getByRole } = renderComponent();
-    userEvent.click(getByRole('button'));
+    setup();
+
+    userEvent.click(screen.getByRole('button'));
+
     await waitFor(() => expect(fetch).toHaveBeenCalled());
   });
 
