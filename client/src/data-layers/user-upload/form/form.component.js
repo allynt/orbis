24539ff6ecec1059213d31c429e18c4e30ed8893
Reignable <joebarret401@gmail.com>
2021-10-06 16:00:12 +0100

    ORB-820: chore(frontend): Use theme values in Form
    ORB-820

--- client/src/data-layers/user-upload/form/form.component.js
+++ client/src/data-layers/user-upload/form/form.component.js
@@ -13,48 +13,48 @@ import CancelIcon from '@material-ui/icons/Cancel';
 
 import { ReactComponent as CsvIcon } from '../intro/csv.svg';
 
-const UploadIcon = styled('div')({
+const UploadIcon = styled('div')(({ theme }) => ({
   position: 'relative',
-  border: 'dashed 1px #979797',
-  borderRadius: '5px',
+  border: `dashed 1px ${theme.palette.grey[500]}`,
+  borderRadius: theme.shape.borderRadius,
   width: '100%',
   maxWidth: '50px',
   aspectRatio: '1.4',
-  padding: '8px 7px',
+  padding: theme.spacing(1),
   '& svg': {
     maxWidth: '31px',
     position: 'absolute',
     right: -5,
     bottom: -5,
   },
-});
+}));
 
 const LinearProgress = styled(BaseLinearProgress)(({ theme }) => ({
   height: 10,
-  borderRadius: 5,
+  borderRadius: '100vh',
   [`&.MuiLinearProgress-colorPrimary`]: {
     backgroundColor: theme.palette.grey[500],
   },
   [`& .MuiLinearProgress-bar`]: {
-    borderRadius: 5,
+    borderRadius: '100vh',
     backgroundColor: theme.palette.info.main,
   },
 }));
 
-const FormWrapper = styled('form')({
+const FormWrapper = styled('form')(({ theme }) => ({
   display: 'grid',
   gridAutoFlow: 'row',
-  rowGap: '20px',
-});
+  rowGap: theme.spacing(2),
+}));
 
-const FileDrop = styled('div')({
+const FileDrop = styled('div')(({ theme }) => ({
   display: 'grid',
   gridAutoFlow: 'row',
   justifyItems: 'center',
   backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='10' ry='10' stroke='%23979797FF' stroke-width='4' stroke-dasharray='16' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`,
   borderRadius: '10px',
-  padding: '20px 20px 30px',
-});
+  padding: theme.spacing(2, 2, 3),
+}));
 
 const UploadControls = styled('div')({
   display: 'grid',
