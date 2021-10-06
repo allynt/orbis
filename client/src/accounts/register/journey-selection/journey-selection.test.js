3589a5eed742e335fc6d54577dcc0e1528436cda
Rory MacGregor <rorymacgregor@astrosat.net>
2021-10-06 17:14:33 +0100

    fix(frontend): Upgrade journey selection tests
    Upgraded component to use new utility helpers, and streamlined tests.

--- client/src/accounts/register/journey-selection/journey-selection.test.js
+++ client/src/accounts/register/journey-selection/journey-selection.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
-
 import { REGISTER_CUSTOMER_USER, REGISTER } from 'accounts/accounts.constants';
+import { render, screen, userEvent } from 'test/test-utils';
 
 import JourneySelection from './journey-selection.component';
 
@@ -13,100 +9,101 @@ const TEAM_REGEX = /team/i;
 const INDIVIDUAL_REGEX = /individual/i;
 const CONTINUE_REGEX = /continue/i;
 
-const renderComponent = ({
-  individualRegistrationIsOpen = true,
-  customerRegistrationIsOpen = true,
-} = {}) => {
-  const history = createMemoryHistory();
-  const utils = render(
-    <JourneySelection
-      individualRegistrationIsOpen={individualRegistrationIsOpen}
-      customerRegistrationIsOpen={customerRegistrationIsOpen}
-    />,
-    {
-      wrapper: ({ children }) => <Router history={history}>{children}</Router>,
-    },
-  );
-  return { ...utils, history };
-};
-
 describe('<JourneySelection />', () => {
   it('displays a radio for Team', () => {
-    const { getByRole } = renderComponent();
-    expect(getByRole('radio', { name: TEAM_REGEX })).toBeInTheDocument();
+    render(
+      <JourneySelection
+        individualRegistrationIsOpen={true}
+        customerRegistrationIsOpen={true}
+      />,
+    );
+
+    expect(screen.getByRole('radio', { name: TEAM_REGEX })).toBeInTheDocument();
   });
 
   it('displays a radio for Individual', () => {
-    const { getByRole } = renderComponent();
-    expect(getByRole('radio', { name: INDIVIDUAL_REGEX })).toBeInTheDocument();
+    render(<JourneySelection />);
+
+    expect(
+      screen.getByRole('radio', { name: INDIVIDUAL_REGEX }),
+    ).toBeInTheDocument();
   });
 
   it('disables the Team radio when team registration is not open', () => {
-    const { getByRole } = renderComponent({
-      customerRegistrationIsOpen: false,
-      individualRegistrationIsOpen: true,
-    });
-    expect(getByRole('radio', { name: TEAM_REGEX })).toHaveAttribute(
+    render(<JourneySelection customerRegistrationIsOpen={false} />);
+
+    expect(screen.getByRole('radio', { name: TEAM_REGEX })).toHaveAttribute(
       'disabled',
     );
   });
 
   it('disables the Individual radio when individual registration is not open', () => {
-    const { getByRole } = renderComponent({
-      customerRegistrationIsOpen: true,
-      individualRegistrationIsOpen: false,
-    });
-    expect(getByRole('radio', { name: INDIVIDUAL_REGEX })).toHaveAttribute(
-      'disabled',
-    );
+    render(<JourneySelection individualRegistrationIsOpen={false} />);
+
+    expect(
+      screen.getByRole('radio', { name: INDIVIDUAL_REGEX }),
+    ).toHaveAttribute('disabled');
   });
 
   it('disables the Continue button when no selection has been made', () => {
-    const { getByRole } = renderComponent();
-    expect(getByRole('button', { name: CONTINUE_REGEX })).toBeDisabled();
+    render(<JourneySelection />);
+
+    expect(screen.getByRole('button', { name: CONTINUE_REGEX })).toBeDisabled();
   });
 
   it('enables the Continue button when a selection is made', () => {
-    const { getByRole } = renderComponent();
-    expect(getByRole('button', { name: CONTINUE_REGEX })).toBeDisabled();
-    userEvent.click(getByRole('radio', { name: TEAM_REGEX }));
-    expect(getByRole('button', { name: CONTINUE_REGEX })).not.toBeDisabled();
+    render(<JourneySelection />);
+
+    expect(screen.getByRole('button', { name: CONTINUE_REGEX })).toBeDisabled();
+
+    userEvent.click(screen.getByRole('radio', { name: TEAM_REGEX }));
+
+    expect(
+      screen.getByRole('button', { name: CONTINUE_REGEX }),
+    ).not.toBeDisabled();
   });
 
   it('navigates to individual registration when the selection is Individual and Continue is clicked', () => {
-    const { getByRole, history } = renderComponent();
-    userEvent.click(getByRole('radio', { name: INDIVIDUAL_REGEX }));
-    userEvent.click(getByRole('button', { name: CONTINUE_REGEX }));
+    const { history } = render(<JourneySelection />);
+
+    userEvent.click(screen.getByRole('radio', { name: INDIVIDUAL_REGEX }));
+    userEvent.click(screen.getByRole('button', { name: CONTINUE_REGEX }));
+
     expect(history.location.pathname).toBe(REGISTER);
   });
 
   it('navigates to customer registration when Team is selected and Continue is clicked', () => {
-    const { getByRole, history } = renderComponent();
-    userEvent.click(getByRole('radio', { name: TEAM_REGEX }));
-    userEvent.click(getByRole('button', { name: CONTINUE_REGEX }));
+    const { history } = render(<JourneySelection />);
+
+    userEvent.click(screen.getByRole('radio', { name: TEAM_REGEX }));
+    userEvent.click(screen.getByRole('button', { name: CONTINUE_REGEX }));
+
     expect(history.location.pathname).toBe(REGISTER_CUSTOMER_USER);
   });
 
   it('Has a terms and conditions link', () => {
-    const { getByRole } = renderComponent();
+    const { getByRole } = render(<JourneySelection />);
     expect(
       getByRole('link', { name: /Terms and Conditions/i }),
     ).toHaveAttribute('href', expect.stringContaining('TERMS'));
   });
 
   it('Has a privacy link', () => {
-    const { getByRole } = renderComponent();
-    expect(getByRole('link', { name: /Privacy Policy/i })).toHaveAttribute(
-      'href',
-      expect.stringContaining('PRIVACY'),
-    );
+    render(<JourneySelection />);
+
+    expect(
+      screen.getByRole('link', { name: /Privacy Policy/i }),
+    ).toHaveAttribute('href', expect.stringContaining('PRIVACY'));
   });
 
   it('Shows an error if neither registration is open', () => {
-    const { getByRole } = renderComponent({
-      customerRegistrationIsOpen: false,
-      individualRegistrationIsOpen: false,
-    });
-    expect(getByRole('alert')).toBeInTheDocument();
+    render(
+      <JourneySelection
+        individualRegistrationIsOpen={false}
+        customerRegistrationIsOpen={false}
+      />,
+    );
+
+    expect(screen.getByRole('alert')).toBeInTheDocument();
   });
 });
