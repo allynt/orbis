d2a3ec45b44dcf9730b6fee33fc6f37f89ef9d88
Rory MacGregor <rorymacgregor@astrosat.net>
2021-10-06 17:57:04 +0100

    fix(frontend): Upgrade orb details tests
    Upgraded component to use new utility helpers, and streamlined tests.

--- client/src/mission-control/views/store/orb-details/orb-details.test.js
+++ client/src/mission-control/views/store/orb-details/orb-details.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { render, screen, userEvent } from 'test/test-utils';
 
 import { OrbDetails } from './orb-details.component';
 
@@ -12,63 +9,65 @@ const orbs = [
   { id: 2, name: 'Orb 2 Name', description: 'Orb 2 Description', images: [''] },
 ];
 
-const renderComponent = () => {
-  const history = createMemoryHistory();
-  history.goBack = jest.fn();
-  const utils = render(
-    <OrbDetails
-      // @ts-ignore
-      orbs={orbs}
-      // @ts-ignore
-      match={{ params: { orbId: '1' } }}
-      // @ts-ignore
-      history={history}
-    />,
-    {
-      wrapper: ({ children }) => <Router history={history}>{children}</Router>,
-    },
-  );
-  return { ...utils, history };
-};
-
 describe('<OrbDetails />', () => {
   it('Shows the name of the orb from the route', () => {
-    const { getByRole } = renderComponent();
-    expect(getByRole('heading', { name: orbs[0].name })).toBeInTheDocument();
+    render(<OrbDetails orbs={orbs} match={{ params: { orbId: '1' } }} />);
+
+    expect(
+      screen.getByRole('heading', { name: orbs[0].name }),
+    ).toBeInTheDocument();
   });
 
   it('Shows the description of the orb from the route', () => {
-    const { getByText } = renderComponent();
-    expect(getByText(orbs[0].description)).toBeInTheDocument();
+    render(<OrbDetails orbs={orbs} match={{ params: { orbId: '1' } }} />);
+
+    expect(screen.getByText(orbs[0].description)).toBeInTheDocument();
   });
 
   it('Shows the product images', () => {
-    const { getByRole } = renderComponent();
+    render(<OrbDetails orbs={orbs} match={{ params: { orbId: '1' } }} />);
+
     expect(
-      getByRole('img', { name: 'Orb 1 Name Example' }),
+      screen.getByRole('img', { name: 'Orb 1 Name Example' }),
     ).toBeInTheDocument();
   });
 
   it('Goes back when the back button is clicked', () => {
-    const { getByRole, history } = renderComponent();
-    userEvent.click(getByRole('link', { name: 'Back' }));
+    const history = { goBack: jest.fn() };
+    render(
+      <OrbDetails
+        orbs={orbs}
+        match={{ params: { orbId: '1' } }}
+        history={history}
+      />,
+    );
+
+    userEvent.click(screen.getByRole('link', { name: 'Back' }));
     expect(history.goBack).toBeCalled();
   });
 
   it('Has a minimum users of 3 and maximum of 30', () => {
-    const { getByRole, queryByRole } = renderComponent();
-    userEvent.click(getByRole('button', { name: /Number of Users/i }));
-    expect(getByRole('option', { name: /^3$/ })).toBeInTheDocument();
-    expect(getByRole('option', { name: /30/ })).toBeInTheDocument();
-    expect(queryByRole('option', { name: /^2$/ })).not.toBeInTheDocument();
-    expect(queryByRole('option', { name: /31/ })).not.toBeInTheDocument();
+    render(<OrbDetails orbs={orbs} match={{ params: { orbId: '1' } }} />);
+
+    userEvent.click(screen.getByRole('button', { name: /Number of Users/i }));
+    expect(screen.getByRole('option', { name: /^3$/ })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: /30/ })).toBeInTheDocument();
+    expect(
+      screen.queryByRole('option', { name: /^2$/ }),
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole('option', { name: /31/ }),
+    ).not.toBeInTheDocument();
   });
 
   it('Navigates to the checkout view with the orb and number of seats as params', () => {
-    const { getByRole, history } = renderComponent();
-    userEvent.click(getByRole('button', { name: /Number of Users/i }));
-    userEvent.click(getByRole('option', { name: '5' }));
-    userEvent.click(getByRole('link', { name: /get access/i }));
+    const { history } = render(
+      <OrbDetails orbs={orbs} match={{ params: { orbId: '1' } }} />,
+    );
+
+    userEvent.click(screen.getByRole('button', { name: /Number of Users/i }));
+    userEvent.click(screen.getByRole('option', { name: '5' }));
+    userEvent.click(screen.getByRole('link', { name: /get access/i }));
     expect(history.location.pathname).toContain('/checkout');
     expect(history.location.search).toBe('?orbId=1&users=5');
   });
