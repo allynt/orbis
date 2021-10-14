ac26fb61f0a887073b02adfdaf484f90ca357286
Rory MacGregor <rorymacgregor@astrosat.net>
2021-10-14 13:11:39 +0100

    fix(frontend): Add Provider to storage stories
    Added a Provider to the Storage stories, as they were breaking without.

--- client/src/mission-control/views/storage/storage.stories.js
+++ client/src/mission-control/views/storage/storage.stories.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 import faker from 'faker/locale/en_GB';
+import { Provider } from 'react-redux';
+import createMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import { Storage } from './storage.component';
+
+const mockStore = createMockStore([thunk]);
 
 const TEST_FILES = new Array(20).fill().map(() => ({
   id: faker.random.uuid(),
@@ -14,7 +19,13 @@ export default {
   title: 'Mission Control/Storage',
 };
 
-const Template = args => <Storage {...args} />;
+const Template = args => {
+  return (
+    <Provider store={mockStore({})}>
+      <Storage {...args} />
+    </Provider>
+  );
+};
 
 const setFiles = () => ({});
 
