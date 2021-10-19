39d463e12bccfb07ac7733b69fce2d7fa95e64ea
Mark Small <mark.small@astrosat.space>
2021-10-19 11:04:37 +0100

    fix(frontend): Make PLD sidebar more like design
    I initially copied the AIS Shipping sidebar, with the intent of
    re-factoring it. I have now done so, realising copying AIS probably
    wasn't the best idea.
    
    I have removed the results list as we don't use anything like that
    and created a sidebar that is very PLD data focussed.
    
    IssueID ORB-851

--- client/src/map/orbs/components/pld/sidebar/sidebar.component.js
+++ client/src/map/orbs/components/pld/sidebar/sidebar.component.js
@@ -1,19 +1,9 @@
 import * as React from 'react';
 
-import {
-  Button,
-  CircularProgress,
-  Fade,
-  FormControlLabel,
-  Grid,
-  List,
-  ListSubheader,
-  makeStyles,
-  Pagination,
-  Radio,
-} from '@astrosat/astrosat-ui';
+import { Grid, makeStyles, Typography } from '@astrosat/astrosat-ui';
 
-import ResultsListItem from './results-list-item/results-list-item.component';
+import CheckboxFilters from 'map/orbs/components/CheckboxFilters';
+import DateRangeFilter from 'map/orbs/components/DateRangeFilter';
 
 const useStyles = makeStyles(theme => ({
   wrapper: {
@@ -25,91 +15,33 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export const PldSidebarComponent = ({
-  results,
-  isLoading,
-  onFindClick,
-  pages,
-  currentPage,
-  onPageClick,
-  onResultClick,
-  selectedResult,
-  onRadioChange,
-  visible,
+  selectedLayer,
+  color,
+  constructionPhaseFilters,
+  developmentTypeFilters,
+  iconColor,
 }) => {
   const styles = useStyles();
+  console.log('SEELECTED LAYER: ', selectedLayer);
 
   return (
-    <div className={styles.wrapper}>
-      <Grid container spacing={2}>
-        <Grid item xs={11}>
-          <FormControlLabel
-            label="Display Data"
-            control={<Radio onClick={onRadioChange} checked={visible} />}
-          />
-        </Grid>
+    <Grid className={styles.wrapper} container spacing={2}>
+      <Grid item xs={11}>
+        <Typography variant="h4">Date Range</Typography>
+        <DateRangeFilter maxDate="today" selectedLayer={selectedLayer} />
       </Grid>
-      <Fade in={visible} unmountOnExit>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12} container justifyContent="center">
-            <Button size="small" onClick={() => !isLoading && onFindClick()}>
-              {isLoading ? (
-                <CircularProgress
-                  data-testid="button-progress"
-                  color="inherit"
-                  size={20}
-                />
-              ) : (
-                'Request Data'
-              )}
-            </Button>
-          </Grid>
-          {(isLoading && !results) || results?.length ? (
-            <>
-              <Grid item xs={12}>
-                <List
-                  subheader={
-                    <ListSubheader disableSticky>List of Builds</ListSubheader>
-                  }
-                >
-                  {isLoading &&
-                    !results &&
-                    Array(10)
-                      .fill(undefined)
-                      // eslint-disable-next-line react/no-array-index-key
-                      .map((_, i) => <ResultsListItem key={i} isLoading />)}
-                  {results?.length
-                    ? results.map((result, i) => (
-                        <ResultsListItem
-                          key={result.id}
-                          result={result}
-                          selected={
-                            selectedResult === undefined ||
-                            result.id === selectedResult?.id
-                          }
-                          onClick={onResultClick}
-                          divider={i + 1 !== results.length}
-                        />
-                      ))
-                    : null}
-                </List>
-              </Grid>
-              {pages > 1 ? (
-                <Grid item xs={12} container justifyContent="center">
-                  <Pagination
-                    hideNextButton
-                    hidePrevButton
-                    shape="rounded"
-                    color="primary"
-                    page={currentPage}
-                    count={pages}
-                    onChange={(_, page) => onPageClick(page)}
-                  />
-                </Grid>
-              ) : null}
-            </>
-          ) : null}
-        </Grid>
-      </Fade>
-    </div>
+      <Grid item xs={11}>
+        <Typography variant="h4">Construction Phase</Typography>
+        <CheckboxFilters filters={constructionPhaseFilters} />
+      </Grid>
+      <Grid item xs={11}>
+        <Typography variant="h4">Development Type</Typography>
+        <CheckboxFilters
+          color={color}
+          filters={developmentTypeFilters}
+          iconColor={iconColor}
+        />
+      </Grid>
+    </Grid>
   );
 };
--- client/src/map/orbs/components/pld/sidebar/sidebar.test.js
+++ client/src/map/orbs/components/pld/sidebar/sidebar.test.js
@@ -1,145 +1,108 @@
 import * as React from 'react';
 
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import createMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import { PldSidebarComponent } from './sidebar.component';
 
-const RESULTS = [
+const mockStore = createMockStore([thunk]);
+
+const CONSTRUCTION_PHASES = [
   {
-    id: 1,
-    properties: {
-      'Project ID': 'Test Title 1',
-      Address: 'Test Address 1',
-      Postcode: 'Test Postcode 1',
-    },
+    icon: 'Circle',
+    value: 'Approved',
+    bgColor: '#75b7b2',
   },
   {
-    id: 2,
-    properties: {
-      'Project ID': 'Test Title 2',
-      Address: 'Test Address 2',
-      Postcode: 'Test Postcode 2',
-    },
+    icon: 'Circle',
+    value: 'Commenced',
+    bgColor: '#f52455',
+  },
+  {
+    icon: 'Circle',
+    value: 'Completed',
+    bgColor: '#8aea73',
   },
 ];
 
-const renderComponent = ({
-  results = RESULTS,
-  isLoading = false,
-  visible = true,
-  selectedResult = undefined,
-  pages = 1,
-} = {}) => {
-  const onFindClick = jest.fn();
-  const onPageClick = jest.fn();
-  const onRadioChange = jest.fn();
-  const utils = render(
-    <PldSidebarComponent
-      visible={visible}
-      onFindClick={onFindClick}
-      pages={pages}
-      onPageClick={onPageClick}
-      onRadioChange={onRadioChange}
-      results={results}
-      isLoading={isLoading}
-      selectedResult={selectedResult}
-    />,
-  );
-  return { ...utils, onFindClick, onPageClick, onRadioChange };
-};
+const DEVELOPMENT_TYPES = [
+  {
+    icon: 'Conversion',
+    value: 'Conversion',
+  },
+  {
+    icon: 'NewBuild',
+    value: 'New Build',
+  },
+  {
+    icon: 'ChangeOfUse',
+    value: 'Change of Use',
+  },
+  {
+    icon: 'Extension',
+    value: 'Extension',
+  },
+];
 
 describe('<PldSidebarComponent />', () => {
-  it('Calls onFindClick when the find button is clicked', () => {
-    const { getByRole, onFindClick } = renderComponent();
-    userEvent.click(getByRole('button', { name: /request\sdata/i }));
-    expect(onFindClick).toHaveBeenCalled();
-  });
-
   it('Shows the results if there are any', () => {
-    const { getByText } = renderComponent();
+    const selectedLayer = { source_id: 'test-source-id' };
+    const store = mockStore({});
 
-    expect(getByText('List of Builds')).toBeInTheDocument();
-
-    RESULTS.forEach(result => {
-      expect(getByText(result.properties['Project ID'])).toBeInTheDocument();
-      expect(
-        getByText('Address: ' + result.properties['Address']),
-      ).toBeInTheDocument();
-    });
-  });
-
-  it('Shows a loading spinner in the button if loading', () => {
-    const { getByTestId } = renderComponent({ isLoading: true });
-    expect(getByTestId('button-progress')).toBeInTheDocument();
-  });
-
-  it("Shows a skeleton results list if loading and there aren't already results", () => {
-    const { getAllByRole } = renderComponent({
-      isLoading: true,
-      results: null,
-    });
-    expect(getAllByRole('progressbar').length).toBeGreaterThanOrEqual(1);
-  });
-
-  it("Doesn't show anything if not loading, no results", () => {
-    const { queryByText } = renderComponent({
-      results: null,
-    });
-    expect(queryByText('List of Vessels')).not.toBeInTheDocument();
-  });
-
-  it('calls onRadioChange when the radio is clicked', () => {
-    const { getByRole, onRadioChange } = renderComponent();
-    userEvent.click(getByRole('radio'));
-    expect(onRadioChange).toHaveBeenCalled();
-  });
-
-  it('hides the button and results when not visible', () => {
-    const { queryByRole } = renderComponent({ visible: false });
-    expect(
-      queryByRole('button', { name: 'Request Data' }),
-    ).not.toBeInTheDocument();
-    expect(queryByRole('list')).not.toBeInTheDocument();
-  });
-
-  it("shows items as active if there's no active result", () => {
-    const { getAllByRole } = renderComponent();
-    const [, ...items] = getAllByRole('listitem');
-    items.forEach(element => expect(element).toHaveClass('Mui-selected'));
-  });
-
-  it('shows the active result as active', () => {
-    const { getByRole } = renderComponent({
-      selectedResult: { id: 1 },
-    });
-    expect(
-      getByRole('button', {
-        name: /Test Title 1/i,
-      }),
-    ).toHaveClass('Mui-selected');
-    expect(getByRole('button', { name: /Test Title 2/i })).not.toHaveClass(
-      'selected',
+    render(
+      <PldSidebarComponent
+        selectedLayer={selectedLayer}
+        color="black"
+        iconColor="white"
+        constructionPhaseFilters={CONSTRUCTION_PHASES}
+        developmentTypeFilters={DEVELOPMENT_TYPES}
+      />,
+      {
+        wrapper: ({ children }) => (
+          <Provider store={store}>{children}</Provider>
+        ),
+      },
     );
-  });
 
-  it('Does not show pagination if pages <= 1', () => {
-    const { queryByRole } = renderComponent();
-    expect(queryByRole('button', { name: '1' })).not.toBeInTheDocument();
-  });
+    expect(
+      screen.getByRole('heading', { name: /date range/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('textbox', { name: /start date/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('textbox', { name: /end date/i }),
+    ).toBeInTheDocument();
 
-  it('Shows pagination if pages > 1', () => {
-    const { getByRole } = renderComponent({
-      pages: 3,
-    });
-    expect(getByRole('button', { name: 'page 1' })).toBeInTheDocument();
-  });
+    expect(
+      screen.getByRole('heading', { name: /construction phase/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /approved/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /commenced/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /completed/i }),
+    ).toBeInTheDocument();
 
-  it('Calls onPageClick with the page number when a number button is clicked', () => {
-    const { getByRole, onPageClick } = renderComponent({
-      pages: 3,
-    });
-    userEvent.click(getByRole('button', { name: 'Go to page 2' }));
-    expect(onPageClick).toHaveBeenCalledWith(2);
+    expect(
+      screen.getByRole('heading', { name: /development type/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /conversion/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /new build/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /change of use/i }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('checkbox', { name: /extension/i }),
+    ).toBeInTheDocument();
   });
 });
