73c4750363cefae2a770a4900c55103af474eb9e
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-10-22 11:17:22 +0100

    Add getLineColor and getFilledColor functions

--- client/src/map/orbs/configurations/geoJsonConfig.js
+++ client/src/map/orbs/configurations/geoJsonConfig.js
@@ -1,31 +1,82 @@
+// @ts-ignore
 import { GeoJsonLayer } from '@deck.gl/layers';
 
 import { hexToRgbArray } from 'utils/color';
 
 import { dataSelector, visibilitySelector } from '../layers.slice';
 
-export const DEFAULT_COLOR = '#000000';
+export const DEFAULT_LINE_COLOR = '#00ff00';
+export const DEFAULT_FILLED_COLOR = '#006400';
+export const DEFAULT_HIGHLIGHT_COLOR = '#483D8B';
+
+export const STROKED_TRANSPARENT_COLOR = [0, 255, 0, 0];
+export const FILLED_TRANSPARENT_COLOR = [0, 100, 0, 0];
 
 /** @type {import("typings/orbis").LayerConfiguration} */
-export default ({
+
+/**
+ * @param {{
+ *   id: string
+ *   filled: boolean
+ *   stroked: boolean
+ *   activeSources?: import('typings').Source[]
+ *   orbState: import('../orbReducer').OrbState
+ *   authToken?: string
+ * }} parameters
+ */
+const geoJsonConfiguration = ({
   id,
   activeSources,
   orbState,
-  color,
   filled,
+  stroked,
+  //@ts-ignore
+  lineColor,
+  // @ts-ignore
+  filledColor,
+  //@ts-ignore
+  highlightColor,
   authToken,
   ...rest
 }) => {
+  // @ts-ignore
   const source = activeSources?.find(source => source.source_id === id);
   const data = dataSelector(id)(orbState);
   const visible = visibilitySelector(id)(orbState);
+  stroked = stroked ? true : false;
+  // filled = filled || filled === undefined ? true : false;
 
-  console.log('foo');
-  console.log(color);
+  console.log('Showed filled value', filled);
+  console.log('Stroked value', stroked);
+  console.log('Color', lineColor);
 
   /* a layer can be filled or not-filled */
-  color = color ? hexToRgbArray(color) : hexToRgbArray(DEFAULT_COLOR);
+  lineColor = lineColor
+    ? hexToRgbArray(lineColor)
+    : hexToRgbArray(DEFAULT_LINE_COLOR);
 
+  console.log(lineColor);
+  filledColor = filledColor
+    ? hexToRgbArray(filledColor)
+    : hexToRgbArray(DEFAULT_FILLED_COLOR);
+
+  highlightColor = highlightColor
+    ? hexToRgbArray(highlightColor)
+    : hexToRgbArray(DEFAULT_HIGHLIGHT_COLOR);
+
+  function getFilledColor() {
+    // console.log('hello');
+    if (filled || filled === undefined) {
+      return filledColor;
+    }
+  }
+
+  function getLineColor() {
+    if (stroked || stroked === undefined) {
+      return lineColor;
+    }
+  }
+  // filled = false;
   /* get color from props */
   /* if filled: fillcolor=whatever, linecolor=transparent, highlightcolor=whatever-inverse */
   /* if not filled: fillcolor=transparent, linecolor=whatever, highlightcolor=whatever-inverse */
@@ -36,12 +87,15 @@ export default ({
     visible,
     pickable: true,
     autoHighlight: true,
-    filled: true,
-    stroked: true,
+    filled,
+    stroked,
+    // extruded: false,
+    // pointType: 'circle',
     getLineWidth: 2,
-    lineWidthMinPixels: 2,
-    getLineColor: color, //[255, 0, 0, 255],
-    getFillColor: [0, 0, 0, 0],
+    lineWidthMinPixels: 1,
+    getLineColor: getLineColor,
+    getFillColor: getFilledColor,
+    highlightColor: highlightColor,
     // minZoom: source?.metadata?.minZoom,
     // maxZoom: source?.metadata?.maxZoom,
     loadOptions: {
@@ -54,3 +108,5 @@ export default ({
     ...rest,
   };
 };
+
+export default geoJsonConfiguration;
