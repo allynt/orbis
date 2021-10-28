492ebae130a3a4a1f6d6dddf2dd9c27c75c934e9
Reignable <joebarret401@gmail.com>
2021-10-28 09:54:21 +0100

    ORB-820: chore(frontend): Add transitions
    ORB-820

--- client/src/data-layers/user-upload/user-upload.component.js
+++ client/src/data-layers/user-upload/user-upload.component.js
@@ -1,21 +1,36 @@
 import React, { useState } from 'react';
 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import { useFadeTransitionProps } from 'mission-control/shared-components/useFadeTransitionProps';
+
 import { Form } from './form/form.component';
 import { Intro } from './intro/intro.component';
 import { Success } from './success/success.component';
 
+const Stages = { intro: 'intro', form: 'form', success: 'success' };
+
 const UserUpload = ({ onComplete }) => {
-  const [stage, setStage] = useState('intro');
+  const [stage, setStage] = useState(Stages.intro);
+  const fadeTransitionProps = useFadeTransitionProps(stage);
   return (
     <>
-      {stage === 'intro' && <Intro onNextClick={() => setStage('form')} />}
-      {stage === 'form' && (
-        <Form
-          onBackClick={() => setStage('intro')}
-          onSubmit={() => setStage('success')}
-        />
-      )}
-      {stage === 'success' && <Success onOkClick={onComplete} />}
+      <TransitionGroup style={{ position: 'relative' }}>
+        <CSSTransition {...fadeTransitionProps}>
+          <div style={{ position: 'absolute', inset: 0 }}>
+            {stage === Stages.intro && (
+              <Intro onNextClick={() => setStage(Stages.form)} />
+            )}
+            {stage === Stages.form && (
+              <Form
+                onBackClick={() => setStage(Stages.intro)}
+                onSubmit={() => setStage(Stages.success)}
+              />
+            )}
+            {stage === Stages.success && <Success onOkClick={onComplete} />}
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
     </>
   );
 };
