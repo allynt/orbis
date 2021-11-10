324fb7439afb463bf10555aef79ecd05b303c25b
Mark Small <mark.small@astrosat.space>
2021-11-10 21:12:48 +0000

    feat(frontend): Add thunk and tests to save AOIs
    Added an async thunk function to save a new AOI, along with tests
    for it. I will be adding fetch thunk at a later date.
    
    IssueID ORB-720

--- client/src/data-layers/aoi/aoi.slice.js
+++ client/src/data-layers/aoi/aoi.slice.js
@@ -1,12 +1,47 @@
-import { createSlice, createSelector } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  createSelector,
+  createAsyncThunk,
+} from '@reduxjs/toolkit';
+import { NotificationManager } from 'react-notifications';
+
+import { userSelector } from 'accounts/accounts.selectors';
+import apiClient from 'api-client';
 
 import { Panels } from '../data-layers.constants';
 
 const name = 'aois';
 
+export const saveAoi = createAsyncThunk(
+  `${name}/saveAoi`,
+  async (params, { getState, dispatch, rejectWithValue }) => {
+    const { id: userId, customers } = userSelector(getState());
+    const { id: customerId } = customers[0];
+
+    try {
+      await apiClient.aois.saveAoi({ customerId, userId });
+      NotificationManager.success(`Successfully saved AOI '${params.name}'`);
+      return;
+    } catch (responseError) {
+      const message = await responseError.getErrors();
+      NotificationManager.error(
+        `Error saving AOI: ${message} Please try again`,
+      );
+      return rejectWithValue({ message });
+    }
+  },
+  {
+    condition: (_, { getState }) => {
+      const requestId = getState().aois.requests?.saveAoi;
+      return !requestId;
+    },
+  },
+);
+
 const initialState = {
   isDrawingAoi: false,
   visiblePanel: Panels.DATA_LAYERS,
+  requests: {},
 };
 
 const satellitesSlice = createSlice({
@@ -27,6 +62,18 @@ const satellitesSlice = createSlice({
     setVisiblePanel: (state, { payload }) => {
       state.visiblePanel = payload;
     },
+  },
+  extraReducers: builder => {
+    builder.addCase(saveAoi.pending, (state, { meta }) => {
+      state.requests.saveAoi = meta.requestId;
+    });
+    builder.addCase(saveAoi.fulfilled, state => {
+      state.requests.saveAoi = undefined;
+    });
+    builder.addCase(saveAoi.rejected, (state, { error }) => {
+      state.requests.saveAoi = undefined;
+      state.error = { message: error.message };
+    });
   },
 });
 
--- client/src/data-layers/aoi/aoi.slice.test.js
+++ client/src/data-layers/aoi/aoi.slice.test.js
@@ -1,12 +1,138 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { waitFor } from 'test/test-utils';
+
 import reducer, {
   startDrawingAoi,
   endDrawingAoi,
   onUnmount,
   aoiSelector,
   isDrawingAoiSelector,
+  saveAoi,
 } from './aoi.slice';
 
+const mockStore = configureMockStore([thunk]);
+
 describe('AOIs Slice', () => {
+  describe('AOI Thunks', () => {
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+
+    describe('pending', () => {
+      it('should set the saveAoi request to the request id', () => {
+        const result = reducer(
+          { requests: {} },
+          { type: saveAoi.pending.type, meta: { requestId: 'id-123' } },
+        );
+
+        expect(result).toEqual(
+          expect.objectContaining({ requests: { saveAoi: 'id-123' } }),
+        );
+      });
+    });
+
+    describe('fulfilled', () => {
+      it(`should dispatch the ${saveAoi.fulfilled.type}`, async () => {
+        fetch.once(JSON.stringify({ source_id: 'source-id-123' }));
+        const store = mockStore({
+          accounts: {
+            user: {
+              id: 'user-id-123',
+              customers: [{ id: 'customer-id-123' }],
+            },
+          },
+          aois: {},
+        });
+        store.dispatch(
+          saveAoi({ name: 'Test name', description: 'Test description' }),
+        );
+
+        await waitFor(() =>
+          expect(store.getActions()).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                type: saveAoi.fulfilled.type,
+              }),
+            ]),
+          ),
+        );
+      });
+    });
+
+    describe('rejected', () => {
+      it(`should dispatch the ${saveAoi.rejected.type} action on rejected request`, async () => {
+        fetch.once(
+          JSON.stringify({
+            message: 'Test error message',
+          }),
+          {
+            ok: false,
+            status: 401,
+            statusText: 'Test Error',
+          },
+        );
+        const store = mockStore({
+          accounts: {
+            user: {
+              id: 'user-id-123',
+              customers: [{ id: 'customer-id-123' }],
+            },
+          },
+          aois: {},
+        });
+
+        store.dispatch(
+          saveAoi({ name: 'Test name', description: 'Test description' }),
+        );
+
+        await waitFor(() =>
+          expect(store.getActions()).toContainEqual(
+            expect.objectContaining({
+              type: saveAoi.rejected.type,
+              error: expect.anything(),
+            }),
+          ),
+        );
+      });
+
+      it('should clear the saveAoi request and set the error', () => {
+        const result = reducer(
+          { requests: { saveAoi: '123' } },
+          {
+            type: saveAoi.rejected.type,
+            error: { message: 'Test message' },
+          },
+        );
+
+        expect(result.requests.saveAoi).toBeUndefined();
+        expect(result.error).toEqual({ message: 'Test message' });
+      });
+
+      it("should not request again if there's already a request pending", () => {
+        const store = mockStore({
+          accounts: {
+            user: { id: 'user-id-123', customers: [{ id: 'customer-id-123' }] },
+          },
+          aois: {
+            requests: {
+              saveAoi: true,
+            },
+          },
+        });
+
+        store.dispatch(
+          saveAoi({ name: 'Test name', description: 'Test description' }),
+        );
+
+        expect(store.getActions()).not.toContainEqual(
+          expect.objectContaining({ type: saveAoi.pending.type }),
+        );
+      });
+    });
+  });
+
   describe('AOI Actions', () => {
     describe('startDrawingAoi', () => {
       let result;
