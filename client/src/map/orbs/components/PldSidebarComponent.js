0472e6b762080f83e0393f1251f4e1cd3fd2ea1e
Mark Small <mark.small@astrosat.space>
2021-11-11 20:59:50 +0000

    fix(frontend): Set min/max range for date filtering
    Added code to take min/max dates from the metadata and apply them
    to the date range picker. This prevents the user from setting dates
    outside the min/max values.
    
    IssueID ORB-980

--- client/src/map/orbs/components/PldSidebarComponent.js
+++ client/src/map/orbs/components/PldSidebarComponent.js
@@ -8,12 +8,16 @@ const ConnectedWrapper = ({ selectedLayer, dispatch }) => {
     constructionPhaseFilters,
     developmentTypeFilters,
     iconColor,
+    minFilterDate,
+    maxFilterDate,
   } = selectedLayer?.metadata?.application?.orbis?.sidebar_component?.props;
   return (
     <PldSidebarComponent
       selectedLayer={selectedLayer}
       dispatch={dispatch}
       color={color}
+      minFilterDate={minFilterDate}
+      maxFilterDate={maxFilterDate}
       constructionPhaseFilters={constructionPhaseFilters}
       developmentTypeFilters={developmentTypeFilters}
       iconColor={iconColor}
