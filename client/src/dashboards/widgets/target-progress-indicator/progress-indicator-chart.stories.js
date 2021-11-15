89db98f3b803be353eef4b430a30076fe140c982
Rory MacGregor <rorymacgregor@astrosat.net>
2021-11-15 17:02:38 +0000

    fix(frontend): Removed WidgetWrapper from stories
    The WidgetWrapper was being included in the stories for the progress indicator, this removes.

--- client/src/dashboards/widgets/target-progress-indicator/progress-indicator-chart.stories.js
+++ client/src/dashboards/widgets/target-progress-indicator/progress-indicator-chart.stories.js
@@ -1,29 +1,18 @@
 import React from 'react';
 
 import { COLORS } from '../../constants';
-import { Widget } from '../widget.component';
 import { ProgressIndicatorChart } from './progress-indicator-chart.component';
 
 export default {
   title: 'Dashboards/Widgets/Progress Indicator Chart',
 };
 
-const Template = ({ source }) => {
-  return (
-    <Widget title={source?.title} info={source?.info}>
-      <ProgressIndicatorChart source={source} />
-    </Widget>
-  );
-};
+const Template = ({ source }) => <ProgressIndicatorChart source={source} />;
 
 export const Default = Template.bind({});
 Default.args = {
   source: {
     name: 'Housing Delivery',
-    title:
-      '% Houses Delivered so Far Out of Housing Delivery Target For Previous 5 Financial Years.',
-    info:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, nisi excepturi. Nemo quia iusto, vel tempora deleniti suscipit rerum soluta inventore consectetur ab consequatur doloribus beatae vero saepe ex magnam.',
     color: COLORS.blue,
     target: 300,
     progress: 240,
@@ -35,13 +24,5 @@ NoProgressOrTarget.args = {
   source: {
     ...Default.args.source,
     progress: undefined,
-  },
-};
-
-export const NoInfo = Template.bind({});
-NoInfo.args = {
-  source: {
-    ...Default.args.source,
-    info: undefined,
   },
 };
