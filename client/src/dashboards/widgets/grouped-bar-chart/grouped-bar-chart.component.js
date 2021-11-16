e3088f677a151e6d02ca0dc18acc8255e809dffd
Rory MacGregor <rorymacgregor@astrosat.net>
2021-11-16 11:08:45 +0000

    fix(frontend): Add mockdata to grouped chart stories
    Added the mockData from the data team to the stories for the grouped bar chart, and converted to fit the chart's data shape.

--- client/src/dashboards/widgets/grouped-bar-chart/grouped-bar-chart.component.js
+++ client/src/dashboards/widgets/grouped-bar-chart/grouped-bar-chart.component.js
@@ -8,18 +8,20 @@ import { BaseChart } from '../base-chart/base-chart.component';
 const GroupedBarChart = ({ xLabel = '', yLabel = '', data }) => {
   const chartTheme = useChartTheme();
 
+  // TODO: width and offset responsive values
+
   const renderWidget = width => {
     const barWidth = width / data.length,
       offset = width / barWidth;
     return (
-      <VictoryGroup offset={offset}>
+      <VictoryGroup offset={30}>
         {data.map((arr, i) => (
           <VictoryBar
             // eslint-disable-next-line react/no-array-index-key
             key={`dataset-${i}`}
             data={arr}
             style={{
-              data: { fill: chartTheme.colors[i], width: barWidth },
+              data: { fill: chartTheme.colors[i], width: 20 },
             }}
           />
         ))}
--- client/src/dashboards/widgets/grouped-bar-chart/grouped-bar-chart.stories.js
+++ client/src/dashboards/widgets/grouped-bar-chart/grouped-bar-chart.stories.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import * as data from '../../mock-data/waltham-forest/mock_total_housing_delivery';
 import { GroupedBarChart } from './grouped-bar-chart.component';
 
 export default {
   title: 'Dashboards/Widgets/Grouped Bar Chart',
 };
 
-const data = new Array(5).fill(undefined).map((_, i) => [
-  { x: 1, y: Math.floor(Math.random() * 100) },
-  { x: 2, y: Math.floor(Math.random() * 100) },
-  { x: 3, y: Math.floor(Math.random() * 100) },
-]);
+const mockData = data.properties[0].data.reduce(
+  (acc, cur) => ({
+    gross: [...acc.gross, { x: cur.Year, y: cur['Total Gross'] }],
+    net: [...acc.gross, { x: cur.Year, y: cur['Total Gross'] }],
+  }),
+  { gross: [], net: [] },
+);
 
 const Template = args => {
   return <GroupedBarChart {...args} />;
@@ -18,7 +21,7 @@ const Template = args => {
 
 export const Default = Template.bind({});
 Default.args = {
-  xLabel: 'This is X label',
-  yLabel: 'This is Y label',
-  data,
+  xLabel: 'Year',
+  yLabel: data.properties[0].name,
+  data: [...Object.values(mockData)],
 };
