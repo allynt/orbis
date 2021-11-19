bf1d64061cf34ab09b513e0e7aa3f6512f559e06
Rory MacGregor <rorymacgregor@astrosat.net>
2021-11-19 11:57:49 +0000

    fix(frontend): Create dashboards slice
    Created a dashboards slice and added to the root reducer.

---
+++ client/src/dashboards/dashboards.slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const initialState = {};
+
+const dashboardsSlice = createSlice({
+  name: 'dashboards',
+  initialState,
+  reducers: {},
+});
+
+export const {} = dashboardsSlice.actions;
+
+export default dashboardsSlice.reducer;
--- client/src/root.reducer.js
+++ client/src/root.reducer.js
@@ -7,6 +7,7 @@ import drawingTools from 'drawing-tools/drawing-tools.slice';
 import accounts from './accounts/accounts.slice';
 import app from './app.slice';
 import bookmarks from './bookmarks/bookmarks.slice';
+import dashboards from './dashboards/dashboards.slice';
 import data from './data-layers/data-layers.slice';
 import map from './map/map.slice';
 import orbs from './map/orbs/orbReducer';
@@ -29,6 +30,7 @@ const createRootReducer = history =>
     satellites,
     stories,
     missionControl,
+    dashboards,
   });
 
 const rootReducer = createRootReducer(history);
