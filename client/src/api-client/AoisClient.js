f19f7401c6a0b7d8952ffaaff074ce82b81b8a87
Mark Small <mark.small@astrosat.space>
2021-11-24 15:18:32 +0000

    fix(frontend): Add extra functions to API client for AOIs
    I've updated the `saveAoi` which is used in this ticket, but also
    added the others to fetch/update/delete AOIs. The UI aspects of these
    will be added in a future branch though.
    
    IssueID ORB-720

--- client/src/api-client/AoisClient.js
+++ client/src/api-client/AoisClient.js
@@ -1,5 +1,3 @@
-import { snakeCase } from 'lodash';
-
 import { SubClient } from './SubClient';
 
 export class AoiClient extends SubClient {
@@ -7,13 +5,46 @@ export class AoiClient extends SubClient {
     super('/aois');
   }
 
-  async saveAoi({ customerId, userId, ...rest }) {
-    return this.makeAuthenticatedPostRequest(
-      `/${customerId}/${userId}/`,
-      Object.entries(rest).reduce(
-        (acc, [key, value]) => ({ ...acc, [snakeCase(key)]: value }),
-        {},
-      ),
-    );
+  async getAois() {
+    const response = await this.makeAuthenticatedRequest('/');
+    return response.json();
+  }
+
+  async saveAoi({ geometry, ...rest }) {
+    const formData = new FormData();
+    Object.keys(rest).forEach(key => formData.set(key, rest[key]));
+    formData.set('geometry', JSON.stringify(geometry));
+
+    const response = await this.makeAuthenticatedRequest('/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, application/xml, text/plain, text/html, *.*',
+      },
+      body: formData,
+    });
+
+    const result = response.json();
+
+    return result;
+  }
+
+  async updateAoi(aoi) {
+    const formData = new FormData();
+    Object.keys(aoi).forEach(key => formData.set(key, aoi[key]));
+
+    const response = await this.makeAuthenticatedRequest(`/${aoi.id}/`, {
+      method: 'PUT',
+      body: formData,
+    });
+
+    const result = response.json();
+
+    return result;
+  }
+
+  async deleteAoi(aoiId) {
+    await this.makeAuthenticatedRequest(`/${aoiId}/`, {
+      method: 'DELETE',
+    });
   }
 }
--- client/src/api-client/AoisClient.test.js
+++ client/src/api-client/AoisClient.test.js
@@ -7,40 +7,80 @@ describe('AoisClient', () => {
     client = new AoiClient();
   });
 
-  it('should return the response from the post request', async () => {
-    const responseBody = { source_id: 'id-123' };
-    fetch.once(JSON.stringify(responseBody));
+  it('should return AOIs from the response', async () => {
+    const aois = [{ id: 1 }, { id: 2 }];
+    fetch.once(JSON.stringify(aois));
 
-    const requestBody = {
-      userId: 'user-id-123',
-      customerId: 'customer-id-123',
+    const client = new AoiClient();
+    const responseAois = await client.getAois();
+
+    expect(responseAois).toEqual(aois);
+  });
+
+  it('should return the response from the Add AOI request', async () => {
+    const newAoi = {
       name: 'Test Name',
       description: 'Test description',
-      geometry: [
-        [0, 0],
-        [0, 1],
-        [1, 1],
-        [1, 0],
-        [0, 0],
-      ],
-      zoom: 10,
+      geometry: JSON.stringify({
+        type: 'Polygon',
+        geometry: {
+          coordinates: [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [0, 0],
+          ],
+        },
+      }),
+      // thumbnail: 10,
     };
-    const response = await client.saveAoi(requestBody);
+    const responseAoi = { ...newAoi, id: 123 };
+    fetch.once(JSON.stringify(responseAoi));
+
+    const aoi = await client.saveAoi(newAoi);
+
+    expect(aoi).toEqual(responseAoi);
+  });
+
+  it('should update the AOI', async () => {
+    const updatedAoi = {
+      id: 1,
+      name: 'Test Name Updated',
+      description: 'Test description',
+      geometry: JSON.stringify({
+        type: 'Polygon',
+        geometry: {
+          coordinates: [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [2, 2],
+            [0, 0],
+          ],
+        },
+      }),
+      // thumbnail: 10,
+    };
+    fetch.once(JSON.stringify(updatedAoi));
+
+    const client = new AoiClient();
+    await client.updateAoi(updatedAoi);
 
     expect(fetch).toBeCalledWith(
-      expect.stringContaining(
-        `/${requestBody.customerId}/${requestBody.userId}/`,
-      ),
-      expect.objectContaining({
-        body: JSON.stringify({
-          name: requestBody.name,
-          description: requestBody.description,
-          geometry: requestBody.geometry,
-          zoom: requestBody.zoom,
-        }),
-      }),
+      expect.stringContaining('/api/aois/1/'),
+      expect.objectContaining({ method: 'PUT' }),
     );
+  });
 
-    expect(response).toEqual(responseBody);
+  it('should delete the AOI', async () => {
+    const client = new AoiClient();
+    await client.deleteAoi(1);
+
+    expect(fetch).toBeCalledWith(
+      expect.stringContaining('/api/aois/1'),
+      expect.objectContaining({ method: 'DELETE' }),
+    );
   });
 });
