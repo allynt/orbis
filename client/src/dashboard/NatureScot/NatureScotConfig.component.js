d5486c15d0834fcf3753563a7d793bbff4b5a0b0
Mark Small <mark.small@astrosat.space>
2021-11-30 22:44:31 +0000

    fix(frontend): Add fetching of data from IR
    I added the `useEffect` to fetch data via the PROXY, from the IR API.
    I then take that data and update some local state, which in turn is
    used as props to the sub-components.
    
    I also played around with the styling to make it look a bit more like
    the design.

--- client/src/dashboard/NatureScot/NatureScotConfig.component.js
+++ client/src/dashboard/NatureScot/NatureScotConfig.component.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Grid, makeStyles, Typography } from '@astrosat/astrosat-ui';
 
 import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
+import { format } from 'date-fns';
 import { NotificationManager } from 'react-notifications';
 import { useSelector } from 'react-redux';
 import { useSortBy } from 'react-table';
@@ -17,32 +18,22 @@ import { NearestProtectedAreas } from './nearest-protected-areas';
 import ProtectedFeature from './protected-feature/protected-feature.component';
 
 const useStyles = makeStyles(theme => ({
-  dashboard: {
-    // justifyContent: 'space-between',
-    // alignItems: 'center',
-    overflowY: 'scroll',
+  subRow: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  header: {
+  dashboard: {
+    justifyContent: 'space-evenly',
     padding: '2rem',
     borderBottom: `1px solid ${theme.palette.primary.main}`,
+    overflowY: 'scroll',
   },
 }));
 
-const NEAREST_PROTECTED_AREAS = [
-  { name: 'Loch Lomond', type: 'National Park', distance: 4 },
-  { name: 'Drumnadrochit', type: 'Protected Area', distance: 8 },
-  { name: 'Ardnamurchan', type: 'National Park', distance: 12 },
-  { name: 'Glenfinnan', type: 'Historic Site', distance: 13 },
-  { name: 'Glencoe', type: 'SSSI', distance: 32 },
-  { name: 'Tarbert', type: 'Historic Site', distance: 34 },
-  { name: 'Cumbernauld', type: 'Supersite', distance: 39 },
-  { name: 'Culzean Castle', type: 'Historic Site', distance: 55 },
-];
-
 const BUTTONS = [
-  { label: 'button one' },
-  { label: 'button two' },
-  { label: 'button three' },
+  { label: 'Species' },
+  { label: 'Geology' },
+  { label: 'Habitat' },
 ];
 
 const TYPES = ['warning', 'not-good', 'neutral', 'good', 'very-good'];
@@ -59,70 +50,15 @@ const PROTECTED_FEATURES = Array(5)
     };
   });
 
-const CASEWORKS = [
-  {
-    code: '9540',
-    received: '"2003-01-16T00:00:00.000Z"',
-    response:
-      'C1 - No objection - little or no natural heritage interests affected',
-    type: 'SEPA Consultations',
-    subRows: undefined,
-  },
-  {
-    code: '11878',
-    received: '"2003-06-26T00:00:00.000Z"',
-    response:
-      'C1 - No objection - little or no natural heritage interests affected',
-    type: 'Other Developments',
-    subRows: undefined,
-  },
-  {
-    code: '13626',
-    received: '"2003-09-12T00:00:00.000Z"',
-    response:
-      'C4 - No objection but recommend conditions/modifications - natural heritage interests of lesser importance with substantial development impacts',
-    type: 'Scottish Forestry Grant Scheme',
-    subRows: undefined,
-  },
-  {
-    code: '13629',
-    received: '"2003-08-14T00:00:00.000Z"',
-    response: 'B. Support',
-    type: 'Woodland Grant Scheme',
-    subRows: undefined,
-  },
-  {
-    code: '13058',
-    received: '"2003-08-14T00:00:00.000Z"',
-    response:
-      'C1 - No objection - little or no natural heritage interests affected',
-    type: 'Housing Developments',
-    subRows: undefined,
-  },
-  {
-    code: '11062',
-    received: '"2003-05-09T00:00:00.000Z"',
-    response: 'Information/Advice only',
-    type: 'Housing Developments',
-    subRows: undefined,
-  },
-  {
-    code: '15485',
-    received: '"2003-12-23T00:00:00.000Z"',
-    response: 'Information/Advice only',
-    type: 'Site Search',
-    subRows: undefined,
-  },
-];
-
 const COLUMNS = [
   {
     Header: 'Code',
-    accessor: 'code',
+    accessor: 'name',
   },
   {
     Header: 'Received',
-    accessor: 'received',
+    accessor: 'date',
+    Cell: ({ value }) => format(new Date(value), 'dd/MM/yyyy'),
   },
   {
     // Make an expander cell
@@ -145,10 +81,10 @@ const NatureScotDashboard = () => {
   const [nearestProtectedAreas, setNearestProtectedAreas] = useState([]);
   const [caseworks, setCaseworks] = useState([]);
   const [protectedFeatures, setProtectedFeatures] = useState([]);
+  const [contactDetails, setContactDetails] = useState({});
   const authToken = useSelector(selectDataToken);
 
   const selectedAoi = useSelector(selectedAoiSelector);
-  console.log('SELECTED AOI: ', selectedAoi);
 
   useEffect(() => {
     // Fetch data from IR API.
@@ -156,12 +92,18 @@ const NatureScotDashboard = () => {
       try {
         const response = await apiClient.dashboard.getNatureScotlandIRDashboardData(
           selectedAoi.geometry,
-          { headers: { Authorization: `Bearer ${authToken}` } },
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+              'Content-Type': 'application/json',
+            },
+          },
         );
-        console.log('IR API RESPONSE: ', response);
 
-        setNearestProtectedAreas(NEAREST_PROTECTED_AREAS);
-        setCaseworks(CASEWORKS);
+        setNearestProtectedAreas(response.protected_areas);
+        setCaseworks(response.casework);
+        setContactDetails(response.contact_details);
+        // setProtectedFeatures(response.protected_features);
         setProtectedFeatures(PROTECTED_FEATURES);
       } catch (error) {
         const { message, status } = error;
@@ -172,58 +114,49 @@ const NatureScotDashboard = () => {
           () => {},
         );
       }
-
-      setNearestProtectedAreas(NEAREST_PROTECTED_AREAS);
-      setCaseworks(CASEWORKS);
-      setProtectedFeatures(PROTECTED_FEATURES);
     };
 
     queryApi();
   }, []);
 
-  const renderRowSubComponent = React.useCallback(({ row }) => {
-    console.log('ROW DATA: ', row.original);
-    return (
-      <Grid
-        container
-        rowSpacing={1}
-        coloumnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        className={styles.dashboard}
-      >
-        <Grid item xs={1}>
+  const renderRowSubComponent = React.useCallback(
+    ({ row }) => (
+      <Grid className={styles.subRow} container>
+        <Grid item xs={3}>
           <Typography variant="h4">Caseword Title:</Typography>
         </Grid>
-        <Grid item xs={11}>
-          <Typography>Type Title Here</Typography>
+        <Grid item xs={9}>
+          <Typography>{row.original.name}</Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={3}>
           <Typography variant="h4">Site Name:</Typography>
         </Grid>
-        <Grid item xs={11}>
-          <Typography>Type Title Here</Typography>
+        <Grid item xs={9}>
+          <Typography>{row.original.name}</Typography>
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={3}>
           <Typography variant="h4">Site Type:</Typography>
         </Grid>
-        <Grid item xs={11}>
-          <Typography>Type Title Here</Typography>
+        <Grid item xs={9}>
+          <Typography>{row.original.name}</Typography>
         </Grid>
-        <Grid item xs={1}>
-          <Typography variant="h4">Casework Decsion:</Typography>
+        <Grid item xs={3}>
+          <Typography variant="h4">Casework Decision:</Typography>
         </Grid>
-        <Grid item xs={11}>
-          <Typography>Type Title Here</Typography>
+        <Grid item xs={9}>
+          <Typography>{row.original.status}</Typography>
         </Grid>
       </Grid>
-    );
-  }, []);
+    ),
+    [],
+  );
 
   return (
     <Grid
       container
-      className={styles.header}
+      className={styles.dashboard}
       rowSpacing={1}
-      coloumnSpacing={{ xs: 4, sm: 2, md: 3 }}
+      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
     >
       <Grid item xs={6}>
         <NearestProtectedAreas data={nearestProtectedAreas} />
