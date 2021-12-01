f7fe51e73102f917336f3f90235d9654031b1722
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-12-01 09:29:24 +0000

    Refactor the area office contact detail component

--- client/src/dashboard/NatureScot/area-office-contact-details.component.js
+++ client/src/dashboard/NatureScot/area-office-contact-details.component.js
@@ -4,17 +4,9 @@ import { makeStyles, Grid, Typography, Link } from '@astrosat/astrosat-ui';
 
 import { ChartWrapper } from 'dashboard/charts/chart-wrapper.component';
 
-const useStyles = makeStyles(theme => ({
-  data: {
-    display: 'flex',
-  },
-  singleData: {
-    marginLeft: '4rem',
-    alignContent: 'center',
-  },
-}));
+const useStyles = makeStyles(theme => ({}));
 
-const AreaOfficeContactDetails = args => {
+const AreaOfficeContactDetails = () => {
   const styles = useStyles({});
   return (
     <ChartWrapper title="Area Office Contact Detail">
@@ -26,38 +18,31 @@ const AreaOfficeContactDetails = args => {
       >
         {/* <Grid item container> */}
         <Grid item xs={6}>
-          <Typography variant="h5" className={styles.data}>
-            Area Office: <div className={styles.singleData}>Data here</div>
-          </Typography>
+          <Typography variant="h4">Area Office:</Typography>
+          <Typography align="center">Data here</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="h5" className={styles.data}>
-            Area Name: <div className={styles.singleData}>Data here</div>
-          </Typography>
+          <Typography variant="h4">Area Name: </Typography>
+          <Typography align="center">Data here</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="h5" className={styles.data}>
-            Telephone Number:
-            <div className={styles.singleData}>Data here</div>
-          </Typography>
+          <Typography variant="h4">Telephone Number:</Typography>
+          <Typography align="center">Data here</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="h5" className={styles.data}>
-            Area Office Address:
-            <div className={styles.singleData}>Data here</div>
-          </Typography>
+          <Typography variant="h4">Area Office Address:</Typography>
+          <Typography align="center">Data here</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="h5" className={styles.data}>
-            Post Code: <div className={styles.singleData}>Data here</div>
-          </Typography>
+          <Typography variant="h4">Post Code</Typography>
+          <Typography align="center">Data here</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography variant="h5" className={styles.data}>
-            Email:
-            <div className={styles.singleData}>
-              <Link>Test</Link>
-            </div>
+          <Typography variant="h4">Email:</Typography>
+          <Typography align="center">
+            <Link target="_blank" href="" rel="noopener noreferrer">
+              test@test.com
+            </Link>
           </Typography>
         </Grid>
       </Grid>
