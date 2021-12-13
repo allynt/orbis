490d7697a19bec7024547810e083df0f8f6315d8
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-12-13 08:57:25 +0000

    feat(frontend): Refactored the contact detail test

--- client/src/dashboard/NatureScot/contact-detail.test.js
+++ client/src/dashboard/NatureScot/contact-detail.test.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-import { render, screen } from 'test/test-utils';
+import { render, screen, within } from 'test/test-utils';
 
 import AreaOfficeContactDetail from './area-office-contact-details.component';
 
-const CONTACT_DETAILS = {
-  area_office: 'Test Area Office',
-  area_name: 'East Highland',
-  telephone_number: '01526272666',
-  area_office_address: ['test area1', 'test area2', 'test area3'],
-  postcode: 'eh21 8bj',
-  email: 'test@test.com',
-};
-
 describe('<AreaOfficeContactDetail />', () => {
   let contactDetails = null;
+  const NO_OF_NO_DATA_FIELDS = 0;
+  const NO_OF_FIELDS = 6;
+  const NO_OF_MISSING_DATA = 4;
+  const noDataMessage = 'No Data';
 
   beforeEach(() => {
-    contactDetails = CONTACT_DETAILS;
+    contactDetails = {
+      area_office: 'Test Area Office',
+      area_name: 'East Highland',
+      telephone_number: '01526272666',
+      area_office_address: ['test area1', 'test area2', 'test area3'],
+      postcode: 'eh21 8bj',
+      email: 'test@test.com',
+    };
   });
 
   it('should render the `No Data` message', () => {
     render(<AreaOfficeContactDetail contactDetails={[]} />);
-    expect(screen.getAllByText('No Data').length).toBe(6);
+    expect(screen.getAllByText(noDataMessage).length).toBe(NO_OF_FIELDS);
   });
 
   it('should render all data', () => {
@@ -33,15 +35,15 @@ describe('<AreaOfficeContactDetail />', () => {
     ).toBeInTheDocument();
     expect(screen.getByText(contactDetails.postcode)).toBeInTheDocument();
     expect(screen.getByText(contactDetails.email)).toBeInTheDocument();
-    expect(
-      screen.getByText(contactDetails.area_office_address[0]),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByText(contactDetails.area_office_address[1]),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByText(contactDetails.area_office_address[2]),
-    ).toBeInTheDocument();
+    const office_address = screen.queryAllByText(
+      contactDetails.area_office_address,
+    );
+    office_address.forEach((address, index) => {
+      expect(within(address).getByText(address[index])).toBeInTheDocument();
+    });
+    expect(screen.queryAllByText(noDataMessage).length).toBe(
+      NO_OF_NO_DATA_FIELDS,
+    );
   });
 
   it('should render some contact details data', () => {
@@ -53,7 +55,7 @@ describe('<AreaOfficeContactDetail />', () => {
         }}
       />,
     );
-    expect(screen.getAllByText('No Data').length).toBe(4);
+    expect(screen.getAllByText(noDataMessage).length).toBe(NO_OF_MISSING_DATA);
     expect(screen.getByText(contactDetails.area_name)).toBeInTheDocument();
   });
 });
