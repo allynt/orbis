34e887f596b3ef686a6e5546a7c1a2c5d996ada4
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-12-15 11:03:24 +0000

    fix(frontend): Fix a bug in the area contact detail component

--- client/src/dashboard/NatureScot/area-office-contact-details.component.js
+++ client/src/dashboard/NatureScot/area-office-contact-details.component.js
@@ -4,6 +4,7 @@ import { makeStyles, Grid, Link, Typography } from '@astrosat/astrosat-ui';
 
 import { ChartWrapper } from 'dashboard/charts/chart-wrapper.component';
 
+import { OrbisTableCell } from '../../components/table/table-cell.component';
 const NO_DATA = 'No Data';
 
 const useStyles = makeStyles(theme => ({}));
@@ -26,64 +27,66 @@ const AreaOfficeContactDetails = ({ contactDetails }) => {
 
   return (
     <ChartWrapper title="Area Office Contact Details">
-      <Grid container justifyContent="space-between" spacing={1}>
-        <Grid item xs={6}>
-          <Typography variant="h4">Area Office :</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="p">{area_office ?? NO_DATA}</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h4">Area Name :</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="p">{area_name ?? NO_DATA}</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h4">Telephone Number :</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="p">{telephone_number ?? NO_DATA}</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h4">Area Office Address :</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Grid container direction="column">
-            {area_office_address?.map(item => (
-              <Grid key={item} item>
-                <Typography variant="p" key={item}>
-                  {item}
-                </Typography>
-              </Grid>
-            )) ?? NO_DATA}
+      <OrbisTableCell>
+        <Grid container justifyContent="space-between" spacing={1}>
+          <Grid item xs={6}>
+            <Typography variant="h4">Area Office :</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="p">{area_office ?? NO_DATA}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4">Area Name :</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="p">{area_name ?? NO_DATA}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4">Telephone Number :</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="p">{telephone_number ?? NO_DATA}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4">Area Office Address :</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container direction="column">
+              {area_office_address?.map(item => (
+                <Grid key={item} item>
+                  <Typography variant="p" key={item}>
+                    {item}
+                  </Typography>
+                </Grid>
+              )) ?? NO_DATA}
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4">Postcode :</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="p">{postcode ?? NO_DATA}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h4">Email :</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="p" className={styles.value}>
+              {!!email ? (
+                <Link
+                  target="_blank"
+                  href={`mailto:${email}`}
+                  rel="noopener noreferrer"
+                >
+                  {email}
+                </Link>
+              ) : (
+                NO_DATA
+              )}
+            </Typography>
           </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h4">Postcode :</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="p">{postcode ?? NO_DATA}</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h4">Email :</Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography variant="p" className={styles.value}>
-            {!!email ? (
-              <Link
-                target="_blank"
-                href={`mailto:${email}`}
-                rel="noopener noreferrer"
-              >
-                {email}
-              </Link>
-            ) : (
-              NO_DATA
-            )}
-          </Typography>
-        </Grid>
-      </Grid>
+      </OrbisTableCell>
     </ChartWrapper>
   );
 };
