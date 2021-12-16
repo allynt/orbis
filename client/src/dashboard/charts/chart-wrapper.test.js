99fd81e315cdb919693473d37daeaa77c13b1e65
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-12-16 12:49:58 +0000

    feat(frontend): Created three tests for the chart-wrapper component.

--- client/src/dashboard/charts/chart-wrapper.test.js
+++ client/src/dashboard/charts/chart-wrapper.test.js
@@ -6,22 +6,35 @@ import { ChartWrapper } from './chart-wrapper.component';
 
 const title = 'Test Title';
 const info = 'It is a test info message';
+
 describe('< ChartWrapper/>', () => {
-  it('the info icon should be visible if exist', () => {
-    render(<ChartWrapper title={title} info={info} children={[]} />);
+  it('the info icon should be visible', () => {
+    render(
+      <ChartWrapper title={title} info={info}>
+        {<div>Test Children</div>}
+      </ChartWrapper>,
+    );
     expect(screen.getByRole('img', { name: 'Info' })).toBeInTheDocument();
   });
 
   it('should render the info icon message when user clicks on it', () => {
-    render(<ChartWrapper title={title} info={info} children={[]} />);
+    render(
+      <ChartWrapper title={title} info={info}>
+        <div>Test Children</div>
+      </ChartWrapper>,
+    );
     userEvent.click(screen.getByRole('button', { name: 'Info' }));
     expect(screen.getByText(info)).toBeInTheDocument();
-    console.log(info);
   });
 
-  it('should render the info icon message when user clicks on it', () => {
-    render(<ChartWrapper title={title} info={info} children={[]} />);
-    userEvent.click(screen.getByRole('button', { name: 'Info' }));
-    expect(screen.getByText(info)).toBeInTheDocument();
+  it('should render the title', () => {
+    render(
+      <ChartWrapper title={title} info={info}>
+        <div>Test Children</div>
+      </ChartWrapper>,
+    );
+    expect(
+      screen.getByRole('heading', { name: 'Test Title' }),
+    ).toBeInTheDocument();
   });
 });
