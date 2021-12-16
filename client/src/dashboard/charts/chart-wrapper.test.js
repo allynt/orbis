3bfff4bf7cb955c6030efa35f23bef4998cd0217
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-12-16 16:16:03 +0000

    feat(frontend): Add one more test, refactor some test code for better readability.

--- client/src/dashboard/charts/chart-wrapper.test.js
+++ client/src/dashboard/charts/chart-wrapper.test.js
@@ -4,37 +4,46 @@ import { render, screen, userEvent } from 'test/test-utils';
 
 import { ChartWrapper } from './chart-wrapper.component';
 
-const title = 'Test Title';
-const info = 'It is a test info message';
+const TITLE = 'Test Title';
+const INFO = 'It is a test info message';
 
 describe('< ChartWrapper/>', () => {
-  it('the info icon should be visible', () => {
-    render(
-      <ChartWrapper title={title} info={info}>
-        {<div>Test Children</div>}
-      </ChartWrapper>,
-    );
-    expect(screen.getByRole('img', { name: 'Info' })).toBeInTheDocument();
-  });
-
-  it('should render the info icon message when user clicks on it', () => {
-    render(
-      <ChartWrapper title={title} info={info}>
-        <div>Test Children</div>
-      </ChartWrapper>,
-    );
-    userEvent.click(screen.getByRole('button', { name: 'Info' }));
-    expect(screen.getByText(info)).toBeInTheDocument();
-  });
-
   it('should render the title', () => {
     render(
-      <ChartWrapper title={title} info={info}>
+      <ChartWrapper title={TITLE} info={INFO}>
         <div>Test Children</div>
       </ChartWrapper>,
     );
     expect(
       screen.getByRole('heading', { name: 'Test Title' }),
     ).toBeInTheDocument();
+  });
+
+  it('the info icon should be visible', () => {
+    render(
+      <ChartWrapper title={TITLE} info={INFO}>
+        <div>Test Children</div>
+      </ChartWrapper>,
+    );
+    expect(screen.getByRole('img', { name: 'Info' })).toBeInTheDocument();
+  });
+
+  it('should not render the info icon when info prop is not pass ', () => {
+    render(
+      <ChartWrapper title={TITLE}>
+        <div>Test Children</div>
+      </ChartWrapper>,
+    );
+    expect(screen.queryByRole('img', { name: 'Info' })).not.toBeInTheDocument();
+  });
+
+  it('should render the info icon message when user clicks on it', () => {
+    render(
+      <ChartWrapper title={TITLE} info={INFO}>
+        <div>Test Children</div>
+      </ChartWrapper>,
+    );
+    userEvent.click(screen.getByRole('button', { name: 'Info' }));
+    expect(screen.getByText(INFO)).toBeInTheDocument();
   });
 });
