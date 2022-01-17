6b153319655d82d158db6d417cbd5135965f9512
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-01-17 12:00:56 +0000

    chore(backend): updated tests
    Added a test to make sure adding a new CustomerUser w/ a licence removes any
    default licences that are also exclusive from that CustomerUser

--- server/orbis/tests/test_customers.py
+++ server/orbis/tests/test_customers.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from rest_framework import status
 
 from astrosat.tests.utils import *
+
+from astrosat_users.models import CustomerUser
 from astrosat_users.tests.utils import *
 
 from .factories import *
@@ -90,17 +92,39 @@ class TestCustomerUserViews:
         assert licences.count() == 1
         assert exclusive_orb not in orbs and normal_orb in orbs
 
-
- def test_add_customer_user_removes_exclusive_default_licence(
-        self, user, api_client, mock_storage
+    def test_add_customer_user_removes_exclusive_default_licence(
+        self, user, user_data, api_client, mock_storage
     ):
-        # tests that adding a non-exclusive licence to a nwe user via the API
-        # correctly removes any exlusive licences that were added b/c they were default from that user
+        # tests that adding a non-exclusive licence to a new user via the API
+        # correctly removes any exlusive licences that were added b/c they were default
 
         exclusive_default_orb = OrbFactory(is_exclusive=True, is_default=True)
         normal_orb = OrbFactory(is_exclusive=False)
 
         customer = CustomerFactory(logo=None)
+        customer.add_user(user, type="MANAGER", status="ACTIVE")
 
-        # TODO: I AM HERE
-        pass
+        licences = customer.add_licences(normal_orb, 1)
+
+        data = {
+            "customer": customer.name,
+            "user": {
+                "name": user_data["name"], "email": user_data["email"]
+            },
+            "licences": map(lambda x: x.id, licences),
+        }
+
+        client = api_client(user)
+        url = reverse("customer-users-list", args=[customer.id])
+
+        response = client.post(url, data, format="json")
+        content = response.json()
+
+        assert status.is_success(response.status_code)
+
+        # assert that the view correctly added a licence to the specified orb
+        # and also added a licence to the default orb, but then removed it b/c it's exclusive
+        customer_user = CustomerUser.objects.get(id=content["id"])
+        assert customer_user.licences.filter(orb=normal_orb).exists()
+        assert not customer_user.licences.filter(orb=exclusive_default_orb
+                                                ).exists()
