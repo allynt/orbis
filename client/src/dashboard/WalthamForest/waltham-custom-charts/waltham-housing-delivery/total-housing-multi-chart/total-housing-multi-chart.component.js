6d4734b39f8ecdb1eec7e9e634624addf3c54fe6
Rory MacGregor <rorymacgregor@astrosat.net>
2022-02-09 09:47:49 +0000

    fix(frontend): Add tests for grouped transformer, remove functions and tests from utils
    Moved the `groupedDataTransformer` and its tests out of the utils file and into the component's file,
    as it is not specific to that dataset with that chart type.
    
    Also added tests for the transformer.

--- client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-delivery/total-housing-multi-chart/total-housing-multi-chart.component.js
+++ client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-delivery/total-housing-multi-chart/total-housing-multi-chart.component.js
@@ -7,15 +7,71 @@ import { VictoryGroup, VictoryBar, VictoryLine, VictoryScatter } from 'victory';
 import { BaseChart } from 'dashboard/charts/base-chart/base-chart.component';
 import { useChartTheme } from 'dashboard/useChartTheme';
 import { GroupedWidthCalculator } from 'dashboard/utils';
-import {
-  groupedDataTransformer,
-  userTargetTransformer,
-} from 'dashboard/WalthamForest/utils';
+import { userTargetTransformer } from 'dashboard/WalthamForest/utils';
 import { WalthamCustomLegend } from 'dashboard/WalthamForest/waltham-custom-legend/waltham-custom-legend.component';
 import {
   TENURE_DATA_TYPES,
   TARGET_LEGEND_DATA,
 } from 'dashboard/WalthamForest/waltham.constants';
+
+/**
+ * This function is necessary because the data does not match what Victory
+ * expects. Specifically, the 'gross' and 'net' values must be split into
+ * separate arrays so that they can be uses to render separate chart datasets.
+ *
+ * It is also used to create a stable timeline of earliest-latest year,
+ * that can house the data on the chart, allowing missing values from
+ * either dataset, padding them with `null` values.
+ *
+ * @param {object[]} data
+ * @param {object[]} targets
+ * @returns {{
+ *  x: string
+ *  y: number
+ * }[][]}
+ */
+export const dataTransformer = (data, targets) => {
+  if (!data) return;
+
+  const apiYears = data.map(obj => {
+    const [year] = obj.Year.split('-');
+    return +year;
+  });
+
+  // Object.keys(targets).length? Deleted targets could be empty object
+  const targetYears = !!targets
+    ? Object.keys(targets).map(key => {
+        const [year] = key.split('-');
+        return +year;
+      })
+    : [];
+
+  const allYears = [...apiYears, ...targetYears];
+
+  const min = Math.min(...allYears);
+  const max = Math.max(...allYears);
+
+  let timeline = [];
+  for (let i = min; i <= max; i++) {
+    timeline = [...timeline, `${i}-${i + 1}`];
+  }
+
+  return Object.values(
+    timeline.reduce(
+      (acc, year) => {
+        const obj = data.find(datum => datum.Year === year) ?? {};
+        return {
+          gross: [
+            ...acc.gross,
+            { x: `${year}`, y: obj['Total Gross'] ?? null },
+          ],
+          net: [...acc.net, { x: `${year}`, y: obj['Total Net'] ?? null }],
+        };
+      },
+      { gross: [], net: [] },
+    ),
+  );
+};
 
 const TotalHousingMultiChart = ({ apiData, userTargetData }) => {
   const { walthamChartColors } = useChartTheme();
@@ -24,11 +80,11 @@ const TotalHousingMultiChart = ({ apiData, userTargetData }) => {
 
   // Transform API/target data to correct data shape, and create a
   // reliable timeline on which to base the data
-  const transformedData = groupedDataTransformer(apiData, userTargetData);
+  const transformedData = dataTransformer(apiData, userTargetData);
   const transformedTargets = userTargetTransformer(userTargetData);
 
-  // hardcoded to return most recent 5 years' data for now, but can be easily
-  // configured to cut out a 5-year window based on a start/end parameter
+  // Can be easily configured to cut out a 5-year window based on a
+  // start/end parameter, but not in scope of ticket
   const filteredApiData = [
     transformedData[0].slice(-5),
     transformedData[1].slice(-5),
