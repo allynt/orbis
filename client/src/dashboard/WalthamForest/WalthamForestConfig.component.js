570a6b930335d84253b5e12e2128ee4d4b578b05
Rory MacGregor <rorymacgregor@astrosat.net>
2022-02-10 16:26:30 +0000

    fix(frontend): Uncomment commented out components, improve other comments
    The other charts were commented out to prevent them rendering every time, this fixes.
    
    Also added some more meaningful comments to explain logic and structure.

--- client/src/dashboard/WalthamForest/WalthamForestConfig.component.js
+++ client/src/dashboard/WalthamForest/WalthamForestConfig.component.js
@@ -63,10 +63,9 @@ const useStyles = makeStyles(theme => ({
     gap: '1rem',
   },
   housingDelivery: {
-    // display: 'flex',
-    // flexDirection: 'column',
-    // gap: '1rem',
-    width: '90vw',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1rem',
   },
 }));
 
@@ -141,18 +140,18 @@ const WalthamForestDashboard = ({ sourceId }) => {
 
       <div className={styles.content}>
         {/* progress indicator charts */}
-        {/* <div className={styles.progressIndicators}>
+        <div className={styles.progressIndicators}>
           <ProgressIndicators
             totalData={totalHousingDelivery}
             tenureData={tenureHousingDeliveryChartData}
             userOrbState={userOrbState}
           />
-        </div> */}
+        </div>
 
         <div className={styles.barCharts}>
-          {/* <div className={styles.progression}>
+          <div className={styles.progression}>
             <ProgressionVsPlanningSchedule data={progressionVsPlanning} />
-          </div> */}
+          </div>
 
           <div className={styles.housingDelivery}>
             {/* group/line and stack/line charts */}
@@ -164,13 +163,13 @@ const WalthamForestDashboard = ({ sourceId }) => {
               userOrbState={userOrbState}
             />
             {/* big multi-line chart */}
-            {/* <HousingApprovalsComponent
+            <HousingApprovalsComponent
               x="Month"
               xLabel="Year"
               yLabel="No. Housing Approvals Granted"
               ranges={['2019', '2020']}
               data={approvalsGranted?.properties}
-            /> */}
+            />
           </div>
         </div>
       </div>
