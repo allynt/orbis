022d13ef7bb193793db007eae9e7a1788ff0752d
Rory MacGregor <rorymacgregor@astrosat.net>
2022-02-10 14:14:03 +0000

    fix(frontend): Updata logic, move files, add tests
    Updated the logic to match design specs, particularly limiting the timeline to display
    to the latest year in API data, but allowing it to go as far back as the earliest year
    in either dataset.
    
    Moved the transformer to its own directory, and added tests.

--- client/src/dashboard/WalthamForest/WalthamForestConfig.component.js
+++ client/src/dashboard/WalthamForest/WalthamForestConfig.component.js
@@ -63,10 +63,10 @@ const useStyles = makeStyles(theme => ({
     gap: '1rem',
   },
   housingDelivery: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '1rem',
-    // width: '90vw',
+    // display: 'flex',
+    // flexDirection: 'column',
+    // gap: '1rem',
+    width: '90vw',
   },
 }));
 
@@ -144,20 +144,18 @@ const WalthamForestDashboard = ({ sourceId }) => {
 
       <div className={styles.content}>
         {/* progress indicator charts */}
-        <div className={styles.progressIndicators}>
+        {/* <div className={styles.progressIndicators}>
           <ProgressIndicators
             totalData={totalHousingDelivery}
             tenureData={tenureHousingDeliveryChartData}
             userOrbState={userOrbState}
           />
-        </div>
+        </div> */}
 
         <div className={styles.barCharts}>
-          <div className={styles.progression}>
+          {/* <div className={styles.progression}>
             <ProgressionVsPlanningSchedule data={progressionVsPlanning} />
-            <DeliverableSupplySummary data={deliverableSupplySummary} />
-            <AffordableHousingDelivery data={affordableHousingDelivery} />
-          </div>
+          </div> */}
 
           <div className={styles.housingDelivery}>
             {/* group/line and stack/line charts */}
@@ -169,13 +167,13 @@ const WalthamForestDashboard = ({ sourceId }) => {
               userOrbState={userOrbState}
             />
             {/* big multi-line chart */}
-            <HousingApprovalsComponent
+            {/* <HousingApprovalsComponent
               x="Month"
               xLabel="Year"
               yLabel="No. Housing Approvals Granted"
               ranges={['2019', '2020']}
               data={approvalsGranted?.properties}
-            />
+            /> */}
           </div>
         </div>
       </div>
