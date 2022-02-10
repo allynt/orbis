62c1e6aefc147b54c164c7d47afd370eb00886c1
Rory MacGregor <rorymacgregor@astrosat.net>
2022-02-10 14:14:03 +0000

    fix(frontend): Updata logic, move files, add tests
    Updated the logic to match design specs, particularly limiting the timeline to display
    to the latest year in API data, but allowing it to go as far back as the earliest year
    in either dataset.
    
    Moved the transformer to its own directory, and added tests.

--- client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-delivery/total-housing-multi-chart/total-housing-multi-chart.component.js
+++ client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-delivery/total-housing-multi-chart/total-housing-multi-chart.component.js
@@ -7,97 +7,35 @@ import { VictoryGroup, VictoryBar, VictoryLine, VictoryScatter } from 'victory';
 import { BaseChart } from 'dashboard/charts/base-chart/base-chart.component';
 import { useChartTheme } from 'dashboard/useChartTheme';
 import { GroupedWidthCalculator } from 'dashboard/utils';
-import { userTargetTransformer } from 'dashboard/WalthamForest/utils';
 import { WalthamCustomLegend } from 'dashboard/WalthamForest/waltham-custom-legend/waltham-custom-legend.component';
 import {
   TENURE_DATA_TYPES,
   TARGET_LEGEND_DATA,
 } from 'dashboard/WalthamForest/waltham.constants';
 
-/**
- * This function is necessary because the data does not match what Victory
- * expects. Specifically, the 'gross' and 'net' values must be split into
- * separate arrays so that they can be uses to render separate chart datasets.
- *
- * It is also used to create a stable timeline of earliest-latest year,
- * that can house the data on the chart, allowing missing values from
- * either dataset, padding them with `null` values.
- *
- * @param {object[]} data
- * @param {object[]} targets
- * @returns {{
- *  x: string
- *  y: number
- * }[][]}
- */
-export const dataTransformer = (data, targets) => {
-  if (!data) return;
-
-  const apiYears = data.map(obj => {
-    const [year] = obj.Year.split('-');
-    return +year;
-  });
-
-  // Object.keys(targets).length? Deleted targets could be empty object
-  // What are uninitiated vs cleared targets?
-  const targetYears = !!targets
-    ? Object.keys(targets).map(key => {
-        const [year] = key.split('-');
-        return +year;
-      })
-    : [];
-
-  const allYears = [...apiYears, ...targetYears];
-
-  const min = Math.min(...allYears);
-  const max = Math.max(...allYears);
-
-  let timeline = [];
-  for (let i = min; i <= max; i++) {
-    timeline = [...timeline, `${i}-${i + 1}`];
-  }
-
-  return Object.values(
-    timeline.reduce(
-      (acc, year) => {
-        const obj = data.find(datum => datum.Year === year) ?? {};
-        return {
-          gross: [
-            ...acc.gross,
-            { x: `${year}`, y: obj['Total Gross'] ?? null },
-          ],
-          net: [...acc.net, { x: `${year}`, y: obj['Total Net'] ?? null }],
-        };
-      },
-      { gross: [], net: [] },
-    ),
-  );
-};
+import { totalHousingTransformer } from './total-housing-transformer/total-housing-transformer';
 
 const TotalHousingMultiChart = ({ apiData, userTargetData }) => {
   const { walthamChartColors } = useChartTheme();
 
   // Transform API/target data to correct data shape, and create a
-  // reliable timeline on which to base the data
-  const transformedData = useMemo(
-    () => dataTransformer(apiData, userTargetData),
+  // reliable timeline form earliest total year -> latest API data year
+  const transformerOutput = useMemo(
+    () => totalHousingTransformer(apiData, userTargetData),
     [apiData, userTargetData],
   );
 
-  const transformedTargets = useMemo(
-    () => userTargetTransformer(userTargetData),
-    [userTargetData],
-  );
+  if (!transformerOutput) return null;
 
-  if (!transformedData) return null;
+  const { transformedData, transformedTargets } = transformerOutput;
 
-  // Can be easily configured to cut out a 5-year window based on a
-  // start/end parameter, but not in scope of ticket
+  // This can easily be upgraded later to cut out a 5-year window
+  // based on a start/end parameter, but hardcoded for now
   const filteredApiData = [
     transformedData[0].slice(-5),
     transformedData[1].slice(-5),
   ];
-  const filteredTargetData = transformedTargets.slice(-5);
+  const filteredTargetData = transformedTargets?.slice(-5);
 
   const apiLegendData = Object.values(TENURE_DATA_TYPES).map((type, i) => ({
     name: type,
