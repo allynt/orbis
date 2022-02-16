b3b315fb1f54ee32f09818fc492bb04f77811380
Rory MacGregor <rorymacgregor@astrosat.net>
2022-02-16 09:20:18 +0000

    fix(frontend): Upgrade tests
    Upgraded the tests for the tenure transformer to match those of the total housing transformer.

--- client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-delivery/tenure-housing-multi-chart/tenure-housing-transformer/tenure-housing-transformer.test.js
+++ client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-delivery/tenure-housing-multi-chart/tenure-housing-transformer/tenure-housing-transformer.test.js
@@ -7,9 +7,10 @@ jest.mock('dashboard/WalthamForest/waltham.constants', () => ({
   },
 }));
 
-describe('tenureHousingTransformer', () => {
-  it('transforms API and target data into correct data shape', () => {
-    const data = [
+const dataArray = [
+  {
+    title: 'transforms API and target data into correct data shape',
+    data: [
       {
         Year: '2015-2016',
         Market: 123,
@@ -20,27 +21,33 @@ describe('tenureHousingTransformer', () => {
         Market: 789,
         Intermediate: 101,
       },
-    ];
-
-    const targets = {
+    ],
+    targets: {
       '2015-2016': '181',
       '2016-2017': '155',
-    };
-
-    const expected = {
-      transformedData: data,
+    },
+    expected: {
+      transformedData: [
+        {
+          Year: '2015-2016',
+          Market: 123,
+          Intermediate: 456,
+        },
+        {
+          Year: '2016-2017',
+          Market: 789,
+          Intermediate: 101,
+        },
+      ],
       transformedTargets: [
         { x: '2015-2016', y: 181 },
         { x: '2016-2017', y: 155 },
       ],
-    };
-
-    const result = tenureHousingTransformer(data, targets);
-    expect(result).toEqual(expected);
-  });
-
-  it('pads null values if there are missing years', () => {
-    const data = [
+    },
+  },
+  {
+    title: 'pads null values if there are missing years',
+    data: [
       {
         Year: '2015-2016',
         Market: 123,
@@ -51,93 +58,112 @@ describe('tenureHousingTransformer', () => {
         Market: 789,
         Intermediate: 101,
       },
-    ];
-
-    const targets = {
+    ],
+    targets: {
       '2014-2015': '181',
       '2015-2016': '155',
-    };
-
-    const expected = {
+    },
+    expected: {
       transformedData: [
         { Year: '2014-2015', Market: null, Intermediate: null },
-        ...data,
+        {
+          Year: '2015-2016',
+          Market: 123,
+          Intermediate: 456,
+        },
+        {
+          Year: '2016-2017',
+          Market: 789,
+          Intermediate: 101,
+        },
       ],
       transformedTargets: [
         { x: '2014-2015', y: 181 },
         { x: '2015-2016', y: 155 },
       ],
-    };
-
-    const result = tenureHousingTransformer(data, targets);
-    expect(result).toEqual(expected);
-  });
-
-  it('only returns data as high as latest year in api data', () => {
-    const data = [
+    },
+  },
+  {
+    title: 'only returns data as high as latest year in api data',
+    data: [
       {
         Year: '2015-2016',
         Market: 123,
         Intermediate: 456,
       },
-    ];
-
-    const targets = {
+    ],
+    targets: {
       '2015-2016': '181',
       '2016-2017': '155',
-    };
-
-    const expected = {
-      transformedData: data,
+    },
+    expected: {
+      transformedData: [
+        {
+          Year: '2015-2016',
+          Market: 123,
+          Intermediate: 456,
+        },
+      ],
       transformedTargets: [{ x: '2015-2016', y: 181 }],
-    };
-
-    const result = tenureHousingTransformer(data, targets);
-    expect(result).toEqual(expected);
-  });
-
-  it('returns null target data if higher than api data years', () => {
-    const data = [
+    },
+  },
+  {
+    title: 'returns null target data if higher than api data years',
+    data: [
       {
         Year: '2015-2016',
         Market: 123,
         Intermediate: 456,
       },
-    ];
-
-    const targets = {
+    ],
+    targets: {
       '2016-2017': '155',
-    };
-
-    const expected = {
-      transformedData: data,
+    },
+    expected: {
+      transformedData: [
+        {
+          Year: '2015-2016',
+          Market: 123,
+          Intermediate: 456,
+        },
+      ],
       transformedTargets: null,
-    };
-
-    const result = tenureHousingTransformer(data, targets);
-    expect(result).toEqual(expected);
-  });
-
-  it('returns null targets if no targets present', () => {
-    const data = [
+    },
+  },
+  {
+    title: 'returns null targets if no targets present',
+    data: [
       {
         Year: '2015-2016',
         Market: 123,
         Intermediate: 456,
       },
-    ];
-
-    const expected = {
-      transformedData: data,
+    ],
+    targets: undefined,
+    expected: {
+      transformedData: [
+        {
+          Year: '2015-2016',
+          Market: 123,
+          Intermediate: 456,
+        },
+      ],
       transformedTargets: null,
-    };
+    },
+  },
+  {
+    title: 'returns undefined if no data present',
+    data: undefined,
+    targets: undefined,
+    expected: undefined,
+  },
+];
 
-    const result = tenureHousingTransformer(data, undefined);
-    expect(result).toEqual(expected);
-  });
-
-  it('returns undefined if no data present', () => {
-    const result = tenureHousingTransformer(undefined);
-    expect(result).toBeUndefined();
-  });
+describe('tenureHousingTransformer', () => {
+  dataArray.forEach(({ title, data, targets, expected }) =>
+    it(title, () => {
+      const result = tenureHousingTransformer(data, targets);
+      expect(result).toEqual(expected);
+    }),
+  );
 });
