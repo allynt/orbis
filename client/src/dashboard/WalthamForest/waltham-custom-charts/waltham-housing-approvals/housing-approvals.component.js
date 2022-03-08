73df8e61e11265382b975421ae466e5f0bd349e0
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-03-08 14:33:43 +0000

    Try to refactor map method

--- client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-approvals/housing-approvals.component.js
+++ client/src/dashboard/WalthamForest/waltham-custom-charts/waltham-housing-approvals/housing-approvals.component.js
@@ -6,15 +6,25 @@ import {
   makeStyles,
 } from '@astrosat/astrosat-ui';
 
-import { VictoryGroup, VictoryLine, VictoryScatter } from 'victory';
+import {
+  VictoryGroup,
+  VictoryLine,
+  VictoryScatter,
+  VictoryChart,
+  VictoryVoronoiContainer,
+  VictoryTooltip,
+} from 'victory';
 
 import { BaseChart } from 'dashboard/charts/base-chart/base-chart.component';
 import { ChartWrapper } from 'dashboard/charts/chart-wrapper.component';
+import * as MOCK_DATA from 'dashboard/mock-data/waltham-forest/mock_approvals_granted';
 import { useChartTheme } from 'dashboard/useChartTheme';
 import FlyoutTooltip from 'dashboard/WalthamForest/FlyoutTooltip';
 import { WalthamCustomLegend } from 'dashboard/WalthamForest/waltham-custom-legend/waltham-custom-legend.component';
 import { HOUSING_APPROVAL_DATA_TYPES } from 'dashboard/WalthamForest/waltham.constants';
 
+import configuration from '../../../../map/orbs/configurations/pldConfig';
+import { labelsForArrayOfObjectsInclusive } from '../../tooltips-utils';
 import { lineDataTransformer } from '../../utils';
 const useStyles = makeStyles(theme => ({
   toggleButtonGroup: {
@@ -40,7 +50,7 @@ const HousingApprovalsComponent = ({
     userOrbState.approvalsGrantedDataType ??
       HOUSING_APPROVAL_DATA_TYPES.monthly,
   );
-
+  console.log('Configaration', configuration);
   /**
    * @param {any} _
    * @param {string} newValue
@@ -53,8 +63,17 @@ const HousingApprovalsComponent = ({
     }));
   };
 
+  // const dataByType = lineDataTransformer;
+
   const dataByType = useMemo(
-    () => lineDataTransformer(data?.find(p => p.name === configuration)?.data),
+    () =>
+      lineDataTransformer(
+        data?.find(p => {
+          console.log('data in memo', data);
+          console.log('p name', p.name);
+          return p.name === configuration;
+        })?.data,
+      ),
     [data, configuration],
   );
 
@@ -79,29 +98,76 @@ const HousingApprovalsComponent = ({
     );
   };
 
-  const renderLineChart = width =>
-    !!dataByType
-      ? ranges?.map((range, i) => {
+  const renderLineChart = width => {
+    if (!dataByType) return null;
+
+    return (
+      <VictoryGroup>
+        {ranges?.map((range, i) => {
           const color = walthamChartColors.housingApproval[i],
             props = {
               data: dataByType,
               x,
               y: range,
             };
-          return (
-            <VictoryGroup key={range}>
-              <VictoryLine {...props} style={{ data: { stroke: color } }} />
 
-              <VictoryScatter
-                {...props}
-                style={{ data: { stroke: color } }}
-                labelComponent={FlyoutTooltip()}
-                labels={({ datum }) => `Total: ${datum._y}`}
-              />
-            </VictoryGroup>
+          <VictoryLine
+            {...props}
+            style={{ data: { stroke: color } }}
+            key={range}
+          />;
+        })}
+        {ranges?.map((range, i) => {
+          const color = walthamChartColors.housingApproval[i],
+            props = {
+              data: dataByType,
+              x,
+              y: range,
+            };
+
+          return (
+            <VictoryScatter
+              key={range}
+              {...props}
+              size={10}
+              style={{ data: { stroke: color } }}
+              labelComponent={<VictoryTooltip />}
+              labels={({ datum }) => {
+                console.log('Props', props);
+                console.log('DAtum_Y', datum._y);
+                return ` ${datum._y}`;
+              }}
+            />
           );
-        })
-      : null;
+        })}
+        ;
+      </VictoryGroup>
+    );
+  };
+
+  // const renderLineChart = width =>
+  //   !!dataByType
+  //     ? ranges?.map((range, i) => {
+  //         const color = walthamChartColors.housingApproval[i],
+  //           props = {
+  //             data: dataByType,
+  //             x,
+  //             y: range,
+  //           };
+  //         return (
+  //           <VictoryGroup key={range}>
+  //             <VictoryLine {...props} style={{ data: { stroke: color } }} />
+
+  //             <VictoryScatter
+  //               {...props}
+  //               style={{ data: { stroke: color } }}
+  //               labelComponent={FlyoutTooltip()}
+  //               labels={({ datum }) => `Total: ${datum._y}`}
+  //             />
+  //           </VictoryGroup>
+  //         );
+  //       })
+  //     : null;
 
   return (
     <ChartWrapper
@@ -134,3 +200,41 @@ const HousingApprovalsComponent = ({
 };
 
 export { HousingApprovalsComponent };
+
+//   if (!dataByType) return null;
+
+//   return (
+// <VictoryChart key="myLineChart">
+//   {ranges?.map((range, i) => {
+//     const color = walthamChartColors.housingApproval[i],
+//       props = {
+//         data: dataByType,
+//         x,
+//         y: range,
+//       };
+//     <VictoryLine {...props} style={{ data: { stroke: color } }} />;
+//   })}
+//   ;
+//   {ranges?.map((range, i) => {
+//     const color = walthamChartColors.housingApproval[i],
+//       props = {
+//         data: dataByType,
+//         x,
+//         y: range,
+//       };
+//     <VictoryScatter
+//       {...props}
+//       size={10}
+//       style={{ data: { stroke: color } }}
+//       labelComponent={<VictoryTooltip />}
+//       labels={({ datum }) => {
+//         console.log('Props', props);
+//         console.log('DAtum_Y', datum._y);
+//         return ` ${datum._y}`;
+//       }}
+//     />;
+//   })}
+//   ;
+// </VictoryChart>
+//   );
+// };
