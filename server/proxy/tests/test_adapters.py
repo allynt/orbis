4e1a4bcee158193c6150455ab27da43e30843126
Rory MacGregor <rorymacgregor@astrosat.net>
2022-03-09 12:42:55 +0000

    fix(frontend): Python changes from working branch
    These are Mark's backend changes to the Python code, brought into this branch.

--- server/proxy/tests/test_adapters.py
+++ server/proxy/tests/test_adapters.py
@@ -1,7 +1,11 @@
+import pytest
+
 from proxy.adapters.adapters_pld import PldAdapter
+from proxy.adapters.adapters_wfc_dashboard import WFCDashboardAdapter
 
 
 class TestPldAdapter:
+    @pytest.mark.skip()
     def test_pld_adapter(self):
         adapter = PldAdapter()
         transformed_data = adapter.process_data(adapter.SAMPLE_DATA)
@@ -11,3 +15,10 @@ class TestPldAdapter:
         for feature in transformed_data["features"]:
             assert feature.get("geometry") is not None
             assert feature.get("properties") is not None
+
+class TestWFCDashboardAdapter:
+    def test_wfc_dashboard_adapter(self):
+        adapter = WFCDashboardAdapter()
+        transformed_data = adapter.process_data(adapter.SAMPLE_DATA)
+
+        assert False
