0f92e55810b3f915399d0add65c72404c8510383
Mark Small <mark.small@astrosat.space>
2022-03-09 13:48:45 +0000

    feat(frontend): Add View Project button to tascomi pin popup
    Added the button to the tascomi pin popup to open a new tascomi
    dashboard, when clicked. Right now, it just opens the dashboard
    with a dummy message. The actual content of the dashboard will be
    done in a separate ticket.
    
    IssueID ORB-1184

--- client/src/map/orbs/components/TascomiMapComponent.js
+++ client/src/map/orbs/components/TascomiMapComponent.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import { Button, makeStyles } from '@astrosat/astrosat-ui';
+
 import { useDispatch, useSelector } from 'react-redux';
+import { push } from 'redux-first-history';
 
 import { FeatureDetail, Popup } from 'components';
 
 import { clickedFeaturesSelector, setClickedFeatures } from '../layers.slice';
 
+const useStyles = makeStyles(theme => ({
+  bodyFooter: {
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+}));
+
 const TascomiMapComponent = ({ source }) => {
+  const styles = useStyles();
+
   const pickedObjects = useSelector(state =>
     clickedFeaturesSelector(source?.source_id)(state?.orbs),
   );
@@ -33,7 +46,19 @@ const TascomiMapComponent = ({ source }) => {
         features={pickedObjects.map(obj => obj.properties)}
         title="Feature Detail"
         dateFormat="yyyy-MM-dd"
-      />
+      >
+        <div className={styles.bodyFooter}>
+          <Button
+            onClick={() =>
+              dispatch(
+                push(`dashboard?source_id=astrosat/tascomi/dashboard/latest`),
+              )
+            }
+          >
+            View Project
+          </Button>
+        </div>
+      </FeatureDetail>
     </Popup>
   );
 };
