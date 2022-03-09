ffcbb9ab852b9d838d2c43f755b4555185f424b6
Mark Small <mark.small@astrosat.space>
2022-03-09 13:46:06 +0000

    feat(frontend): Add Popup when tascomi map pin clicked
    Added code to manage displaying of a popup, when the user clicks
    one of the tascomi map pins.
    
    IssueID ORB-1189

---
+++ client/src/map/orbs/components/TascomiMapComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { FeatureDetail, Popup } from 'components';
+
+import { clickedFeaturesSelector, setClickedFeatures } from '../layers.slice';
+
+const TascomiMapComponent = ({ source }) => {
+  const pickedObjects = useSelector(state =>
+    clickedFeaturesSelector(source?.source_id)(state?.orbs),
+  );
+
+  const dispatch = useDispatch();
+
+  if (!pickedObjects?.length) return null;
+
+  return (
+    <Popup
+      longitude={pickedObjects[0]?.geometry.coordinates[0]}
+      latitude={pickedObjects[0]?.geometry.coordinates[1]}
+      onClose={() =>
+        dispatch(
+          setClickedFeatures({
+            key: source.source_id,
+            clickedFeatures: [],
+          }),
+        )
+      }
+      captureScroll
+    >
+      <FeatureDetail
+        features={pickedObjects.map(obj => obj.properties)}
+        title="Feature Detail"
+        dateFormat="yyyy-MM-dd"
+      />
+    </Popup>
+  );
+};
+
+export default TascomiMapComponent;
--- client/src/map/orbs/configurations/tascomiMapConfig.js
+++ client/src/map/orbs/configurations/tascomiMapConfig.js
@@ -1,4 +1,12 @@
-import { dataSelector } from '../layers.slice';
+import { FlyToInterpolator } from '@deck.gl/core';
+
+import { MAX_ZOOM } from 'map/map.constants';
+import { easeInOutCubic } from 'utils/easingFunctions';
+
+import {
+  setClickedFeatures,
+  dataSelector,
+} from '../layers.slice';
 
 const PIN_COLOR = [255, 254, 25, 255];
 
@@ -6,8 +14,28 @@ const PIN_COLOR = [255, 254, 25, 255];
  * @typedef {import('typings').GeoJsonFeature<{type?: string, Type?: string}>} ActionForHelpFeature
  */
 
-const configuration = ({ id, orbState }) => {
+const configuration = ({ id, dispatch, setViewState, orbState }) => {
   const data = dataSelector(id)(orbState);
+
+  /** @param {import('typings').PickedMapFeature} info */
+  const onClick = info => {
+    if (info.object.properties.cluster) {
+      if (info.object.properties.expansion_zoom <= MAX_ZOOM)
+        setViewState({
+          longitude: info.object.geometry.coordinates[0],
+          latitude: info.object.geometry.coordinates[1],
+          zoom: info.object.properties.expansion_zoom,
+          transitionDuration: 1000,
+          transitionEasing: easeInOutCubic,
+          transitionInterpolator: new FlyToInterpolator(),
+        });
+      else
+        dispatch(
+          setClickedFeatures({ key: id, clickedFeatures: info.objects }),
+        );
+    } else
+      dispatch(setClickedFeatures({ key: id, clickedFeatures: [info.object] }));
+  };
 
   const getPinColor = feature => PIN_COLOR;
 
@@ -15,6 +43,7 @@ const configuration = ({ id, orbState }) => {
     id,
     data,
     visible: true,
+    onClick: onClick,
     pointType: 'icon',
     getPinColor,
   };
