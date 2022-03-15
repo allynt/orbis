76a5581199405a9d96c683051b369073b2710f17
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-03-15 11:30:09 +0000

    feat(frontend): create a character counting mechanism in the description for impact assessment
    I added a character counting mechanism and some styling for the description for impact assessment.
    
    IssueID ORB-1115

--- client/src/dashboard/NatureScot/dialog/assessment-dialog-form.js
+++ client/src/dashboard/NatureScot/dialog/assessment-dialog-form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Button, makeStyles, TextField } from '@astrosat/astrosat-ui';
 
@@ -7,33 +7,63 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 import { Form } from 'components';
-// import { register } from 'numeral';
 
 const validationSchema = yup.object({
   description: yup.string(),
 });
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  fieldset: {
+    border: '4px solid',
+    borderColor: theme.palette.background.paper,
+  },
+  legend: {
+    fontSize: 10,
+    fontWeight: 800,
+  },
+  descriptionText: {
+    fontSize: 14,
+  },
+  placeholder: {
+    backgroundColor: theme.palette.background.paper,
+    height: 25,
+  },
+}));
 
-const DescriptionInput = ({ register }) => (
-  <fieldset>
-    <legend>Describe Your Development or Change</legend>
-    <p>
-      Please provide a basic description of your development or change e.g.
-      build a burn, convert shed to the house, clear a wood etc.
-    </p>
-    <TextField
-      id="description"
-      name="description"
-      {...register('description')}
-      label="Describe Your Change or Development"
-      placeholder="Please describe your development or change here"
-      margin="normal"
-      variant="filled"
-      helperText="0/150 characters max length"
-    />
-  </fieldset>
-);
+const DescriptionInput = ({ register }) => {
+  const styles = useStyles();
+  const [textAreaCount, ChangeTextAreaCount] = useState(0);
+
+  const recalculate = e => {
+    ChangeTextAreaCount(e.target.value.length);
+  };
+  return (
+    <fieldset className={styles.fieldset}>
+      <legend className={styles.legend}>
+        Describe Your Development or Change
+      </legend>
+      <p className={styles.descriptionText}>
+        Please provide a basic description of your development or change e.g.
+        build a burn, convert shed to the house, clear a wood etc.
+      </p>
+      <TextField
+        id="description"
+        name="description"
+        {...register('description')}
+        label="Describe Your Change or Development"
+        margin="normal"
+        helperText={`${textAreaCount}/5 characters max length`}
+        inputProps={{ maxLength: 5 }}
+        onChange={recalculate}
+        InputProps={{
+          disableUnderline: true,
+          classes: { input: styles.placeholder },
+        }}
+        focused
+      />
+    </fieldset>
+  );
+};
 
 const AssessmentDialogForm = ({ onSubmit }) => {
   const styles = useStyles();
