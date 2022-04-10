6a82419f7e54c1dd596cad3c6643c35b72925a05
Mark Small <mark.small@astrosat.space>
2022-04-10 23:15:33 +0100

    fix(backend): Add Proposal permissions
    Create new IsAdminOrOwner permission and added it, along with built-in
    IsAuthenticated permission to the viewset.
    
    IssueID ORB-1263

---
+++ server/orbs/eco_an_alba/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return user.is_superuser or user == obj.owner
+
--- server/orbs/eco_an_alba/views.py
+++ server/orbs/eco_an_alba/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 
+from rest_framework.permissions import IsAuthenticated
+
 from astrosat.decorators import swagger_fake
 
+from orbs.eco_an_alba.permissions import IsAdminOrOwner
 from orbs.eco_an_alba.models import Proposal
 from orbs.eco_an_alba.serializers import ProposalSerializer
 
 class ProposalViewSet(viewsets.ModelViewSet):
 
-    permission_classes = []
+    permission_classes = [IsAuthenticated, IsAdminOrOwner, ]
     serializer_class = ProposalSerializer
 
     @swagger_fake(Proposal.objects.none())
