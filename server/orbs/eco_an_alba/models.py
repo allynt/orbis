5f2707774745a595a10009c3d6124ecb40f772ab
Mark Small <mark.small@astrosat.space>
2022-04-12 14:58:04 +0100

    fix(backend): Temporarily remove report schema
    Since the report data shape has yet to be agreed, I have temporarily
    removed the validation for a looser version, that just returns true.
    Once the data shape has been finalized, I expect to update the schema
    to reflext the shape and add proper validation back in.
    
    IssueID ORB-1163

--- server/orbs/eco_an_alba/models.py
+++ server/orbs/eco_an_alba/models.py
@@ -8,60 +8,62 @@ from django.utils.translation import gettext as _
 
 from astrosat.utils import validate_schema
 
-REPORT_STATE_SCHEMA = {
-    # defines the schema of the report_state JSONField below
-    "summary": {
-        "type": "array",
-        "items": {
-            "type": "object",
-            "properties": {
-                "type": {
-                    "type": "string",
-                },
-                "impact": {
-                    "type": "string",
-                },
-            },
-        },
-    },
-    "areas": {
-        "type": "array",
-        "items": {
-            "type": "object",
-            "properties": {
-                "title": {
-                    "type": "string",
-                },
-            },
-        },
-    },
-    "impacts": {
-        "type": "array",
-        "items": {
-            "type": "object",
-            "properties": {
-                "name": {
-                    "type": "string",
-                },
-                "impact": {
-                    "type": "object",
-                    "properties": {
-                        "name": {
-                            "type": "string",
-                        },
-                        "effect": {
-                            "type": "string",
-                        },
-                    },
-                },
-            },
-        },
-    },
-    "required": ["summary", "areas", "impacts"],
-}
+# FIXME: Uncomment this when the data shape is fixed.
+# REPORT_STATE_SCHEMA = {
+#     # defines the schema of the report_state JSONField below
+#     "summary": {
+#         "type": "array",
+#         "items": {
+#             "type": "object",
+#             "properties": {
+#                 "type": {
+#                     "type": "string",
+#                 },
+#                 "impact": {
+#                     "type": "string",
+#                 },
+#             },
+#         },
+#     },
+#     "areas": {
+#         "type": "array",
+#         "items": {
+#             "type": "object",
+#             "properties": {
+#                 "title": {
+#                     "type": "string",
+#                 },
+#             },
+#         },
+#     },
+#     "impacts": {
+#         "type": "array",
+#         "items": {
+#             "type": "object",
+#             "properties": {
+#                 "name": {
+#                     "type": "string",
+#                 },
+#                 "impact": {
+#                     "type": "object",
+#                     "properties": {
+#                         "name": {
+#                             "type": "string",
+#                         },
+#                         "effect": {
+#                             "type": "string",
+#                         },
+#                     },
+#                 },
+#             },
+#         },
+#     },
+#     "required": ["summary", "areas", "impacts"],
+# }
 
 def validate_report_state(value):
-    return validate_schema(value, REPORT_STATE_SCHEMA)
+    # return validate_schema(value, REPORT_STATE_SCHEMA)
+    return True
 
 class ProposalManager(models.Manager):
     pass
