f5be1a1952c7ad61c94191622c4a3f182c345f26
stevefaeembra <stevendkay@gmail.com>
2022-05-11 11:09:45 +0100

    feat(frontend): Add tests for the button wording
    Additional tests for save button wording
    
    IssueID 1220

--- client/src/dashboard/NatureScot/assessments/assessment-results.component.test.js
+++ client/src/dashboard/NatureScot/assessments/assessment-results.component.test.js
@@ -23,4 +23,19 @@ describe('Assessment Results', () => {
 
     expect(screen.getAllByRole('button', { name: /info/i }).length).toBe(3);
   });
+
+  it('should render button as save if no id', () => {
+    const formState = { reportGenerated: '2020-01-01T00:00:00.000Z' };
+    render(<AssessmentResults results={RESULTS} formState={formState} />);
+    expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument();
+  });
+
+  it('should render button as save if no id', () => {
+    const formState = {
+      id: 'this_should_be_a_GUID_added_by_django',
+      reportGenerated: '2020-01-01T00:00:00.000Z',
+    };
+    render(<AssessmentResults results={RESULTS} formState={formState} />);
+    expect(screen.getByRole('button', { name: /update/i })).toBeInTheDocument();
+  });
 });
