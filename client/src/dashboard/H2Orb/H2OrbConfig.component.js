c9aecdc6ead650c0ee35844f2c22fd1cb7ef4207
Mark Small <mark.small@astrosat.net>
2022-05-31 17:55:43 +0100

    feat(frontend): Setup data fetching
    Setup data polling of indicator sensor data and added basic progress
    indicators. A number of values are hard-coded right now. There is also
    basic handling of breakpoints for smaller screens.
    
    IssueID ORB-1303

--- client/src/dashboard/H2Orb/H2OrbConfig.component.js
+++ client/src/dashboard/H2Orb/H2OrbConfig.component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { format } from 'date-fns';
 import { useSelector } from 'react-redux';
@@ -12,13 +12,46 @@ import {
 import { useInterval } from 'hooks/useInterval';
 import { getAuthTokenForSource } from 'utils/tokens';
 
-const H2OrbHeader = () => <h1>H2Orb Title</h1>;
+import ProgressIndicators from './progress-indicators.component';
+
 const DATE_FORMAT = 'dd-MM-yyyy';
 const START_DATE = format(new Date('2022-05-25'), DATE_FORMAT);
 const END_DATE = format(new Date(), DATE_FORMAT);
 const apiSourceId = 'astrosat/h2orb/indicators/latest';
+const INDICATORS = [
+  'Disolved Oxygen',
+  'Electrical Conductivity',
+  'PH',
+  'Temperature',
+];
+const INFOS = [
+  'Disolved Oxygen Info',
+  'Electrical Conductivity Info',
+  'PH Info',
+  'Temperature Info',
+];
 
 const DEFAULT_DELAY = 60000; // 1 minute
+
+const transformData = data => {
+  let transformed = [];
+
+  Object.entries(data.payload.params).forEach(([key, value], i) => {
+    const indicator = {
+      key: key,
+      name: INDICATORS[i],
+      info: INFOS[i],
+      target: 100,
+      progress: value,
+    };
+
+    transformed.push(indicator);
+  });
+
+  return transformed;
+};
+
+const H2OrbHeader = () => <h1>H2Orb Title</h1>;
 
 /**
  * Dashboard for H2Orb
@@ -58,15 +91,43 @@ const H2OrbDashboard = ({ sourceId }) => {
       const data = await response.json();
       console.log('DATA: ', data);
 
-      setProgressIndicators(data[0]);
+      const transformed = transformData(data[0]);
+
+      setProgressIndicators(transformed);
     })();
   }, delay);
 
   console.log('PROGRESS INDICATOR DATA', progressIndicators);
 
+  // useEffect(() => {
+  //   const transformed = transformData({
+  //     application_name: 'AQUASENSE - AQUACULTURE',
+  //     client_id: '1151-1193-1217-1115',
+  //     data_received_time: '2022-05-25 16-32-20',
+  //     dev_eui: 'ea421ba0219bb8db',
+  //     gateway_name: 'aq_trial_ug_lkjp',
+  //     payload: {
+  //       $lati: 21.8553,
+  //       $long: 88.4258,
+  //       params: {
+  //         DO: 86.45,
+  //         EC: 1419.91,
+  //         pH: 7.49,
+  //         temperature: 17.63,
+  //       },
+  //       timestamp: '345928983000',
+  //     },
+  //   });
+
+  //   console.log('TRANSFORMED: ', transformed);
+  //   setProgressIndicators(transformed);
+  // }, []);
+
+  // console.log('PROGRESS INDICATORS: ', progressIndicators);
+
   return (
     <DashboardWrapper isTabs HeaderComponent={<H2OrbHeader />}>
-      <h1>H2Orb Dashboard Content</h1>
+      <ProgressIndicators data={progressIndicators} />
     </DashboardWrapper>
   );
 };
---
+++ client/src/dashboard/H2Orb/progress-indicators.component.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Grid, Typography } from '@astrosat/astrosat-ui';
+
+import { format } from 'date-fns';
+
+import { ChartWrapper } from 'dashboard/charts/chart-wrapper.component';
+import { ProgressIndicatorChart } from 'dashboard/charts/progress-indicator-chart/progress-indicator-chart.component';
+import { useChartTheme } from 'dashboard/useChartTheme';
+
+const DATE_FORMAT = 'dd/MM/yyyy - HH:mm:ss';
+
+/**
+ * Takes an object of data, for a number of individual ProgressIndicatorCharts.
+ *
+ * @param {object} { data } - Object of data to be used for each chart.
+ */
+const ProgressIndicators = ({ data }) => {
+  const chartTheme = useChartTheme();
+
+  return (
+    <Grid container spacing={3}>
+      {data?.map(indicator => (
+        <Grid key={indicator.name} item xs={12} md={3}>
+          <ChartWrapper title={indicator.name} info={indicator.info}>
+            <ProgressIndicatorChart
+              property={indicator}
+              color={chartTheme.colors[0]}
+            />
+          </ChartWrapper>
+
+          <Typography>
+            {/* Last reading taken at: {format(data.data_received_time, DATE_FORMAT)} */}
+          </Typography>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default ProgressIndicators;
