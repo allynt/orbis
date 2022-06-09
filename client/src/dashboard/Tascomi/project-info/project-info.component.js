d1fdd0977f2e37a13da18c1fb104713bbf70a460
Rory MacGregor <rory.macgregor@astrosat.net>
2022-06-09 13:25:08 +0100

    fix(frontend): Refactor nested components
    Refactored the nested components to be styled independently from the mapped out components
    to avoid lots of code to condiaonally style.

--- client/src/dashboard/Tascomi/project-info/project-info.component.js
+++ client/src/dashboard/Tascomi/project-info/project-info.component.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
       textAlign: 'start',
     },
   },
-  nestedComponent: {
+  nestedContent: {
     alignItems: 'flex-start',
   },
   label: {
@@ -44,13 +44,10 @@ const DataWrapper = ({ data }) => {
     return (
       <div
         key={key}
-        className={clsx(
-          styles.content,
-          isNested ? styles.nestedComponent : null,
-        )}
+        className={clsx(styles.content, isNested ? styles.nestedContent : null)}
       >
-        <div className={styles.label}>{key}: </div>
-        <div>{value ?? '-'}</div>
+        <span className={styles.label}>{key}: </span>
+        <span>{value ?? '-'}</span>
       </div>
     );
   });
@@ -79,123 +76,144 @@ const ContentWrapper = ({ data }) => {
 /**
  * @param {{ selectedFeature: object }} props
  */
-const ProjectInfo = ({ selectedFeature }) => (
-  <Grid container alignItems="flex-start" wrap="wrap" spacing={4}>
-    <Grid item xs={6}>
-      <ChartWrapper
-        title="Reference Numbers"
-        info="Data source: LBWF Tascomi Planning"
-      >
-        <ContentWrapper
-          data={{
-            'Planning Application Reference Number':
-              selectedFeature['Application ID'],
-            UPRN: selectedFeature['UPRN'],
-          }}
-        />
-      </ChartWrapper>
-    </Grid>
+const ProjectInfo = ({ selectedFeature }) => {
+  const styles = useStyles();
+  return (
+    <Grid container alignItems="flex-start" wrap="wrap" spacing={4}>
+      <Grid item xs={6}>
+        <ChartWrapper
+          title="Reference Numbers"
+          info="Data source: LBWF Tascomi Planning"
+        >
+          <ContentWrapper
+            data={{
+              'Planning Application Reference Number':
+                selectedFeature['Application ID'],
+              UPRN: selectedFeature['UPRN'],
+            }}
+          />
+        </ChartWrapper>
+      </Grid>
+      <Grid item xs={6}>
+        <ChartWrapper
+          title="Location"
+          info="Data source: LBWF Tascomi Planning"
+        >
+          <ContentWrapper
+            data={{
+              Address: selectedFeature['Address'],
+              'Co-ordinates': selectedFeature['Site co-ordinates'],
+            }}
+          />
+        </ChartWrapper>
+      </Grid>
+      <Grid item xs={6}>
+        <ChartWrapper
+          title="Site Details"
+          info="Data source: LBWF Tascomi Planning"
+        >
+          <ContentWrapper
+            data={{
+              'Applicant Name': selectedFeature['Applicant name'],
+              "Applicant's Company Name":
+                selectedFeature["Applicant's company name"],
+              Description: selectedFeature.Description,
+              'Number of Units (Gross)':
+                selectedFeature['Number of units (Gross)'],
+              'Number of Units (Net)': selectedFeature['Number of units (Net)'],
+              'Site Tenure Mix (Gross)': !!selectedFeature[
+                'Site Tenure Mix (Gross)'
+              ]
+                ? Object.entries(
+                    selectedFeature['Site Tenure Mix (Gross)'],
+                  ).map(([key, value]) => (
+                    <div
+                      key={key}
+                      className={clsx(styles.content, styles.nestedContent)}
+                    >
+                      <span>{key}: </span>
+                      <span>{value ?? '-'}</span>
+                    </div>
+                  ))
+                : null,
+              'Site Tenure Mix (Net)': !!selectedFeature[
+                'Site Tenure Mix (Net)'
+              ]
+                ? Object.entries(selectedFeature['Site Tenure Mix (Net)']).map(
+                    ([key, value]) => (
+                      <div
+                        key={key}
+                        className={clsx(styles.content, styles.nestedContent)}
+                      >
+                        <span>{key}: </span>
+                        <span>{value ?? '-'}</span>
+                      </div>
+                    ),
+                  )
+                : null,
+              'No. Bedrooms': null,
+            }}
+          />
+        </ChartWrapper>
+      </Grid>
 
-    <Grid item xs={6}>
-      <ChartWrapper title="Location" info="Data source: LBWF Tascomi Planning">
-        <ContentWrapper
-          data={{
-            Address: selectedFeature['Address'],
-            'Co-ordinates': selectedFeature['Site co-ordinates'],
-          }}
-        />
-      </ChartWrapper>
+      <Grid item xs={6}>
+        <ChartWrapper
+          title="Dates"
+          info="Data sources: Tascomi, and Exacom ('S106 agreement date', 'S106 discharged date')"
+        >
+          <ContentWrapper
+            data={{
+              'Received Date': !!selectedFeature['Received date']
+                ? formatDate(selectedFeature['Received date'])
+                : null,
+              'Committee Date': !!selectedFeature['Committee date']
+                ? formatDate(selectedFeature['Committee date'])
+                : null,
+              'Decision Date': !!selectedFeature['Decision date']
+                ? formatDate(selectedFeature['Decision date'])
+                : null,
+              'Legal Agreement Date': !!selectedFeature['Legal agreement date']
+                ? formatDate(selectedFeature['Legal agreement date'])
+                : null,
+              'S106 Agreement Date': !!selectedFeature['S106 agreement date']
+                ? formatDate(selectedFeature['S106 agreement date'])
+                : null,
+              'Registration Date': !!selectedFeature['Registered date']
+                ? formatDate(selectedFeature['Registered date'])
+                : null,
+              'Lapsed Date': !!selectedFeature['Lapsed date']
+                ? formatDate(selectedFeature['Lapsed date'])
+                : null,
+            }}
+          />
+        </ChartWrapper>
+      </Grid>
+      <Grid item xs={6}>
+        <ChartWrapper title="CIL" info="Data source: Exacom">
+          <ContentWrapper
+            data={{ 'CIL Liable': selectedFeature['CIL liable'] }}
+          />
+        </ChartWrapper>
+      </Grid>
+      {/* for alignment in browser */}
+      <Grid item xs={6} />
+      <Grid item xs={6}>
+        <ChartWrapper title="S106" info="Data source: Exacom">
+          <ContentWrapper
+            data={{
+              'S106 Liable': selectedFeature['S106 liable'],
+              'S106 Conditions Discharged on Site':
+                selectedFeature['S106 discharged date'],
+            }}
+          />
+        </ChartWrapper>
+      </Grid>
+      {/* for alignment in browser */}
+      <Grid item xs={6} />
     </Grid>
-
-    <Grid item xs={6}>
-      <ChartWrapper
-        title="Site Details"
-        info="Data source: LBWF Tascomi Planning"
-      >
-        <ContentWrapper
-          data={{
-            'Applicant Name': selectedFeature['Applicant name'],
-            "Applicant's Company Name":
-              selectedFeature["Applicant's company name"],
-            Description: selectedFeature.Description,
-            'Number of Units (Gross)':
-              selectedFeature['Number of units (Gross)'],
-            'Number of Units (Net)': selectedFeature['Number of units (Net)'],
-            'Site Tenure Mix (Gross)': !!selectedFeature[
-              'Site Tenure Mix (Gross)'
-            ] ? (
-              <DataWrapper data={selectedFeature['Site Tenure Mix (Gross)']} />
-            ) : null,
-            'Site Tenure Mix (Net)': !!selectedFeature[
-              'Site Tenure Mix (Net)'
-            ] ? (
-              <DataWrapper data={selectedFeature['Site Tenure Mix (Net)']} />
-            ) : null,
-            'No. Bedrooms': null,
-          }}
-        />
-      </ChartWrapper>
-    </Grid>
-
-    <Grid item xs={6}>
-      <ChartWrapper
-        title="Dates"
-        info="Data sources: Tascomi, and Exacom ('S106 agreement date', 'S106 discharged date')"
-      >
-        <ContentWrapper
-          data={{
-            'Received Date': !!selectedFeature['Received date']
-              ? formatDate(selectedFeature['Received date'])
-              : null,
-            'Committee Date': !!selectedFeature['Committee date']
-              ? formatDate(selectedFeature['Committee date'])
-              : null,
-            'Decision Date': !!selectedFeature['Decision date']
-              ? formatDate(selectedFeature['Decision date'])
-              : null,
-            'Legal Agreement Date': !!selectedFeature['Legal agreement date']
-              ? formatDate(selectedFeature['Legal agreement date'])
-              : null,
-            'S106 Agreement Date': !!selectedFeature['S106 agreement date']
-              ? formatDate(selectedFeature['S106 agreement date'])
-              : null,
-            'Registration Date': !!selectedFeature['Registered date']
-              ? formatDate(selectedFeature['Registered date'])
-              : null,
-            'Lapsed Date': !!selectedFeature['Lapsed date']
-              ? formatDate(selectedFeature['Lapsed date'])
-              : null,
-          }}
-        />
-      </ChartWrapper>
-    </Grid>
-
-    <Grid item xs={6}>
-      <ChartWrapper title="CIL" info="Data source: Exacom">
-        <ContentWrapper
-          data={{ 'CIL Liable': selectedFeature['CIL liable'] }}
-        />
-      </ChartWrapper>
-    </Grid>
-
-    {/* for alignment in browser */}
-    <Grid item xs={6} />
-    <Grid item xs={6}>
-      <ChartWrapper title="S106" info="Data source: Exacom">
-        <ContentWrapper
-          data={{
-            'S106 Liable': selectedFeature['S106 liable'],
-            'S106 Conditions Discharged on Site':
-              selectedFeature['S106 discharged date'],
-          }}
-        />
-      </ChartWrapper>
-    </Grid>
-
-    {/* for alignment in browser */}
-    <Grid item xs={6} />
-  </Grid>
-);
+  );
+};
 
 export const ProjectInfoSkeleton = () => (
   <Grid
