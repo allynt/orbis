81867925b2b060a3e7be6d77fbd0f9d2e8b01feb
Mark Small <mark.small@astrosat.net>
2022-10-12 20:28:17 +0100

    feat(frontend): Add new cross-filtering layer config
    Using the ISO+ data layer config, I have simplified it a lot to just
    get the cross-filtering data visible on the map. I fully expect more
    work to come on this, to add `Data Filtering` for instance, as well
    as set the colours of map features correctly, but that can easily be
    copied from `isolationPlusLayerConfig.js`, when we focus on that.
    
    IssueID ORB-1418

---
+++ client/src/map/orbs/configurations/crossFilteringLayerConfig.js
@@ -0,0 +1,78 @@
+import { get } from 'lodash';
+
+import { getValueForTimestamp } from 'utils/data';
+
+import { dataSelector } from '../crossfilter-layers.slice';
+
+/** @typedef {import('typings').GeoJsonFeature<import('typings').IsoPlusCommonProperties>} AccessorFeature */
+
+export const COLOR_PRIMARY = [246, 190, 0, 255],
+  COLOR_TRANSPARENT = [0, 0, 0, 0],
+  COLOR_HOVER = [255, 255, 255],
+  OPACITY_FLAT = 150,
+  OPACITY_EXTRUDED = OPACITY_FLAT,
+  OPACITY_EXTRUDED_SELECTED = 255,
+  OPACITY_TRANSPARENT = 0,
+  LINE_WIDTH = 0,
+  LINE_WIDTH_SELECTED = 3,
+  TRANSITION_DURATION = 150,
+  // This is used to convert property values from floats to ints
+  // to avoid rounding problems. Ints are needed as filtering occurs
+  // on the GPU
+  FILTER_SCALING_VALUE = 1000;
+
+/**
+ * @param {import('typings').GeoJsonFeature} feature
+ * @param {import('typings').Property} selectedProperty
+ * @param {number} [selectedTimestamp]
+ */
+export const getValue = (feature, selectedProperty, selectedTimestamp) =>
+  selectedProperty.timeseries
+    ? getValueForTimestamp(
+        feature.properties[selectedProperty.name],
+        selectedTimestamp ?? selectedProperty.timeseries_latest_timestamp,
+      )
+    : get(feature.properties, selectedProperty.name);
+
+/**
+ * @param {{
+ *   id: string
+ *   data: GeoJSON.FeatureCollection
+ *   activeSources?: import('typings').Source[]
+ *   dispatch: import('redux').Dispatch
+ *   orbState: import('../orbReducer').OrbState
+ *   authToken?: string
+ * }} parameters
+ */
+const configuration = ({
+  id,
+  activeSources,
+  dispatch,
+  orbState,
+  authToken,
+}) => {
+  const source = activeSources?.find(source => source.source_id === id);
+  const urls = dataSelector(id)(orbState);
+  const data = urls ? urls[0] : [];
+  const extraData = urls ? urls.slice(1) : [];
+
+  return {
+    data,
+    extraData,
+    minZoom: source?.metadata?.minZoom,
+    maxZoom: source?.metadata?.maxZoom,
+    uniqueIdProperty: 'LAD Code',
+    getFillColor: feature => [0, 128, 200, 150],
+    getLineColor: [16, 16, 16],
+    getLineWidth: 64,
+    loadOptions: {
+      fetch: {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      },
+    },
+  };
+};
+
+export default configuration;
